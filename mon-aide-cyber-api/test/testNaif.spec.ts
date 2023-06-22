d39eb86a3b281f589c7c889fb0308a599974ad54
Bertrand Bougon <bertrandbougon@gmail.com>
2023-06-22 11:09:40 +0200

    Initialise une app node - un package.json est ajouté à la racine du projet afin de lancer les tests depuis la racine - prettier a été ajouté au pre-commit

---
+++ mon-aide-cyber-api/test/testNaif.spec.ts
@@ -0,0 +1,7 @@
+import { describe, expect, it } from "vitest";
+
+describe("Premier test naif", () => {
+  it("devrait être toujours vert", () => {
+    expect(true).to.be.true;
+  });
+});
--- mon-aide-cyber-ui/src/App.tsx
+++ mon-aide-cyber-ui/src/App.tsx
@@ -1,14 +1,13 @@
-import './App.css'
+import "./App.css";
 
 function App() {
-
   return (
     <>
       <div>
-          <h2>Mon Aide Cyber</h2>
+        <h2>Mon Aide Cyber</h2>
       </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
