77486b85d7731cca65a1176d842bf0afae253654
Bertrand Bougon <bertrandbougon@gmail.com>
2023-06-22 18:07:38 +0200

    Initialise un conteneur docker pour l'ui

--- mon-aide-cyber-ui/vite.config.ts
+++ mon-aide-cyber-ui/vite.config.ts
@@ -4,4 +4,11 @@ import react from "@vitejs/plugin-react-swc";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    watch: {
+      usePolling: true,
+    },
+    host: true,
+    port: 3000,
+  },
 });
