4a0bbdd610e7692071824ce553e148aad98744c5
Bertrand Bougon <bertrandbougon@gmail.com>
2023-06-27 22:15:29 +0200

    Ajoute un rate limit sur la route qui dessert le contenu statique de l'app React

--- mon-aide-cyber-api/src/serveur.ts
+++ mon-aide-cyber-api/src/serveur.ts
@@ -1,12 +1,24 @@
 import * as path from "path";
 import express, { Request, Response } from "express";
 import * as http from "http";
+import rateLimit from "express-rate-limit";
 
 const creeServeur = () => {
   let serveur: http.Server;
 
   const app = express();
 
+  const limiteurTrafficUI = rateLimit({
+    windowMs: 5 * 60 * 1000,
+    max: 100,
+    message:
+      "Vous avez atteint le nombre maximal de requête. Veuillez réessayer ultérieurement.",
+    standardHeaders: true,
+    legacyHeaders: false,
+    skip: (request: Request, _response: Response) =>
+      request.path.startsWith("/api"),
+  });
+  app.use(limiteurTrafficUI);
   app.use(
     express.static(path.join(__dirname, "./../../mon-aide-cyber-ui/dist")),
   );
