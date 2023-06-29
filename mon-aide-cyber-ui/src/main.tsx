56f1aeca793d50311ce67f349297c8c4444bb365
Bertrand Bougon <bertrandbougon@gmail.com>
2023-06-29 17:13:23 +0200

    [DIAGNOSTIQUE][SAISIE-CONTEXTE] Affiche le contenu de la première question sur le contexte de l'organisation

--- mon-aide-cyber-ui/src/main.tsx
+++ mon-aide-cyber-ui/src/main.tsx
@@ -2,9 +2,22 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./assets/styles/index.scss";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { ComposantDiagnostique } from "./composants/ComposantDiagnostique.tsx";
+
+const routeur = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+  {
+    path: "diagnostique/:idDiagnostique",
+    element: <ComposantDiagnostique />,
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={routeur} />
   </React.StrictMode>,
 );
