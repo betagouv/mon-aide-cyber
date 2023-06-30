2f2f1d7a8dc3311bb0d1973aedcb3ffe1ca61132
Bertrand Bougon <bertrandbougon@gmail.com>
2023-06-30 16:59:15 +0200

    [DIAGNOSTIQUE][SAISIE-CONTEXTE] Récupère le diagnostique dans son intégralité pour l'exposer à l'API

---
+++ mon-aide-cyber-api/src/diagnostique/serviceDiagnostique.ts
@@ -0,0 +1,14 @@
+import { Referentiel } from "./referentiel";
+import { AdaptateurDonnees } from "../adaptateurs/AdaptateurDonnees";
+import { Diagnostique } from "./diagnostique";
+import * as crypto from "crypto";
+
+export class ServiceDiagnostique {
+  constructor(private readonly adaptateurDonnees: AdaptateurDonnees) {}
+
+  diagnostique = async (id: crypto.UUID): Promise<Diagnostique> =>
+    this.adaptateurDonnees.lis().then((contenu) => ({
+      identifiant: id,
+      referentiel: contenu as Referentiel,
+    }));
+}
