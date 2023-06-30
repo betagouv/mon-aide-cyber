4647cf1b9fef11a31e7d4393158529364420156a
Bertrand Bougon <bertrandbougon@gmail.com>
2023-06-30 18:59:43 +0200

    [DIAGNOSTIQUE][SAISIE-CONTEXTE] Ajoute un transcripteur pour piloter le comportement de l'UI lors de l'afichage des réponses possibles

---
+++ mon-aide-cyber-api/test/utilitaires/aseptise.ts
@@ -0,0 +1,13 @@
+export const aseptise = (
+  chaineDeCaractere: string,
+  separateur = "-",
+): string => {
+  return chaineDeCaractere
+    .toString()
+    .normalize("NFD")
+    .replace(/[\u0300-\u036f]/g, "")
+    .toLowerCase()
+    .trim()
+    .replace(/[^a-z0-9 ]/g, "")
+    .replace(/\s+/g, separateur);
+};
