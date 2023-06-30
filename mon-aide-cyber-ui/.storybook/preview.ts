ad335276aeaf92f6e40ed972e326f70d3f3bf33b
Bertrand Bougon <bertrandbougon@gmail.com>
2023-06-30 09:02:43 +0200

    [DIAGNOSTIQUE][SAISIE-CONTEXTE] Initialise les tests de composant avec Storybook

---
+++ mon-aide-cyber-ui/.storybook/preview.ts
@@ -0,0 +1,17 @@
+import type { Preview } from "@storybook/react";
+import "../public/styles/fonts.css";
+import "../src/assets/styles/index.scss";
+
+const preview: Preview = {
+  parameters: {
+    actions: { argTypesRegex: "^on[A-Z].*" },
+    controls: {
+      matchers: {
+        color: /(background|color)$/i,
+        date: /Date$/,
+      },
+    },
+  },
+};
+
+export default preview;
