3f6c1344c619478e905a32ffd60b910597f04b18
Bertrand Bougon <bertrandbougon@gmail.com>
2023-06-30 14:55:22 +0200

    [DIAGNOSTIQUE][SAISIE-CONTEXTE] Ajoute un fournisseur d'entrepots
    - Afin de pouvoir récupérer les diagnostiques depuis l'API

---
+++ mon-aide-cyber-ui/src/stories/Diagnostique.stories.tsx
@@ -0,0 +1,65 @@
+import type { Meta, StoryObj } from "@storybook/react";
+import { waitFor, within } from "@storybook/testing-library";
+import { ComposantDiagnostique } from "../composants/ComposantDiagnostique.tsx";
+import { expect } from "@storybook/jest";
+import { FournisseurEntrepots } from "../fournisseurs/FournisseurEntrepot.ts";
+import { EntrepotDiagnostiqueMemoire } from "../../test/infrastructure/entrepots/EntrepotMemoire.ts";
+import { unDiagnostique } from "../../test/consructeurs/constructeurDiagnostique.ts";
+import { unReferentiel } from "../../test/consructeurs/constructeurReferentiel.ts";
+import { faker } from "@faker-js/faker/locale/fr";
+import { UUID } from "../types/Types.ts";
+
+const entrepotDiagnostiqueMemoire = new EntrepotDiagnostiqueMemoire();
+const identifiantDiagnostique: UUID = faker.string.uuid() as UUID;
+
+const meta = {
+  title: "Diagnostique",
+  component: ComposantDiagnostique,
+  parameters: {
+    layout: "fullscreen",
+  },
+  decorators: [
+    (story) => (
+      <FournisseurEntrepots.Provider
+        value={{ diagnostique: () => entrepotDiagnostiqueMemoire }}
+      >
+        {story()}
+      </FournisseurEntrepots.Provider>
+    ),
+  ],
+} satisfies Meta<typeof ComposantDiagnostique>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const QuestionDiagnostique: Story = {
+  name: "Affiche une seule question du diagnostique",
+  args: { identifiant: identifiantDiagnostique },
+  play: async ({ canvasElement }) => {
+    const diagnostique = unDiagnostique()
+      .avecIdentifiant(identifiantDiagnostique)
+      .avecUnReferentiel(
+        unReferentiel()
+          .avecUneQuestion("Quelle entreprise êtes-vous ?", [
+            "Entreprise privée (ex. TPE, PME, ETI)",
+          ])
+          .construis(),
+      )
+      .construis();
+    await entrepotDiagnostiqueMemoire.persiste(diagnostique);
+    const canvas = within(canvasElement);
+
+    expect(
+      await waitFor(() =>
+        canvas.getByText(
+          diagnostique.referentiel.contexte.questions[0].libelle,
+        ),
+      ),
+    ).toBeInTheDocument();
+    expect(
+      await waitFor(() =>
+        canvas.getByLabelText("Entreprise privée (ex. TPE, PME, ETI)"),
+      ),
+    ).toBeInTheDocument();
+  },
+};
---
+++ mon-aide-cyber-ui/test/consructeurs/constructeurDiagnostique.ts
@@ -0,0 +1,29 @@
+import { Referentiel } from "../../src/domaine/diagnostique/Referentiel.ts";
+import { unReferentiel } from "./constructeurReferentiel.ts";
+import { Diagnostique } from "../../src/domaine/diagnostique/Diagnostique.ts";
+import { faker } from "@faker-js/faker/locale/fr";
+import { UUID } from "../../src/types/Types.ts";
+
+class ConstructeurDiagnostique {
+  private referentiel: Referentiel = unReferentiel().construis();
+  private identifiant: UUID = faker.string.uuid() as UUID;
+
+  avecUnReferentiel(referentiel: Referentiel): ConstructeurDiagnostique {
+    this.referentiel = referentiel;
+    return this;
+  }
+
+  avecIdentifiant(identifiant: UUID): ConstructeurDiagnostique {
+    this.identifiant = identifiant;
+    return this;
+  }
+
+  construis(): Diagnostique {
+    return {
+      identifiant: this.identifiant,
+      referentiel: this.referentiel,
+    };
+  }
+}
+
+export const unDiagnostique = () => new ConstructeurDiagnostique();
