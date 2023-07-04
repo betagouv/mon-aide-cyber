e8f75ac940cc3fdca1677c160bbd9b5476d73fe8
Bertrand Bougon <bertrandbougon@gmail.com>
2023-07-04 13:15:57 +0200

    [DIAGNOSTIQUE][SAISIE-CONTEXTE] Ajoute des marges pour dissocier la saisie de texte et les questions issues de réponses

--- mon-aide-cyber-ui/src/composants/ComposantDiagnostique.tsx
+++ mon-aide-cyber-ui/src/composants/ComposantDiagnostique.tsx
@@ -45,7 +45,7 @@ const ComposantReponsePossible = (
       <label htmlFor={proprietes.reponsePossible.identifiant}>
         {proprietes.reponsePossible.libelle}
       </label>
-      {champsASaisir}
+      <div>{champsASaisir}</div>
       {proprietes.children}
       <br />
     </>
@@ -82,7 +82,7 @@ const ComposantQuestion = ({ question }: ProprietesComposantQuestion) => {
             typeDeSaisie="radio"
           >
             {reponse.question?.type === "choixMultiple" ? (
-              <>
+              <div className="question-secondaire">
                 <br />
                 <label>{reponse.question?.libelle}</label>
                 <br />
@@ -96,7 +96,7 @@ const ComposantQuestion = ({ question }: ProprietesComposantQuestion) => {
                     />
                   );
                 })}
-              </>
+              </div>
             ) : (
               ""
             )}
@@ -132,7 +132,7 @@ export const ComposantDiagnostique = ({
     <>
       <form>
         <h2>Contexte</h2>
-        <section>
+        <section className="question">
           <div>
             {etatReferentiel.diagnostique?.referentiel?.contexte.questions.map(
               (question) => (
