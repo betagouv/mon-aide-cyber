10abb66d896dce6c4079adcca0e10a22cd03bcd1
Bertrand Bougon <bertrandbougon@gmail.com>
2023-07-07 10:10:11 +0200

    [REMANIEMENT] Corrige la signature de la méthode 'lis' de l'adaptateur pour retourner un Referentiel

--- mon-aide-cyber-api/src/infrastructure/adaptateurs/AdaptateurReferentielMAC.ts
+++ mon-aide-cyber-api/src/infrastructure/adaptateurs/AdaptateurReferentielMAC.ts
@@ -1,6 +1,7 @@
 import { AdaptateurReferentiel } from "../../adaptateurs/AdaptateurReferentiel";
 import { referentiel } from "../../diagnostique/donneesReferentiel";
+import { Referentiel } from "../../diagnostique/Referentiel";
 
 export class AdaptateurReferentielMAC implements AdaptateurReferentiel {
-  lis = (): Promise<object> => Promise.resolve({ referentiel });
+  lis = (): Promise<Referentiel> => Promise.resolve(referentiel);
 }
