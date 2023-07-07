afd5652e5cf2f24ae87c1763da46b33ad71008c9
Bertrand Bougon <bertrandbougon@gmail.com>
2023-07-07 10:34:16 +0200

    [REMANIEMENT] Extrais les routes de l'API pour le diagnostic
    - renomme la route /diagnostiques/:id en /diagnostique/:id

--- mon-aide-cyber-api/src/api/routesAPI.ts
+++ mon-aide-cyber-api/src/api/routesAPI.ts
@@ -1,29 +1,11 @@
-import express, { Request, Response, Router } from "express";
+import { routesAPIDiagnostic } from "./routesAPIDiagnostic";
+import express, { Router } from "express";
 import { ConfigurationServeur } from "../serveur";
-import * as crypto from "crypto";
-import { ServiceDiagnostic } from "../diagnostic/ServiceDiagnostic";
-import { representeLeDiagnosticPourLeClient } from "./representateurs/representateurDiagnostic";
-import { EntrepotsMemoire } from "../infrastructure/entrepots/memoire/Entrepots";
 
 const routesAPI = (configuration: ConfigurationServeur) => {
   const routes: Router = express.Router();
 
-  routes.get("/diagnostic/:id", (_req: Request, res: Response) => {
-    const { id } = _req.params;
-    new ServiceDiagnostic(
-      configuration.adaptateurReferentiel,
-      new EntrepotsMemoire(),
-    )
-      .diagnostic(id as crypto.UUID)
-      .then((diagnostic) =>
-        res.json(
-          representeLeDiagnosticPourLeClient(
-            diagnostic,
-            configuration.adaptateurTranscripteurDonnees.transcripteur(),
-          ),
-        ),
-      );
-  });
+  routes.use("/diagnostic", routesAPIDiagnostic(configuration));
 
   return routes;
 };
