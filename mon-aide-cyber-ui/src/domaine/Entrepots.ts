417fa8fbda2a7b8d7c716fc6b1612f4c417bda86
Bertrand Bougon <bertrandbougon@gmail.com>
2023-07-10 10:52:01 +0200

    [REMANIEMENT] Abstrais une méthode de la classe APIEntrepotDiagnostique

--- mon-aide-cyber-ui/src/domaine/Entrepots.ts
+++ mon-aide-cyber-ui/src/domaine/Entrepots.ts
@@ -1,6 +1,7 @@
 import { EntrepotDiagnostique } from "./diagnostique/Diagnostique.ts";
+import { Aggregat } from "./Aggregat.ts";
 
-export interface Entrepot<T> {
+export interface Entrepot<T extends Aggregat> {
   lis(identifiant: string): Promise<T>;
 }
 
