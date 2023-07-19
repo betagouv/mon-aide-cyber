c9a620db114f0af97509c18fd32a72aeb2d84b61
Bertrand Bougon <bertrandbougon@gmail.com>
2023-07-19 15:31:23 -0300

    [DIAGNOSTIC] Envoie la réponse de l’utilisateur vers l’API

--- mon-aide-cyber-ui/src/domaine/diagnostic/reducteurReponse.ts
+++ mon-aide-cyber-ui/src/domaine/diagnostic/reducteurReponse.ts
@@ -2,22 +2,37 @@ import { ReponseDonnee } from "./Referentiel.ts";
 
 enum TypeActionReponse {
   REPONSE_DONNEE = "REPONSE_DONNEE",
+  REPONSE_CHANGEE = "REPONSE_CHANGEE",
 }
 
 type EtatReponse = {
   reponseDonnee?: ReponseDonnee | undefined;
 };
 
-type ActionReponse = {
-  reponseDonnee: ReponseDonnee;
-  type: TypeActionReponse.REPONSE_DONNEE;
-};
+type ActionReponse =
+  | {
+      reponseDonnee: ReponseDonnee;
+      type: TypeActionReponse.REPONSE_DONNEE;
+    }
+  | {
+      reponse: string;
+      type: TypeActionReponse.REPONSE_CHANGEE;
+    };
 export const reducteurReponse = (
   etat: EtatReponse,
   action: ActionReponse,
 ): EtatReponse => {
   switch (action.type) {
+    case TypeActionReponse.REPONSE_CHANGEE:
+      return { ...etat, reponseDonnee: { valeur: action.reponse } };
     case TypeActionReponse.REPONSE_DONNEE:
       return { ...etat, reponseDonnee: action.reponseDonnee };
   }
+};
+
+export const reponseChangee = (reponse: string): ActionReponse => {
+  return {
+    reponse: reponse,
+    type: TypeActionReponse.REPONSE_CHANGEE,
+  };
 };
---
+++ mon-aide-cyber-ui/test/domaine/diagnostic/reducteurReponse.spec.ts
@@ -0,0 +1,21 @@
+import { describe, it } from "vitest";
+import {
+  reducteurReponse,
+  reponseChangee,
+} from "../../../src/domaine/diagnostic/reducteurReponse";
+import { uneReponsePossible } from "../../constructeurs/constructeurReponsePossible";
+
+describe("Le réducteur de réponse", () => {
+  it("change la réponse donnée", () => {
+    const nouvelleReponse = uneReponsePossible().construis();
+
+    const etatReponse = reducteurReponse(
+      { reponseDonnee: undefined },
+      reponseChangee(nouvelleReponse.identifiant),
+    );
+
+    expect(etatReponse.reponseDonnee).toMatchObject({
+      valeur: nouvelleReponse.identifiant,
+    });
+  });
+});
