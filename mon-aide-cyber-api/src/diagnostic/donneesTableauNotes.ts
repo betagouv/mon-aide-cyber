198efd57159240e8cef4ac7237073841898c10d5
Bertrand Bougon <bertrandbougon@gmail.com>
2023-09-12 12:31:39 +0200

    [REMANIEMENT] Redéfinis la structure du modèle des notes du diagnostic

--- mon-aide-cyber-api/src/diagnostic/donneesTableauNotes.ts
+++ mon-aide-cyber-api/src/diagnostic/donneesTableauNotes.ts
@@ -1,28 +1,40 @@
 import { TableauDeNotes } from "./TableauDeNotes";
 
 const tableauDeNotes: TableauDeNotes = {
-  "gouvernance-infos-et-processus-a-proteger-nsp": null,
-  "gouvernance-infos-et-processus-a-proteger-non": 0,
-  "gouvernance-infos-et-processus-a-proteger-oui-idee-generale": 1.5,
-  "gouvernance-infos-et-processus-a-proteger-oui-precise": 3,
-  "gouvernance-schema-nsp": null,
-  "gouvernance-schema-non-pas-a-jour": 0,
-  "gouvernance-schema-oui-macro": 2,
-  "gouvernance-schema-oui-detaille": 3,
-  "gouvernance-schema-si-industriel-a-jour-nsp": null,
-  "gouvernance-schema-si-industriel-a-jour-non": 0,
-  "gouvernance-schema-si-industriel-a-jour-oui-partiel": 2,
-  "gouvernance-schema-si-industriel-a-jour-oui-detaille": 3,
-  "gouvernance-connaissance-rgpd-nsp": null,
-  "gouvernance-connaissance-rgpd-non": 0,
-  "gouvernance-moyen-budgetaire-cyber-securite-nsp": null,
-  "gouvernance-moyen-budgetaire-cyber-securite-non": 0,
-  "gouvernance-moyen-budgetaire-cyber-securite-oui-ponctuels": 1.5,
-  "gouvernance-moyen-budgetaire-cyber-securite-oui-dedie": 3,
-  "gouvernance-exigence-cyber-securite-presta-nsp": null,
-  "gouvernance-exigence-cyber-securite-presta-non": 0,
-  "gouvernance-exigence-cyber-securite-presta-oui-formalisee": 2,
-  "gouvernance-exigence-cyber-securite-presta-oui-fixee": 3,
+  "gouvernance-infos-et-processus-a-proteger": {
+    "gouvernance-infos-et-processus-a-proteger-nsp": null,
+    "gouvernance-infos-et-processus-a-proteger-non": 0,
+    "gouvernance-infos-et-processus-a-proteger-oui-idee-generale": 1.5,
+    "gouvernance-infos-et-processus-a-proteger-oui-precise": 3,
+  },
+  "gouvernance-schema-si-a-jour": {
+    "gouvernance-schema-nsp": null,
+    "gouvernance-schema-non-pas-a-jour": 0,
+    "gouvernance-schema-oui-macro": 2,
+    "gouvernance-schema-oui-detaille": 3,
+  },
+  "gouvernance-schema-si-industriel-a-jour": {
+    "gouvernance-schema-si-industriel-a-jour-nsp": null,
+    "gouvernance-schema-si-industriel-a-jour-non": 0,
+    "gouvernance-schema-si-industriel-a-jour-oui-partiel": 2,
+    "gouvernance-schema-si-industriel-a-jour-oui-detaille": 3,
+  },
+  "gouvernance-connaissance-rgpd": {
+    "gouvernance-connaissance-rgpd-nsp": null,
+    "gouvernance-connaissance-rgpd-non": 0,
+  },
+  "gouvernance-moyen-budgetaire-cyber-securite": {
+    "gouvernance-moyen-budgetaire-cyber-securite-nsp": null,
+    "gouvernance-moyen-budgetaire-cyber-securite-non": 0,
+    "gouvernance-moyen-budgetaire-cyber-securite-oui-ponctuels": 1.5,
+    "gouvernance-moyen-budgetaire-cyber-securite-oui-dedie": 3,
+  },
+  "gouvernance-exigence-cyber-securite-presta": {
+    "gouvernance-exigence-cyber-securite-presta-nsp": null,
+    "gouvernance-exigence-cyber-securite-presta-non": 0,
+    "gouvernance-exigence-cyber-securite-presta-oui-formalisee": 2,
+    "gouvernance-exigence-cyber-securite-presta-oui-fixee": 3,
+  },
 };
 
 export { tableauDeNotes };
