4628214ad2a03c788aa3a5e6520700321278a490
Bertrand Bougon <bertrandbougon@gmail.com>
2023-09-20 23:12:24 +0200

    [DIAGNOSTIC][RECOMMANDATIONS] Ajout des recommandations liées à la sécurité des accès

--- mon-aide-cyber-api/src/diagnostic/donneesTableauNotes.ts
+++ mon-aide-cyber-api/src/diagnostic/donneesTableauNotes.ts
@@ -52,6 +52,78 @@ const tableauDeNotes: TableauDeNotes = {
     "gouvernance-exigence-cyber-securite-presta-si-industriel-oui-formalisee": 2,
     "gouvernance-exigence-cyber-securite-presta-si-industriel-oui-fixee": 3,
   },
+  "acces-outil-gestion-des-comptes": {
+    "acces-outil-gestion-des-comptes-nsp": null,
+    "acces-outil-gestion-des-comptes-oui": 3,
+    "acces-outil-gestion-des-comptes-non": 0,
+  },
+  "acces-liste-compte-utilisateurs": {
+    "acces-liste-compte-utilisateurs-nsp": null,
+    "acces-liste-compte-utilisateurs-non": 0,
+    "acces-liste-compte-utilisateurs-revue-reguliere": 2,
+    "acces-liste-compte-utilisateurs-revue-en-continu": 3,
+  },
+  "acces-droits-acces-utilisateurs-limites": {
+    "acces-droits-acces-utilisateurs-limites-nsp": null,
+    "acces-droits-acces-utilisateurs-limites-non": 0,
+    "acces-droits-acces-utilisateurs-limites-restrictions-ponctuelles": 1,
+    "acces-droits-acces-utilisateurs-limites-restrictions-limitees": 3,
+  },
+  "acces-administrateurs-informatiques-suivie-et-limitee": {
+    "acces-administrateurs-informatiques-suivie-et-limitee-nsp": null,
+    "acces-administrateurs-informatiques-suivie-et-limitee-non": 0,
+    "acces-administrateurs-informatiques-suivie-et-limitee-revue-reguliere": 2,
+    "acces-administrateurs-informatiques-suivie-et-limitee-revue-continue": 3,
+  },
+  "acces-utilisation-comptes-administrateurs-droits-limitee": {
+    "acces-utilisation-comptes-administrateurs-droits-limitee-nsp": null,
+    "acces-utilisation-comptes-administrateurs-droits-limitee-non": 0,
+    "acces-utilisation-comptes-administrateurs-droits-quelques-restrictions": 2,
+    "acces-utilisation-comptes-administrateurs-droits-justifies": 3,
+  },
+  "acces-utilisateurs-administrateurs-poste": {
+    "acces-utilisateurs-administrateurs-poste-nsp": null,
+    "acces-utilisateurs-administrateurs-poste-oui": 0,
+    "acces-utilisateurs-administrateurs-poste-suppression-privilege-en-cours": 1,
+    "acces-utilisateurs-administrateurs-poste-non-exceptions-justifiees": 3,
+  },
+  "acces-mesures-securite-robustesse-mdp": {
+    "acces-mesures-securite-robustesse-mdp-nsp": null,
+    "acces-mesures-securite-robustesse-mdp-non": 0,
+    "acces-mesures-securite-robustesse-mdp-utilisateurs-sensibilises": 1,
+    "acces-mesures-securite-robustesse-mdp-contraintes-par-defaut": 3,
+  },
+  "acces-utilisateurs-donnees-sensibles-mesures-securite-additionnelles": {
+    "acces-utilisateurs-donnees-sensibles-mesures-securite-additionnelles-nsp":
+      null,
+    "acces-utilisateurs-donnees-sensibles-mesures-securite-additionnelles-non": 0,
+    "acces-utilisateurs-donnees-sensibles-mesures-securite-additionnelles-oui-mesures-authentification-renforcees": 2,
+    "acces-utilisateurs-donnees-sensibles-mesures-securite-additionnelles-oui-mesures-authentification-renforcees-et-donnees-chiffrees": 3,
+  },
+  "acces-teletravail-acces-distants-mesures-particulieres": {
+    "acces-teletravail-acces-distants-mesures-particulieres-nsp": null,
+    "acces-teletravail-acces-distants-mesures-particulieres-non": 0,
+    "acces-teletravail-acces-distants-mesures-particulieres-mfa": 2,
+    "acces-teletravail-acces-distants-mesures-particulieres-vpn": 3,
+  },
+  "acces-si-industriel-teletravail-acces-distants-mesures-particulieres": {
+    "acces-si-industriel-teletravail-acces-distants-mesures-particulieres-nsp":
+      null,
+    "acces-si-industriel-teletravail-acces-distants-mesures-particulieres-non": 0,
+    "acces-si-industriel-teletravail-acces-distants-mesures-particulieres-mfa": 2,
+    "acces-si-industriel-teletravail-acces-distants-mesures-particulieres-vpn": 3,
+  },
+  "acces-entite-dispose-plusieurs-sites-geographiques": {
+    "acces-entite-dispose-plusieurs-sites-geographiques-nsp": null,
+    "acces-entite-dispose-plusieurs-sites-geographiques-non": 0,
+    "acces-entite-dispose-plusieurs-sites-geographiques-oui": 3,
+  },
+  "acces-administrateurs-si-mesures-specifiques": {
+    "acces-administrateurs-si-mesures-specifiques-nsp": null,
+    "acces-administrateurs-si-mesures-specifiques-non": 0,
+    "acces-administrateurs-si-mesures-specifiques-oui-mesures-authentification-renforcees": 1.5,
+    "acces-administrateurs-si-mesures-specifiques-oui-mesures-authentification-renforcees-postes-dedies-administration": 3,
+  },
 };
 
 export { tableauDeNotes };
