592ef30c867020ae93313e0a193c9068c332af75
Bertrand Bougon <bertrandbougon@gmail.com>
2023-09-27 12:46:25 +0200

    [DSFR] Utilise les boutons de la librairie
    - supprime également le fichier fonts.css

--- mon-aide-cyber-ui/src/composants/diagnostic/ComposantLancerDiagnostic.tsx
+++ mon-aide-cyber-ui/src/composants/diagnostic/ComposantLancerDiagnostic.tsx
@@ -2,6 +2,7 @@ import { useCallback, useContext } from "react";
 import { FournisseurEntrepots } from "../../fournisseurs/FournisseurEntrepot.ts";
 import { useErrorBoundary } from "react-error-boundary";
 import { useNavigate } from "react-router-dom";
+import { Button } from "@codegouvfr/react-dsfr/Button";
 
 export const ComposantLancerDiagnostic = () => {
   const entrepots = useContext(FournisseurEntrepots);
@@ -15,5 +16,5 @@ export const ComposantLancerDiagnostic = () => {
       .then((lien) => navigate(lien.route()))
       .catch((erreur) => showBoundary(erreur));
   }, [entrepots, showBoundary, navigate]);
-  return <button onClick={lancerDiagnostic}>Lancer un diagnostic</button>;
+  return <Button onClick={lancerDiagnostic}>Lancer un diagnostic</Button>;
 };
