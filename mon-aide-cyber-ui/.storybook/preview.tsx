d53187ab6a33e73b5ea6021c2a59eef1f29b3923
Bertrand Bougon <bertrandbougon@gmail.com>
2023-09-28 17:16:35 +0200

    [DSFR] Intègre le dsfr à storybook

--- mon-aide-cyber-ui/.storybook/preview.tsx
+++ mon-aide-cyber-ui/.storybook/preview.tsx
@@ -1,7 +1,20 @@
 import type { Preview } from "@storybook/react";
 import "../src/assets/styles/index.scss";
+import "../public/dsfr/utility/icons/icons.min.css";
+import "../public/dsfr/dsfr/dsfr.min.css";
 
 const preview: Preview = {
+  decorators: [
+    (Story) => (
+      <main role="main">
+        <div className="fr-container">
+          <div className="fr-grid-row">
+            <Story />
+          </div>
+        </div>
+      </main>
+    ),
+  ],
   parameters: {
     actions: { argTypesRegex: "^on[A-Z].*" },
     controls: {
