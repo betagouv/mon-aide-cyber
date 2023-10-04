5590801993f7b594a31a8188d1d475024e79b1f7
Bertrand Bougon <bertrandbougon@gmail.com>
2023-10-04 18:03:14 +0200

    [DIAGNOSTIC)[REMANIEMENT] Modifie le modèle des notes en vue d'ajouter les recommandations suivant le barème
    - afin de découpler le sytème de récupération des recommandations
    - et que chaque question référencée dans le tableau de note expose les recommandations à fournir suivant le résultat

--- mon-aide-cyber-api/src/diagnostic/TableauDeNotes.ts
+++ mon-aide-cyber-api/src/diagnostic/TableauDeNotes.ts
@@ -8,8 +8,11 @@ type Bareme = {
 };
 export type Note = 0 | 0.5 | 1 | 1.5 | 2 | 2.5 | 3 | null | undefined;
 export type RegleDeCalcul = Operation & Bareme;
+type Notation = {
+  [identifiantReponse: string]: Note | RegleDeCalcul;
+};
 export type TableauDeNotes = {
   [identifiantQuestion: string]: {
-    [identifiantReponse: string]: Note | RegleDeCalcul;
+    notation: Notation;
   };
 };
--- mon-aide-cyber-api/test/constructeurs/constructeurTableauDeNotes.ts
+++ mon-aide-cyber-api/test/constructeurs/constructeurTableauDeNotes.ts
@@ -1,9 +1,27 @@
 import { ConstructeurDeTableau } from "./constructeur";
-import { TableauDeNotes } from "../../src/diagnostic/TableauDeNotes";
+import {
+  Note,
+  RegleDeCalcul,
+  TableauDeNotes,
+} from "../../src/diagnostic/TableauDeNotes";
 
 class ConstructeurDeTableauDeNotes extends ConstructeurDeTableau<TableauDeNotes> {
-  avecDesNotes(notes: TableauDeNotes[]): ConstructeurDeTableauDeNotes {
-    this.tableau.push(...notes);
+  avecDesNotes(
+    notes: {
+      [identifiantQuestion: string]: {
+        [identifiantReponse: string]: Note | RegleDeCalcul;
+      };
+    }[],
+  ): ConstructeurDeTableauDeNotes {
+    this.tableau.push(
+      ...Object.entries(notes).flatMap(([__, notes]) => {
+        return Object.entries(notes).map(
+          ([identifiantQuestion, noteReponse]) => ({
+            [identifiantQuestion]: { notation: noteReponse },
+          }),
+        );
+      }),
+    );
     return this;
   }
 }
