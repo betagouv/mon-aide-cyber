46de3fcba0cf03d3b2bdb92c467d210acb2200b3
Bertrand Bougon <bertrandbougon@gmail.com>
2023-10-09 15:54:26 +0200

    [DIAGNOSTIC][RECOMMANDATIONS] Genere les notes pour les questions à tiroir

--- mon-aide-cyber-api/src/diagnostic/MoteurDeNote.ts
+++ mon-aide-cyber-api/src/diagnostic/MoteurDeNote.ts
@@ -1,4 +1,4 @@
-import { Diagnostic } from "./Diagnostic";
+import { Diagnostic, QuestionDiagnostic } from "./Diagnostic";
 import { Note } from "./TableauDeNotes";
 
 export type NotesDiagnostic = {
@@ -11,17 +11,48 @@ export class MoteurDeNote {
       (reducteur, [thematique, questions]) => {
         const noteThematique = questions.questions
           .filter((q) => q.reponseDonnee.reponseUnique != null)
-          .map((q) => {
-            return {
-              identifiant: q.identifiant,
-              note: q.reponsesPossibles.find(
-                (rep) => rep.identifiant === q.reponseDonnee.reponseUnique,
-              )?.resultat?.note,
-            };
+          .flatMap((q) => {
+            if (q.reponseDonnee.reponsesMultiples.length > 0) {
+              return this.genereLesNotesPourReponsesMultiples(q);
+            }
+            return [
+              {
+                identifiant: q.identifiant,
+                note: q.reponsesPossibles.find(
+                  (rep) => rep.identifiant === q.reponseDonnee.reponseUnique,
+                )?.resultat?.note,
+              },
+            ];
           });
         return { ...reducteur, [thematique]: noteThematique };
       },
       {},
     );
   };
+
+  private static genereLesNotesPourReponsesMultiples(
+    question: QuestionDiagnostic,
+  ) {
+    return question.reponseDonnee.reponsesMultiples
+      .flatMap((reponsesMultiples) =>
+        question.reponsesPossibles
+          .flatMap(
+            (rep) =>
+              rep.questions?.filter(
+                (q) => q.identifiant === reponsesMultiples.identifiant,
+              ) || [],
+          )
+          .flatMap((questionATiroir) => {
+            return questionATiroir.reponsesPossibles
+              .filter((reponse) =>
+                reponsesMultiples.reponses.has(reponse.identifiant),
+              )
+              .flatMap((rep) => ({
+                identifiant: reponsesMultiples.identifiant,
+                note: rep.resultat?.note,
+              }));
+          }),
+      )
+      .flatMap((questionDiagnostic) => questionDiagnostic);
+  }
 }
