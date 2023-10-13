6c8c15a4ad459020186dce73ae14be80c05c7d9f
Bertrand Bougon <bertrandbougon@gmail.com>
2023-10-13 15:56:23 +0200

    [DIAGNOSTIC] Mets à jour la question sur la RGPD

---
+++ mon-aide-cyber-api/src/diagnostic/recommandations/recommandationsSensibilisation.ts
@@ -0,0 +1,61 @@
+export const recommandationsSensibilisation = {
+  'sensibilisation-actions-sensibilisation-menace-et-bonnes-pratiques': {
+    niveau1: {
+      titre: 'Mettre en œuvre des bonnes pratiques de sensibilisation.',
+      pourquoi:
+        'recommandations/sensibilisation/sensibilisation-actions-sensibilisation-menace-et-bonnes-pratiques-niveau1-pourquoi.pug',
+      comment:
+        'recommandations/sensibilisation/sensibilisation-actions-sensibilisation-menace-et-bonnes-pratiques-niveau1-comment.pug',
+    },
+    niveau2: {
+      titre:
+        "En complément des pratiques déjà en œuvre, mettre en place des mesures complémentaires de sécurisation de l'espace dédié au stockage des serveurs d'administration et des équipements réseau.",
+      pourquoi:
+        'recommandations/sensibilisation/sensibilisation-actions-sensibilisation-menace-et-bonnes-pratiques-niveau2-pourquoi.pug',
+      comment:
+        'recommandations/sensibilisation/sensibilisation-actions-sensibilisation-menace-et-bonnes-pratiques-niveau2-comment.pug',
+    },
+    priorisation: 13,
+  },
+  'sensibilisation-risque-espionnage-industriel-r-et-d': {
+    niveau1: {
+      titre: 'Mettre en œuvre des bonnes pratiques de sensibilisation.',
+      pourquoi:
+        'recommandations/sensibilisation/sensibilisation-risque-espionnage-industriel-r-et-d-niveau1-pourquoi.pug',
+      comment:
+        'recommandations/sensibilisation/sensibilisation-risque-espionnage-industriel-r-et-d-niveau1-comment.pug',
+    },
+    priorisation: 13,
+  },
+  'sensibilisation-collaborateurs-soumis-obligations-usages-securises': {
+    niveau1: {
+      titre:
+        "Établir une charte informatique répertoriant les moyens informatiques mis à disposition, clarifiant la gestion des terminaux personnels et rappelant à minima les exigences de cybersécurité liées à la gestion des comptes d'accès, des mots de passe, des données à protéger en priorités, de l'utilisation de la messagerie et des ressources cloud.",
+      pourquoi:
+        'recommandations/sensibilisation/sensibilisation-collaborateurs-soumis-obligations-usages-securises-niveau1-pourquoi.pug',
+      comment:
+        'recommandations/sensibilisation/sensibilisation-collaborateurs-soumis-obligations-usages-securises-niveau1-comment.pug',
+    },
+    niveau2: {
+      titre:
+        "S'assurer que la charte informatique est annexée au contrat de travail et est signée par les salariés.",
+      pourquoi:
+        'recommandations/sensibilisation/sensibilisation-collaborateurs-soumis-obligations-usages-securises-niveau2-pourquoi.pug',
+      comment:
+        'recommandations/sensibilisation/sensibilisation-collaborateurs-soumis-obligations-usages-securises-niveau2-comment.pug',
+    },
+    priorisation: 23,
+  },
+  'sensibilisation-declaration-incidents-encouragee': {
+    niveau1: {
+      titre:
+        'Encourager régulièrement (2 fois par an à minima) la déclaration d’incident auprès de vos agents en rappelant les évènements "signaux faibles" devant être signalés ainsi que le contact à alerter.' +
+        'Formaliser et diffuser une fiche réflexe dédiée aux utilisateurs.',
+      pourquoi:
+        'recommandations/sensibilisation/sensibilisation-declaration-incidents-encouragee-niveau1-pourquoi.pug',
+      comment:
+        'recommandations/sensibilisation/sensibilisation-declaration-incidents-encouragee-niveau1-comment.pug',
+    },
+    priorisation: 16,
+  },
+};
