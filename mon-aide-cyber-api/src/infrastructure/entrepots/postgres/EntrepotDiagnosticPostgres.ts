b86c40c3b93565d04218eff75d6d13ce25ab2421
Bertrand Bougon <bertrandbougon@gmail.com>
2023-10-17 16:22:38 +0200

    [REMANIEMENT] Enlève le couplage au niveau de l'entrepot Postgres avec Diagnostic

--- mon-aide-cyber-api/src/infrastructure/entrepots/postgres/EntrepotDiagnosticPostgres.ts
+++ mon-aide-cyber-api/src/infrastructure/entrepots/postgres/EntrepotDiagnosticPostgres.ts
@@ -6,33 +6,40 @@ import {
   ReponseDonnee,
   ReponsesMultiples,
   Thematique,
-} from "../../../diagnostic/Diagnostic";
-import { DTO, EntrepotPostgres } from "./EntrepotPostgres";
+} from '../../../diagnostic/Diagnostic';
+import { DTO, EntrepotPostgres } from './EntrepotPostgres';
 
+export type DiagnosticDTO = DTO & {
+  donnees: object;
+};
 export class EntrepotDiagnosticPostgres
-  extends EntrepotPostgres<Diagnostic>
+  extends EntrepotPostgres<Diagnostic, DiagnosticDTO>
   implements EntrepotDiagnostic
 {
   typeAggregat(): string {
-    return "diagnostic";
+    return 'diagnostic';
   }
 
   protected nomTable(): string {
-    return "diagnostics";
+    return 'diagnostics';
   }
 
-  protected deEntiteADTO<D = DiagnosticDTO>(entite: Diagnostic): D {
-    const referentiel: { [thematique: Thematique]: QuestionsThematiqueDTO } =
-      this.transcris(
-        entite,
-        <E = Set<string>, S = string[]>(reponses: E) =>
-          Array.from(reponses as Set<string>) as S,
-      );
-    return { ...entite, referentiel } as D;
+  protected deEntiteADTO(entite: Diagnostic): DiagnosticDTO {
+    const referentiel: {
+      [thematique: Thematique]: RepresentationQuestionsThematique;
+    } = this.transcris(
+      entite,
+      <E = Set<string>, S = string[]>(reponses: E) =>
+        Array.from(reponses as Set<string>) as S,
+    );
+    return {
+      id: entite.identifiant,
+      donnees: { ...entite, referentiel },
+    } as DiagnosticDTO;
   }
 
-  protected deDTOAEntite<D extends DTO = DiagnosticDTO>(dto: D): Diagnostic {
-    const diagnosticDTO = dto as unknown as DiagnosticDTO;
+  protected deDTOAEntite(dto: DiagnosticDTO): Diagnostic {
+    const diagnosticDTO = dto.donnees as RepresentationDiagnostic;
     const referentiel = this.transcris(
       diagnosticDTO,
       <E = string[], S = Set<string>>(reponses: E) =>
@@ -41,8 +48,12 @@ export class EntrepotDiagnosticPostgres
     return { ...diagnosticDTO, referentiel: referentiel } as Diagnostic;
   }
 
+  protected champsAMettreAJour(entiteDTO: DiagnosticDTO): { donnees: object } {
+    return { donnees: entiteDTO.donnees };
+  }
+
   private transcris(
-    entite: Diagnostic | DiagnosticDTO,
+    entite: Diagnostic | RepresentationDiagnostic,
     transformeReponsesMultiples: <E, S>(reponses: E) => S,
   ) {
     return Object.entries(entite.referentiel).reduce(
@@ -50,7 +61,9 @@ export class EntrepotDiagnosticPostgres
         ...reducteur,
         [thematique as Thematique]: {
           questions: questions.questions.map(
-            (question: QuestionDiagnostic | QuestionDiagnosticDTO) => ({
+            (
+              question: QuestionDiagnostic | RepresentationQuestionDiagnostic,
+            ) => ({
               ...question,
               reponseDonnee: {
                 ...question.reponseDonnee,
@@ -70,22 +83,27 @@ export class EntrepotDiagnosticPostgres
   }
 }
 
-type ReponsesMultiplesDTO = Omit<ReponsesMultiples, "reponses"> & {
+type RepresentationReponsesMultiples = Omit<ReponsesMultiples, 'reponses'> & {
   reponses: string[];
 };
-type ReponseDonneeDTO = Omit<ReponseDonnee, "reponsesMultiples"> & {
-  reponsesMultiples: ReponsesMultiplesDTO[];
+type ReponseDonneeDTO = Omit<ReponseDonnee, 'reponsesMultiples'> & {
+  reponsesMultiples: RepresentationReponsesMultiples[];
 };
-type QuestionDiagnosticDTO = Omit<QuestionDiagnostic, "reponseDonnee"> & {
+type RepresentationQuestionDiagnostic = Omit<
+  QuestionDiagnostic,
+  'reponseDonnee'
+> & {
   reponseDonnee: ReponseDonneeDTO;
 };
-type QuestionsThematiqueDTO = Omit<QuestionsThematique, "questions"> & {
-  questions: QuestionDiagnosticDTO[];
+type RepresentationQuestionsThematique = Omit<
+  QuestionsThematique,
+  'questions'
+> & {
+  questions: RepresentationQuestionDiagnostic[];
 };
-type ReferentielDiagnosticDTO = {
-  [thematique: Thematique]: QuestionsThematiqueDTO;
+type RepresentationReferentielDiagnostic = {
+  [thematique: Thematique]: RepresentationQuestionsThematique;
 };
-type DiagnosticDTO = DTO &
-  Omit<Diagnostic, "referentiel"> & {
-    referentiel: ReferentielDiagnosticDTO;
-  };
+type RepresentationDiagnostic = Omit<Diagnostic, 'referentiel'> & {
+  referentiel: RepresentationReferentielDiagnostic;
+};
