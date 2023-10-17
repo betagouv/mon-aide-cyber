a36deba4103e1152d80f23790ec9350dcfb786fc
Bertrand Bougon <bertrandbougon@gmail.com>
2023-10-17 16:22:38 +0200

    [REMANIEMENT] Enlève le couplage au niveau de l'entrepot Postgres avec Diagnostic

--- mon-aide-cyber-api/test/infrastructure/entrepots/postgres/EntrepotDiagnosticPostgres.spec.ts
+++ mon-aide-cyber-api/test/infrastructure/entrepots/postgres/EntrepotDiagnosticPostgres.spec.ts
@@ -8,46 +8,44 @@ import {
 } from '../../../constructeurs/constructeurReferentiel';
 import { EntrepotDiagnosticPostgres } from '../../../../src/infrastructure/entrepots/postgres/EntrepotDiagnosticPostgres';
 
-describe.runIf(process.env.URL_SERVEUR_BASE_DONNEES)(
-  'Entrepot Diagnostic Postgres',
-  () => {
-    afterEach(async () => {
-      await nettoieLaBaseDeDonnees();
-    });
-    it('persiste un diagnostic', async () => {
-      const diagnostic = unDiagnostic().construis();
+describe('Entrepot Diagnostic Postgres', () => {
+  afterEach(async () => {
+    await nettoieLaBaseDeDonnees();
+  });
+  it('persiste un diagnostic', async () => {
+    const diagnostic = unDiagnostic().construis();
 
-      await new EntrepotDiagnosticPostgres().persiste(diagnostic);
+    await new EntrepotDiagnosticPostgres().persiste(diagnostic);
 
-      const entrepotDiagnosticPostgresLecture =
-        await new EntrepotDiagnosticPostgres();
-      expect(
-        await entrepotDiagnosticPostgresLecture.lis(diagnostic.identifiant),
-      ).toStrictEqual(diagnostic);
-    });
+    const entrepotDiagnosticPostgresLecture =
+      await new EntrepotDiagnosticPostgres();
+    expect(
+      await entrepotDiagnosticPostgresLecture.lis(diagnostic.identifiant),
+    ).toStrictEqual(diagnostic);
+  });
 
-    it('persiste un diagnostic avec les réponses données', async () => {
-      const diagnostic = unDiagnostic()
-        .avecUnReferentiel(
-          unReferentiel()
-            .sansThematique()
-            .ajouteUneThematique('question-set', [
-              uneQuestion()
-                .aChoixMultiple('Sauvegardes-tu les set?', [
-                  uneReponsePossible().avecLibelle('Oui').construis(),
-                  uneReponsePossible().avecLibelle('Un peu').construis(),
-                  uneReponsePossible().avecLibelle('Beaucoup').construis(),
-                ])
-                .construis(),
-            ])
-            .construis(),
-        )
-        .avecLesReponsesDonnees('question-set', [
-          { 'sauvegardestu-les-set': ['un-peu', 'beaucoup'] },
-        ])
-        .construis();
+  it('persiste un diagnostic avec les réponses données', async () => {
+    const diagnostic = unDiagnostic()
+      .avecUnReferentiel(
+        unReferentiel()
+          .sansThematique()
+          .ajouteUneThematique('question-set', [
+            uneQuestion()
+              .aChoixMultiple('Sauvegardes-tu les set?', [
+                uneReponsePossible().avecLibelle('Oui').construis(),
+                uneReponsePossible().avecLibelle('Un peu').construis(),
+                uneReponsePossible().avecLibelle('Beaucoup').construis(),
+              ])
+              .construis(),
+          ])
+          .construis(),
+      )
+      .avecLesReponsesDonnees('question-set', [
+        { 'sauvegardestu-les-set': ['un-peu', 'beaucoup'] },
+      ])
+      .construis();
 
-      await new EntrepotDiagnosticPostgres().persiste(diagnostic);
+    await new EntrepotDiagnosticPostgres().persiste(diagnostic);
 
     const entrepotDiagnosticPostgresLecture =
       await new EntrepotDiagnosticPostgres();
