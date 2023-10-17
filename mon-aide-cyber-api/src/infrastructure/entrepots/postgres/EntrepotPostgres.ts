1c07389cab4987406a379dd483e28f26da958bd5
Bertrand Bougon <bertrandbougon@gmail.com>
2023-10-17 16:22:38 +0200

    [REMANIEMENT] Enlève le couplage au niveau de l'entrepot Postgres avec Diagnostic

--- mon-aide-cyber-api/src/infrastructure/entrepots/postgres/EntrepotPostgres.ts
+++ mon-aide-cyber-api/src/infrastructure/entrepots/postgres/EntrepotPostgres.ts
@@ -1,10 +1,12 @@
-import crypto from "crypto";
-import { Aggregat, AggregatNonTrouve } from '../../../domaine/Aggregat'
-import { Entrepot } from "../../../domaine/Entrepot";
-import { knex, Knex } from "knex";
-import knexfile from "./knexfile";
+import { Aggregat, AggregatNonTrouve } from '../../../domaine/Aggregat';
+import { Entrepot } from '../../../domaine/Entrepot';
+import { knex, Knex } from 'knex';
+import knexfile from './knexfile';
+import crypto from 'crypto';
 
-export abstract class EntrepotPostgres<T extends Aggregat>
+export type DTO = { id: crypto.UUID };
+
+export abstract class EntrepotPostgres<T extends Aggregat, D extends DTO>
   implements Entrepot<T>
 {
   private knex: Knex;
@@ -16,11 +18,9 @@ export abstract class EntrepotPostgres<T extends Aggregat>
   lis(identifiant: string): Promise<T> {
     return this.knex
       .from(this.nomTable())
-      .where("id", identifiant)
+      .where('id', identifiant)
       .first()
-      .then((ligne: { id: crypto.UUID; donnees: object }) =>
-        this.deDTOAEntite(ligne.donnees as DTO),
-      )
+      .then((ligne: D) => this.deDTOAEntite(ligne))
       .catch(() => {
         throw new AggregatNonTrouve(this.typeAggregat());
       });
@@ -30,17 +30,14 @@ export abstract class EntrepotPostgres<T extends Aggregat>
     const entiteDTO = this.deEntiteADTO(entite);
     const entiteExistante = await this.knex
       .from(this.nomTable())
-      .where("id", entiteDTO.identifiant)
+      .where('id', entiteDTO.id)
       .first();
     if (!entiteExistante) {
-      await this.knex(this.nomTable()).insert({
-        id: entiteDTO.identifiant,
-        donnees: entiteDTO,
-      });
+      await this.knex(this.nomTable()).insert(entiteDTO);
     } else {
       await this.knex(this.nomTable())
-        .where("id", entiteDTO.identifiant)
-        .update({ donnees: entiteDTO });
+        .where('id', entiteDTO.id)
+        .update(this.champsAMettreAJour(entiteDTO));
     }
   }
 
@@ -50,14 +47,14 @@ export abstract class EntrepotPostgres<T extends Aggregat>
   }
 
   typeAggregat(): string {
-    throw new Error("Non implémenté");
+    throw new Error('Non implémenté');
   }
+
+  protected abstract champsAMettreAJour(entiteDTO: D): Partial<D>;
 
   protected abstract nomTable(): string;
 
-  protected abstract deEntiteADTO<D extends DTO>(entite: T): D;
+  protected abstract deEntiteADTO(entite: T): D;
 
-  protected abstract deDTOAEntite<D extends DTO>(dto: D): T;
+  protected abstract deDTOAEntite(dto: D): T;
 }
-
-export type DTO = { identifiant: crypto.UUID };
