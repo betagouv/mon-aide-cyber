e4b4f2e95483d2989f876f345c8f67eb2f91366c
tomi <thomas.bennett@caminu.fr>
2023-10-30 14:51:17 +0100

    [DESIGN][ACCUEIL] ajoute une section 'devenir aidant' avec les étapes à suivre et un lien vers la charte aidant

--- mon-aide-cyber-ui/src/Accueil.tsx
+++ mon-aide-cyber-ui/src/Accueil.tsx
@@ -1,4 +1,5 @@
 import sceneUnAidantEtUnAideFaisantUnDiagnostic from '../public/images/scene-un-aidant-et-un-aidant-faisant-un-diagnostic.svg';
+import illustrationSectionDevenirAidant from '../public/images/devenir-aidant.svg';
 import { ComposantLancerDiagnostic } from './composants/diagnostic/ComposantLancerDiagnostic.tsx';
 
 function Accueil() {
@@ -9,7 +10,7 @@ function Accueil() {
           <div className="corps fr-col-offset-2 fr-col-5">
             <h1>MonAideCyber</h1>
             <p>
-              Accompagnez les PME et collectivités locales à gagner en maturité
+              Accompagnez les entreprises et collectivités à gagner en maturité
               sur leurs pratiques cyber en les aidant à identifier les
               faiblesses de leur infrastructure.
             </p>
@@ -28,7 +29,7 @@ function Accueil() {
       <br />
       <br />
       <div className="fr-container--fluid">
-        <div className="presentation-details fr-grid-row fr-grid-row--gutters">
+        <div className="section presentation-details fr-grid-row fr-grid-row--gutters">
           <div className="fr-col-offset-2 fr-col-10 titre">
             <h2>
               MonAideCyber est en construction... Mais est déjà capable de vous
@@ -65,6 +66,44 @@ function Accueil() {
               avant, tandis que les autres sont rassemblées en{' '}
               <strong>annexe</strong> du bilan.
             </p>
+          </div>
+        </div>
+      </div>
+      <br />
+      <br />
+      <div className="fr-container--fluid">
+        <div className="section devenir-aidant fr-grid-row fr-grid-row--gutters fr-grid-row--center fr-grid-row--middle">
+          <div className="fr-col-2">
+            <img
+              src={illustrationSectionDevenirAidant}
+              alt="illustration temporaire pour la section"
+            />
+          </div>
+          <div className="fr-container--fluid">
+            <div className="fr-grid-row fr-grid-row--gutters fr-grid-row--middle">
+              <div className="fr-col-12 titre">
+                <h2>Devenir aidant</h2>
+              </div>
+
+              <div className="fr-col-12">
+                <p>Les démarches à suivre pour commencer à accompagner.</p>
+                <ul>
+                  <li>Être bénévole et dans une posture bienveillante</li>
+                  <li>Assister à une formation</li>
+                  <li>Prendre en main l&apos;outil diagnostic</li>
+                  <li>
+                    Signer la{' '}
+                    <a href="/charte-aidant">
+                      charte aidant{' '}
+                      <span
+                        className="fr-icon-pen-nib-line"
+                        aria-hidden="true"
+                      ></span>
+                    </a>
+                  </li>
+                </ul>
+              </div>
+            </div>
           </div>
         </div>
       </div>
