defb2dfb1211824673477e6a91fefc5d4c08cbfd
Bertrand Bougon <bertrandbougon@gmail.com>
2023-11-23 10:10:32 +0100

    [AUTHENTIFICATION] Ajoute un entrepot aidant postgresql

--- mon-aide-cyber-api/src/infrastructure/entrepots/postgres/EntrepotsPostgres.ts
+++ mon-aide-cyber-api/src/infrastructure/entrepots/postgres/EntrepotsPostgres.ts
@@ -2,15 +2,20 @@ import { Entrepots } from '../../../domaine/Entrepots';
 import { EntrepotDiagnostic } from '../../../diagnostic/Diagnostic';
 import { EntrepotDiagnosticPostgres } from './EntrepotDiagnosticPostgres';
 import { EntrepotAidant } from '../../../authentification/Aidant';
+import { EntrepotAidantPostgres } from './EntrepotAidantPostgres';
+import { ServiceDeChiffrementChacha20 } from '../../securite/ServiceDeChiffrementChacha20';
 
 export class EntrepotsPostgres implements Entrepots {
   private readonly entrepotDiagnostic = new EntrepotDiagnosticPostgres();
+  private readonly entrepotAidant: EntrepotAidant = new EntrepotAidantPostgres(
+    new ServiceDeChiffrementChacha20(),
+  );
 
   diagnostic(): EntrepotDiagnostic {
     return this.entrepotDiagnostic;
   }
 
   aidants(): EntrepotAidant {
-    throw new Error('Non implémenté');
+    return this.entrepotAidant;
   }
 }
---
+++ mon-aide-cyber-api/test/infrastructure/securite/ServiceDeChiffrementChacha20.spec.ts
@@ -0,0 +1,29 @@
+import { describe, expect } from 'vitest';
+import { ServiceDeChiffrementChacha20 } from '../../../src/infrastructure/securite/ServiceDeChiffrementChacha20';
+
+describe('Service de chiffrement', () => {
+  it('est capable de chiffrer une chaine de caractère', () => {
+    const chaineChiffree = new ServiceDeChiffrementChacha20(
+      Buffer.from('ivlongueur12'),
+      Buffer.from('assoc-longueur16'),
+      'ma-clef-secrete-de-longueur-0032',
+    ).chiffre('contenu à chiffrer');
+
+    expect(chaineChiffree).toBe(
+      '69766c6f6e677565757231326173736f632d6c6f6e67756575723136edb9dabf70ccb254aa95be2682b4fe9ef399822cab7c76219230aee727cb907a0bb7ff',
+    );
+  });
+
+  it('est capable de déchiffrer une chaine de caractère', () => {
+    '69762d64652d6c6f6e67756575723136';
+    const chaineDechiffree = new ServiceDeChiffrementChacha20(
+      Buffer.from('ivlongueur12'),
+      Buffer.from('assoc-longueur16'),
+      'ma-clef-secrete-de-longueur-0032',
+    ).dechiffre(
+      '69766c6f6e677565757231326173736f632d6c6f6e67756575723136edb9dabf70ccb254aa95be2682b4fe9ef399822cab7c76219230aee727cb907a0bb7ff',
+    );
+
+    expect(chaineDechiffree).toBe('contenu à chiffrer');
+  });
+});
