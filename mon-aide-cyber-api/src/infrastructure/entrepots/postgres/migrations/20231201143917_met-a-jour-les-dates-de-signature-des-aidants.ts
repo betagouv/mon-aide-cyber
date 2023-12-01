28f94b87c480b46af8451b21d778f8106abd1fd9
Bertrand Bougon <bertrandbougon@gmail.com>
2023-12-01 15:50:51 +0100

    [AIDANT] Ajoute un script de migration pour mettre à jour les dates de signature des CGU et de la charte
    pour les aidants pour lesquels il n'y a pas de date

---
+++ mon-aide-cyber-api/src/infrastructure/entrepots/postgres/migrations/20231201143917_met-a-jour-les-dates-de-signature-des-aidants.ts
@@ -0,0 +1,37 @@
+import { Knex } from 'knex';
+import crypto from 'crypto';
+import { DonneesUtilisateur } from '../EntrepotAidantPostgres';
+
+export async function up(knex: Knex): Promise<void | number[]> {
+  return knex('utilisateurs').then(
+    (
+      lignes: {
+        id: crypto.UUID;
+        type: 'AIDANT' | 'REFERENT';
+        donnees: DonneesUtilisateur;
+      }[],
+    ) => {
+      const misesAJour = lignes
+        .filter(
+          (ligne) =>
+            !ligne.donnees.dateSignatureCGU ||
+            !ligne.donnees.dateSignatureCharte,
+        )
+        .map((ligne) => {
+          if (!ligne.donnees.dateSignatureCharte) {
+            ligne.donnees.dateSignatureCharte = '2023-11-30T12:00:00+01:00';
+          }
+          if (!ligne.donnees.dateSignatureCGU) {
+            ligne.donnees.dateSignatureCGU = '2023-11-30T12:00:00+01:00';
+          }
+          return knex('utilisateurs').where('id', ligne.id).update({
+            donnees: ligne.donnees,
+          });
+        });
+      return Promise.all(misesAJour);
+    },
+  );
+}
+
+// eslint-disable-next-line @typescript-eslint/no-empty-function
+export async function down(__: Knex): Promise<void> {}
