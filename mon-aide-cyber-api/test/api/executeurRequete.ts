0308366a941ef6b5050ee4f4a2852583b09e0443
Bertrand Bougon <bertrandbougon@gmail.com>
2023-12-01 16:18:44 +0100

    [REMANIEMENT] Nettoie l'exécuteur de Requête
    afin de supprimer le code mort

--- mon-aide-cyber-api/test/api/executeurRequete.ts
+++ mon-aide-cyber-api/test/api/executeurRequete.ts
@@ -6,15 +6,8 @@ export const executeRequete = (
   verbe: 'GET' | 'POST' | 'PATCH',
   chemin: string,
   port: number,
-  corps: object | undefined = undefined
+  corps: object | undefined = undefined,
 ): Promise<Response> => {
-  const requeteInit: RequestInit = {
-    method: verbe,
-    headers: { 'Content-Type': 'application/json' },
-  };
-  if (corps !== null) {
-    requeteInit['body'] = JSON.stringify(corps);
-  }
   return inject(app, {
     method: verbe,
     url: { pathname: chemin, port },
