a5c2baeabdda05933a21d54407e31cacfb04a587
Bertrand Bougon <bertrandbougon@gmail.com>
2023-12-07 10:52:30 +0100

    [INDICATEURS] Ajoute la notion de poids

--- mon-aide-cyber-api/src/diagnostic/MoteurDesIndicateurs.ts
+++ mon-aide-cyber-api/src/diagnostic/MoteurDesIndicateurs.ts
@@ -1,5 +1,10 @@
 import { ValeursDesReponsesAuDiagnostic } from './MoteurDeValeur';
 import { Indicateurs } from './Diagnostic';
+import { PoidsPossible, Valeur, ValeurPossible } from './Valeur';
+
+const estUnNombre = (
+  valeur: PoidsPossible | ValeurPossible,
+): valeur is number => !!valeur && typeof valeur === 'number';
 
 export class MoteurDesIndicateurs {
   static genereLesIndicateurs(
@@ -9,19 +14,43 @@ export class MoteurDesIndicateurs {
       (indicateurs, [thematique, valeurs]) => {
         indicateurs[thematique] = {
           moyennePonderee:
-            valeurs.reduce((valeurPrecedente, valeurCourante) => {
-              if (valeurCourante.valeur?.theorique) {
-                return valeurPrecedente + valeurCourante.valeur.theorique || 0;
-              }
-              return valeurPrecedente;
-            }, 0) /
-            valeurs.filter(
-              (valeur) => valeur && valeur.valeur && valeur.valeur.theorique,
-            ).length,
+            this.calculeLaSommeDesProduitsDesValeurs(valeurs) /
+            this.caluleLaSommeDesPoids(valeurs),
         };
         return indicateurs;
       },
       {} as unknown as Indicateurs,
     );
+  }
+
+  private static caluleLaSommeDesPoids(
+    valeurs: { identifiant: string; valeur: Valeur }[],
+  ) {
+    return valeurs.reduce((valeurPrecedente, valeurCourante) => {
+      const poids = valeurCourante.valeur?.poids;
+      if (estUnNombre(poids)) {
+        return valeurPrecedente + poids;
+      }
+      if (estUnNombre(valeurCourante.valeur?.theorique)) {
+        return valeurPrecedente + 1;
+      }
+      return valeurPrecedente;
+    }, 0);
+  }
+
+  private static calculeLaSommeDesProduitsDesValeurs(
+    valeurs: { identifiant: string; valeur: Valeur }[],
+  ) {
+    return valeurs.reduce((valeurPrecedente, valeurCourante) => {
+      const valeurTheorique = valeurCourante.valeur?.theorique;
+      const poids = valeurCourante.valeur?.poids;
+      if (estUnNombre(valeurTheorique) && estUnNombre(poids)) {
+        return valeurPrecedente + valeurTheorique * poids;
+      }
+      if (estUnNombre(valeurTheorique)) {
+        return valeurPrecedente + valeurTheorique;
+      }
+      return valeurPrecedente;
+    }, 0);
   }
 }
