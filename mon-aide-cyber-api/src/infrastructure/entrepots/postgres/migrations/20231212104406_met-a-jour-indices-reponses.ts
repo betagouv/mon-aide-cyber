9fa0b192fa5722ac60b36918d7d8938e8f8b3920
Bertrand Bougon <bertrandbougon@gmail.com>
2023-12-12 16:40:18 +0100

    [REMANIEMENT] Ajoute le script de migration des valeurs en indices

---
+++ mon-aide-cyber-api/src/infrastructure/entrepots/postgres/migrations/20231212104406_met-a-jour-indices-reponses.ts
@@ -0,0 +1,158 @@
+import { Knex } from 'knex';
+import {
+  QuestionATiroir,
+  Recommandation,
+  ReponsePossible,
+} from '../../../../diagnostic/Referentiel';
+import { Poids, Valeur } from '../../../../diagnostic/Indice';
+import {
+  QuestionDiagnostic,
+  RecommandationPriorisee,
+  Thematique,
+} from '../../../../diagnostic/Diagnostic';
+import crypto from 'crypto';
+import { TableauDeRecommandations } from '../../../../diagnostic/TableauDeRecommandations';
+
+type RepresentationReponsePossible = Omit<
+  ReponsePossible,
+  'resultat' | 'questions'
+> & {
+  resultat?: {
+    recommandations?: Recommandation[];
+    indice?: { valeur: Valeur; poids?: Poids };
+    valeur?: {
+      theorique: Valeur;
+      poids?: Poids;
+    };
+  };
+  questions?: RepresentationQuestionTiroir[];
+};
+type RepresentationQuestionTiroir = Omit<
+  QuestionATiroir,
+  'reponsesPossibles'
+> & {
+  reponsesPossibles: RepresentationReponsePossible[];
+};
+
+type RepresentationQuestionDiagnostic = Omit<
+  QuestionDiagnostic,
+  'reponsesPossibles'
+> & {
+  reponsesPossibles: RepresentationReponsePossible[];
+};
+type RepresentationQuestionsThematique = {
+  questions: RepresentationQuestionDiagnostic[];
+};
+
+type RepresentationReferentiel = {
+  [clef: Thematique]: RepresentationQuestionsThematique;
+};
+
+type RepresentationRecommandationPriorisee = Omit<
+  RecommandationPriorisee,
+  'valeurObtenue'
+> & {
+  noteObtenue?: Valeur;
+  valeurObtenue:
+    | { theorique?: Valeur; poids?: Poids; indice?: Valeur }
+    | Valeur;
+};
+
+type RepresentationRecommandations = {
+  recommandationsPrioritaires: RepresentationRecommandationPriorisee[];
+  autresRecommandations: RepresentationRecommandationPriorisee[];
+};
+
+type RepresentationRestitution = {
+  recommandations?: RepresentationRecommandations;
+};
+
+export async function up(knex: Knex): Promise<void> {
+  const metsAJour = (reponsePossible: RepresentationReponsePossible) => {
+    if (reponsePossible.resultat && reponsePossible.resultat.valeur) {
+      reponsePossible.resultat = {
+        indice: {
+          valeur: reponsePossible.resultat.valeur.theorique,
+          poids: reponsePossible.resultat.valeur.poids || 1,
+        },
+        ...(reponsePossible.resultat.recommandations && {
+          recommandations: reponsePossible.resultat.recommandations,
+        }),
+      };
+    }
+  };
+
+  const metsAJourLesRecommandations = (
+    recommandations: RepresentationRecommandationPriorisee[],
+  ) =>
+    recommandations.map((recommandation) => {
+      if (recommandation.valeurObtenue) {
+        const { valeurObtenue, ...reste } = recommandation;
+        recommandation = {
+          ...reste,
+          valeurObtenue: (
+            valeurObtenue as {
+              theorique?: Valeur;
+              poids?: Poids;
+              indice?: Valeur;
+            }
+          ).theorique,
+        };
+      }
+      return recommandation;
+    });
+
+  knex('diagnostics').then(
+    (
+      lignes: {
+        id: crypto.UUID;
+        donnees: {
+          dateCreation: Date;
+          dateDerniereModification: Date;
+          identifiant: crypto.UUID;
+          restitution?: RepresentationRestitution;
+          referentiel: RepresentationReferentiel;
+          tableauDesRecommandations: TableauDeRecommandations;
+        };
+      }[],
+    ) => {
+      const misesAJour = lignes.map((ligne) => {
+        Object.entries(ligne.donnees.referentiel)
+          .filter(([thematique]) => thematique !== 'contexte')
+          .forEach(([, questions]) => {
+            questions.questions.forEach((question) => {
+              question.reponsesPossibles.forEach((reponsePossible) => {
+                metsAJour(reponsePossible);
+                reponsePossible.questions?.forEach((question) => {
+                  question.reponsesPossibles.forEach((reponsePossible) =>
+                    metsAJour(reponsePossible),
+                  );
+                });
+              });
+            });
+          });
+        if (
+          ligne.donnees.restitution &&
+          ligne.donnees.restitution.recommandations
+        ) {
+          ligne.donnees.restitution.recommandations.recommandationsPrioritaires =
+            metsAJourLesRecommandations(
+              ligne.donnees.restitution.recommandations
+                .recommandationsPrioritaires,
+            );
+          ligne.donnees.restitution.recommandations.autresRecommandations =
+            metsAJourLesRecommandations(
+              ligne.donnees.restitution.recommandations.autresRecommandations,
+            );
+        }
+        return knex('diagnostics')
+          .where('id', ligne.id)
+          .update({ donnees: ligne.donnees });
+      });
+      return Promise.all(misesAJour);
+    },
+  );
+}
+
+// eslint-disable-next-line @typescript-eslint/no-empty-function
+export async function down(__: Knex): Promise<void> {}
