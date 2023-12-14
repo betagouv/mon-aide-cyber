a5f119f8de715ed0a22018713b8655e0ac696297
Bertrand Bougon <bertrandbougon@gmail.com>
2023-12-14 17:07:14 +0100

    [REMANIEMENT] Ajoute un script de migration pour supprimer le poids des réponses

---
+++ mon-aide-cyber-api/src/infrastructure/entrepots/postgres/migrations/20231214155415_supprime-le-poids-des-reponses.ts
@@ -0,0 +1,98 @@
+import { Knex } from 'knex';
+import crypto from 'crypto';
+import { TableauDeRecommandations } from '../../../../diagnostic/TableauDeRecommandations';
+import {
+  QuestionATiroir,
+  Recommandation,
+  ReponsePossible,
+} from '../../../../diagnostic/Referentiel';
+import { Poids, Valeur } from '../../../../diagnostic/Indice';
+import {
+  QuestionDiagnostic,
+  Thematique,
+} from '../../../../diagnostic/Diagnostic';
+
+type RepresentationReponsePossible = Omit<
+  ReponsePossible,
+  'resultat' | 'questions'
+> & {
+  resultat?: {
+    recommandations?: Recommandation[];
+    indice?: { valeur: Valeur; poids?: Poids };
+    valeur?: {
+      theorique: Valeur;
+      poids?: Poids;
+    };
+  };
+  questions?: RepresentationQuestionTiroir[];
+};
+type RepresentationQuestionTiroir = Omit<
+  QuestionATiroir,
+  'reponsesPossibles'
+> & {
+  reponsesPossibles: RepresentationReponsePossible[];
+};
+
+type RepresentationQuestionDiagnostic = Omit<
+  QuestionDiagnostic,
+  'reponsesPossibles'
+> & {
+  reponsesPossibles: RepresentationReponsePossible[];
+};
+type RepresentationQuestionsThematique = {
+  questions: RepresentationQuestionDiagnostic[];
+};
+
+type RepresentationReferentiel = {
+  [clef: Thematique]: RepresentationQuestionsThematique;
+};
+
+export async function up(knex: Knex): Promise<void> {
+  const metsAJour = (reponsePossible: RepresentationReponsePossible) => {
+    if (reponsePossible.resultat) {
+      reponsePossible.resultat = {
+        ...reponsePossible.resultat,
+        indice: { valeur: reponsePossible.resultat.indice?.valeur },
+      };
+    }
+  };
+
+  knex('diagnostics').then(
+    (
+      lignes: {
+        id: crypto.UUID;
+        donnees: {
+          dateCreation: Date;
+          dateDerniereModification: Date;
+          identifiant: crypto.UUID;
+          referentiel: RepresentationReferentiel;
+          tableauDesRecommandations: TableauDeRecommandations;
+        };
+      }[],
+    ) => {
+      const misesAJour = lignes.map((ligne) => {
+        Object.entries(ligne.donnees.referentiel)
+          .filter(([thematique]) => thematique !== 'contexte')
+          .forEach(([, questions]) => {
+            questions.questions.forEach((question) => {
+              question.reponsesPossibles.forEach((reponsePossible) => {
+                metsAJour(reponsePossible);
+                reponsePossible.questions?.forEach((question) => {
+                  question.reponsesPossibles.forEach((reponsePossible) =>
+                    metsAJour(reponsePossible),
+                  );
+                });
+              });
+            });
+          });
+        return knex('diagnostics')
+          .where('id', ligne.id)
+          .update({ donnees: ligne.donnees });
+      });
+      return Promise.all(misesAJour);
+    },
+  );
+}
+
+// eslint-disable-next-line @typescript-eslint/no-empty-function
+export async function down(__: Knex): Promise<void> {}
