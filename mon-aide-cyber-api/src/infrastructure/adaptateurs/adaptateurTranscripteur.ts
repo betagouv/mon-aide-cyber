29fcc282e073c379b4f4d4ea054393e85ae60ff7
Bertrand Bougon <bertrandbougon@gmail.com>
2023-12-18 17:09:02 +0100

    [REMANIEMENT] Renomme le libellé de la sécurité des accès en Sécurité des accès au système d'information

--- mon-aide-cyber-api/src/infrastructure/adaptateurs/adaptateurTranscripteur.ts
+++ mon-aide-cyber-api/src/infrastructure/adaptateurs/adaptateurTranscripteur.ts
@@ -21,15 +21,6 @@ export const adaptateurTranscripteur = () =>
               '/images/diagnostic/illustration_contexte.svg',
             libelle: 'Contexte',
             questions: [
-              // {
-              //   identifiant: "contexte-nature-organisation",
-              //   reponses: [
-              //     {
-              //       identifiant: "nature-organisation-autre",
-              //       type: { type: "saisieLibre", format: "texte" },
-              //     },
-              //   ],
-              // },
               {
                 identifiant: 'contexte-secteur-activite',
                 type: 'liste',
@@ -39,52 +30,26 @@ export const adaptateurTranscripteur = () =>
                 identifiant: 'contexte-departement-tom-siege-social',
                 type: 'liste',
               },
-              // {
-              //   identifiant: "contexte-usage-cloud",
-              //   reponses: [
-              //     {
-              //       identifiant: "usage-cloud-oui",
-              //       question: {
-              //         identifiant: "contexte-usage-cloud-oui-question-tiroir-usages",
-              //         type: "choixMultiple",
-              //         reponses: [
-              //           {
-              //             identifiant:
-              //               "contexte-usage-cloud-oui-question-tiroir-usages-autre",
-              //             type: { type: "saisieLibre", format: "texte" },
-              //           },
-              //         ],
-              //       },
-              //     },
-              //   ],
-              // },
-              // {
-              //   identifiant: "contexte-cyber-attaque-subie",
-              //   reponses: [
-              //     {
-              //       identifiant: "contexte-cyber-attaque-subie-oui",
-              //       question: {
-              //         identifiant: "contexte-cyber-attaque-subie-oui-tiroir-type",
-              //         type: "choixMultiple",
-              //         reponses: [
-              //           {
-              //             identifiant:
-              //               "contexte-cyber-attaque-subie-oui-tiroir-type-autre",
-              //             type: { type: "saisieLibre", format: "texte" },
-              //           },
-              //         ],
-              //       },
-              //     },
-              //   ],
-              // },
             ],
           },
-          gouvernance: { libelle: 'Gouvernance',  localisationIllustration:
-              '/images/diagnostic/illustration_gouvernance.svg', questions: [] },
-          SecuriteAcces: { libelle: 'Sécurité des accès', localisationIllustration:
-              '/images/diagnostic/illustration_securite_acces.svg', questions: [] },
-          securiteposte: { libelle: 'Sécurité des postes', localisationIllustration:
-              '/images/diagnostic/illustration_securite_postes.svg', questions: [] },
+          gouvernance: {
+            libelle: 'Gouvernance',
+            localisationIllustration:
+              '/images/diagnostic/illustration_gouvernance.svg',
+            questions: [],
+          },
+          SecuriteAcces: {
+            libelle: "Sécurité des accès au système d'information",
+            localisationIllustration:
+              '/images/diagnostic/illustration_securite_acces.svg',
+            questions: [],
+          },
+          securiteposte: {
+            libelle: 'Sécurité des postes',
+            localisationIllustration:
+              '/images/diagnostic/illustration_securite_postes.svg',
+            questions: [],
+          },
           securiteinfrastructure: {
             libelle: 'Sécurité des infrastructures',
             localisationIllustration:
@@ -97,7 +62,12 @@ export const adaptateurTranscripteur = () =>
               '/images/diagnostic/illustration_sensibilisation.svg',
             questions: [],
           },
-          reaction: { libelle: 'Réaction à une cyber attaque',  localisationIllustration: '/images/diagnostic/illustration_reaction.svg', questions: [] },
+          reaction: {
+            libelle: 'Réaction à une cyber attaque',
+            localisationIllustration:
+              '/images/diagnostic/illustration_reaction.svg',
+            questions: [],
+          },
         },
       };
     }
