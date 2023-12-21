72c5870ad36825dde2ebc7811de511342e23288a
tomi <thomas.bennett@caminu.fr>
2023-12-21 11:38:30 +0100

    [REMANIEMENT] utilise un service de chiffrement en clair pour l'environnement de développement

---
+++ mon-aide-cyber-api/src/infrastructure/adaptateurs/adaptateurServiceChiffrement.ts
@@ -0,0 +1,18 @@
+import { ServiceDeChiffrementChacha20 } from '../securite/ServiceDeChiffrementChacha20';
+import { ServiceDeChiffrement } from '../../securite/ServiceDeChiffrement';
+
+class ServiceDechiffrementClair implements ServiceDeChiffrement {
+  chiffre(chaine: string): string {
+    return chaine;
+  }
+  dechiffre(chaine: string): string {
+    return chaine;
+  }
+}
+
+export const adaptateurServiceChiffrement = (): ServiceDeChiffrement => {
+  return process.env.CLEF_SECRETE_CHIFFREMENT !== undefined ||
+    process.env.CLEF_SECRETE_CHIFFREMENT === null
+    ? new ServiceDeChiffrementChacha20()
+    : new ServiceDechiffrementClair();
+};
