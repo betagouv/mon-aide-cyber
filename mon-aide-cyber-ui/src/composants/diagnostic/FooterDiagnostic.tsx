43eeaeb826d613faeae50e5cb22d4cecbf319d58
Bertrand Bougon <bertrandbougon@gmail.com>
2023-12-22 12:19:29 +0100

    [DIAGNOSTIC][INTEGRATION CSS] Ajoute le footer du diagnostic

---
+++ mon-aide-cyber-ui/src/composants/diagnostic/FooterDiagnostic.tsx
@@ -0,0 +1,8 @@
+export const FooterDiagnostic = () => (
+  <footer role="contentinfo" id="footer">
+    <div className="fond-clair-mac piedpage-diagnostic">
+      <div className="bordure-piedpage-diagnostic"></div>
+      <div className="conteneur-thematique"></div>
+    </div>
+  </footer>
+);
