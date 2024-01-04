122a36137107483370961d546a400ab293e27d03
Bertrand Bougon <bertrandbougon@gmail.com>
2024-01-04 17:10:50 +0100

    [REMANIEMENT] Extrais les différentes thématiques du transcripteur dans leur propre fichier

---
+++ mon-aide-cyber-api/src/infrastructure/adaptateurs/transcripteur/transcripteurContexte.ts
@@ -0,0 +1,59 @@
+import { Thematique } from '../../../api/representateurs/types';
+
+export const transcripteurContexte: Thematique = {
+  description:
+    "Les informations contextuelles d'une entreprise aident à situer sa position par rapport à d'autres dans le même secteur, à cerner les défis cyber propres à son domaine d'activité et en fonction du nombre d'employés.",
+  libelle: 'Contexte',
+  localisationIconeNavigation:
+    '/images/diagnostic/contexte/icone-navigation.svg',
+  localisationIllustration: '/images/diagnostic/contexte/illustration.svg',
+  groupes: [
+    {
+      questions: [
+        { identifiant: 'contexte-nature-organisation' },
+        {
+          identifiant: 'contexte-secteur-activite',
+          type: 'liste',
+        },
+        {
+          identifiant: 'contexte-region-siege-social',
+          type: 'liste',
+        },
+        {
+          identifiant: 'contexte-departement-tom-siege-social',
+          type: 'liste',
+        },
+      ],
+    },
+    {
+      questions: [
+        {
+          identifiant: 'contexte-nombre-personnes-dans-organisation',
+        },
+        {
+          identifiant: 'contexte-nombre-postes-travail-dans-organisation',
+        },
+      ],
+    },
+    {
+      questions: [
+        {
+          identifiant: 'contexte-activites-recherche-et-developpement',
+        },
+      ],
+    },
+    {
+      questions: [
+        {
+          identifiant: 'contexte-opere-systemes-information-industriels',
+        },
+      ],
+    },
+    {
+      questions: [{ identifiant: 'contexte-cyber-attaque-subie' }],
+    },
+    {
+      questions: [{ identifiant: 'contexte-usage-cloud' }],
+    },
+  ],
+};
---
+++ mon-aide-cyber-api/src/infrastructure/adaptateurs/transcripteur/transcripteurReaction.ts
@@ -0,0 +1,30 @@
+import { Thematique } from '../../../api/representateurs/types';
+
+export const transcripteurReaction: Thematique = {
+  description:
+    "La première phase du traitement d'un incident consiste à le qualifier. Cela implique d'identifier les sources d'informations et les éventuelles anomalies qui sont associées. L'objectif principal est d'établir une vision aussi précise que possible de la situation en cours.",
+  libelle: 'Réaction à une cyber attaque',
+  localisationIconeNavigation:
+    '/images/diagnostic/reaction/icone-navigation.svg',
+  localisationIllustration: '/images/diagnostic/reaction/illustration.svg',
+  groupes: [
+    {
+      questions: [
+        {
+          identifiant:
+            'reaction-surveillance-veille-vulnerabilites-potentielles',
+        },
+      ],
+    },
+    {
+      questions: [{ identifiant: 'reaction-sauvegardes-donnees-realisees' }],
+    },
+    {
+      questions: [
+        {
+          identifiant: 'reaction-dispositif-gestion-crise-adapte-defini',
+        },
+      ],
+    },
+  ],
+};
