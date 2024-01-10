b6ffe5853fa83f2b611fa9a46bf66b5dd1c5b699
Bertrand Bougon <bertrandbougon@gmail.com>
2024-01-10 08:37:29 +0100

    [RESTITUTION] Affiche le menu de navigation de la restitution

---
+++ mon-aide-cyber-ui/src/domaine/diagnostic/reducteurRestitution.ts
@@ -0,0 +1,45 @@
+import { Restitution } from './Restitution.ts';
+
+export type EtatRestitution = {
+  restitution?: Restitution;
+  rubrique?: string;
+};
+
+type ActionRestitution =
+  | {
+      restitution: Restitution;
+      type: TypeActionRestitution.RESTITUTION_CHARGEE;
+    }
+  | {
+      rubrique: string;
+      type: TypeActionRestitution.RUBRIQUE_CLIQUEE;
+    };
+
+enum TypeActionRestitution {
+  RESTITUTION_CHARGEE = 'RESTITUTION_CHARGEE',
+  RUBRIQUE_CLIQUEE = 'RUBRIQUE_CLIQUEE',
+}
+
+export const reducteurRestitution = (
+  etat: EtatRestitution,
+  action: ActionRestitution,
+): EtatRestitution => {
+  switch (action.type) {
+    case TypeActionRestitution.RUBRIQUE_CLIQUEE:
+      return { ...etat, rubrique: action.rubrique };
+    case TypeActionRestitution.RESTITUTION_CHARGEE:
+      return { ...etat, restitution: action.restitution };
+  }
+};
+
+export const restitutionChargee = (
+  restitution: Restitution,
+): ActionRestitution => ({
+  restitution: restitution,
+  type: TypeActionRestitution.RESTITUTION_CHARGEE,
+});
+
+export const rubriqueCliquee = (rubrique: string): ActionRestitution => ({
+  rubrique,
+  type: TypeActionRestitution.RUBRIQUE_CLIQUEE,
+});
---
+++ mon-aide-cyber-ui/test/constructeurs/constructeurRestitution.ts
@@ -0,0 +1,21 @@
+import { fakerFR } from '@faker-js/faker';
+import { Restitution } from '../../src/domaine/diagnostic/Restitution';
+import { Constructeur } from './Constructeur';
+
+class ConstructeurRestitution implements Constructeur<Restitution> {
+  constructor(
+    private autresMesures: string = fakerFR.lorem.text(),
+    private indicateurs: string = fakerFR.lorem.text(),
+    private mesuresPrioritaires: string = fakerFR.lorem.text(),
+  ) {}
+
+  construis(): Restitution {
+    return {
+      autresMesures: this.autresMesures,
+      indicateurs: this.indicateurs,
+      mesuresPrioritaires: this.mesuresPrioritaires,
+    };
+  }
+}
+
+export const uneRestitution = () => new ConstructeurRestitution();
