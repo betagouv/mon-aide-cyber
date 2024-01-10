b80ae8e5eaf091ed74376c198b7278929701f38f
Bertrand Bougon <bertrandbougon@gmail.com>
2024-01-10 08:37:29 +0100

    [RESTITUTION] Affiche le menu de navigation de la restitution

--- mon-aide-cyber-ui/src/composants/diagnostic/ComposantRestitution.tsx
+++ mon-aide-cyber-ui/src/composants/diagnostic/ComposantRestitution.tsx
@@ -1,31 +1,48 @@
 import { Header } from '../Header.tsx';
 import { Footer } from '../Footer.tsx';
-import { useCallback, useEffect, useState } from 'react';
+import { useCallback, useEffect, useReducer } from 'react';
 import { useEntrepots } from '../../fournisseurs/hooks.ts';
-import { useParams, useNavigate } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import { useErrorBoundary } from 'react-error-boundary';
-import { Restitution } from '../../domaine/diagnostic/Restitution.ts';
 import '../../assets/styles/_restitution.scss';
 import '../../assets/styles/_commun.scss';
+import {
+  reducteurRestitution,
+  restitutionChargee,
+  rubriqueCliquee,
+} from '../../domaine/diagnostic/reducteurRestitution.ts';
+import { UUID } from '../../types/Types.ts';
 
-export const ComposantRestitution = () => {
+type ProprietesComposantRestitution = {
+  idDiagnostic: UUID;
+};
+
+export const ComposantRestitution = ({
+  idDiagnostic,
+}: ProprietesComposantRestitution) => {
   const entrepots = useEntrepots();
-  const { idDiagnostic } = useParams();
   const { showBoundary } = useErrorBoundary();
-  const [restitution, setRestitution] = useState<Restitution>();
   const navigate = useNavigate();
+  const [etatRestitution, envoie] = useReducer(reducteurRestitution, {});
 
   useEffect(() => {
     entrepots
       .diagnostic()
-      .restitution(idDiagnostic!)
-      .then((restitution) => setRestitution(restitution))
+      .restitution(idDiagnostic)
+      .then((restitution) => {
+        envoie(restitutionChargee(restitution));
+      })
       .catch((erreur) => showBoundary(erreur));
-  }, [entrepots, idDiagnostic, showBoundary]);
+  }, [entrepots, envoie, idDiagnostic, showBoundary]);
 
   const modifierLeDiagnostic = useCallback(
     () => navigate(`/diagnostic/${idDiagnostic}`),
     [idDiagnostic, navigate],
+  );
+
+  const rubriqueCliqueee = useCallback(
+    (rubrique: string) => envoie(rubriqueCliquee(rubrique)),
+    [envoie],
   );
 
   return (
@@ -50,7 +67,7 @@ export const ComposantRestitution = () => {
             <div className="fr-grid-row fr-grid-row--right">
               <div className="fr-pl-2w">
                 <button
-                  className={`fr-btn fr-btn--icon-left fr-icon-download-line bouton-mac bouton-mac-secondaire-inverse`}
+                  className={`fr-btn--icon-left fr-icon-download-line bouton-mac bouton-mac-secondaire-inverse`}
                   disabled={true}
                 >
                   Télécharger
@@ -58,7 +75,7 @@ export const ComposantRestitution = () => {
               </div>
               <div className="fr-pl-2w">
                 <button
-                  className={`fr-btn fr-btn--icon-left fr-icon-pencil-line bouton-mac bouton-mac-primaire-inverse`}
+                  className={`fr-btn--icon-left fr-icon-pencil-line bouton-mac bouton-mac-primaire-inverse`}
                   onClick={modifierLeDiagnostic}
                 >
                   Modifier le diagnostic
@@ -70,23 +87,116 @@ export const ComposantRestitution = () => {
         <div className="fond-clair-mac">
           <div className="fr-container restitution">
             <div className="fr-grid-row">
-              <div className="fr-col-md-3 fr-col-3">MENU</div>
+              <div className="fr-col-md-3 fr-col-3">
+                <nav
+                  className="fr-sidemenu fr-sidemenu--sticky"
+                  aria-labelledby="fr-sidemenu-title"
+                >
+                  <div className="fr-sidemenu__inner">
+                    <div className="fr-collapse" id="fr-sidemenu-wrapper">
+                      <ul className="fr-sidemenu__list">
+                        <li
+                          className={`fr-sidemenu__item ${
+                            etatRestitution.rubrique === 'informations'
+                              ? 'fr-sidemenu__item--active'
+                              : ''
+                          }`}
+                        >
+                          <a
+                            className="fr-sidemenu__link"
+                            href="#informations"
+                            target="_self"
+                            {...(etatRestitution.rubrique ===
+                              'informations' && { 'aria-current': 'page' })}
+                            onClick={() => rubriqueCliqueee('informations')}
+                          >
+                            Informations
+                          </a>
+                        </li>
+                        <li
+                          className={`fr-sidemenu__item ${
+                            etatRestitution.rubrique === 'indicateurs'
+                              ? 'fr-sidemenu__item--active'
+                              : ''
+                          }`}
+                        >
+                          <a
+                            className="fr-sidemenu__link"
+                            href="#indicateurs"
+                            target="_self"
+                            {...(etatRestitution.rubrique === 'indicateurs' && {
+                              'aria-current': 'page',
+                            })}
+                            onClick={() => rubriqueCliqueee('indicateurs')}
+                          >
+                            Indicateurs de maturité
+                          </a>
+                        </li>
+                        <li
+                          className={`fr-sidemenu__item ${
+                            etatRestitution.rubrique === 'mesures-prioritaires'
+                              ? 'fr-sidemenu__item--active'
+                              : ''
+                          }`}
+                        >
+                          <a
+                            className="fr-sidemenu__link"
+                            href="#mesures-prioritaires"
+                            target="_self"
+                            {...(etatRestitution.rubrique ===
+                              'mesures-prioritaires' && {
+                              'aria-current': 'page',
+                            })}
+                            onClick={() =>
+                              rubriqueCliqueee('mesures-prioritaires')
+                            }
+                          >
+                            Les 6 mesures prioritaires
+                          </a>
+                        </li>
+                        <li
+                          className={`fr-sidemenu__item ${
+                            etatRestitution.rubrique === 'autres-mesures'
+                              ? 'fr-sidemenu__item--active'
+                              : ''
+                          }`}
+                        >
+                          <a
+                            className="fr-sidemenu__link"
+                            href="#autres-mesures"
+                            target="_self"
+                            {...(etatRestitution.rubrique ===
+                              'autres-mesures' && { 'aria-current': 'page' })}
+                            onClick={() => rubriqueCliqueee('autres-mesures')}
+                          >
+                            Les autres mesures
+                          </a>
+                        </li>
+                      </ul>
+                    </div>
+                  </div>
+                </nav>
+              </div>
               <div className="fr-col-md-9 fr-col-9 section">
                 <h3>Récapitulatif</h3>
-                <div>INFORMATIONS</div>
+                <div id="informations">INFORMATIONS</div>
                 <div
+                  id="indicateurs"
                   dangerouslySetInnerHTML={{
-                    __html: restitution?.indicateurs || '',
+                    __html: etatRestitution.restitution?.indicateurs || '',
                   }}
                 ></div>
                 <div
+                  id="mesures-prioritaires"
                   dangerouslySetInnerHTML={{
-                    __html: restitution?.mesuresPrioritaires || '',
+                    __html:
+                      etatRestitution.restitution?.mesuresPrioritaires || '',
                   }}
                 ></div>
                 <div
+                  id="autres-mesures"
                   dangerouslySetInnerHTML={{
-                    __html: restitution?.autresMesures || '',
+                    __html: etatRestitution.restitution?.autresMesures || '',
                   }}
                 ></div>
               </div>
---
+++ mon-aide-cyber-ui/test/domaine/diagnostic/reducteurRestitution.spec.ts
@@ -0,0 +1,36 @@
+import { describe, it } from 'vitest';
+import {
+  EtatRestitution,
+  reducteurRestitution,
+  restitutionChargee,
+  rubriqueCliquee,
+} from '../../../src/domaine/diagnostic/reducteurRestitution';
+import { uneRestitution } from '../../constructeurs/constructeurRestitution';
+
+describe('Réducteur Restitution', () => {
+  it('charge la restitution', () => {
+    const restitution = uneRestitution().construis();
+    const etatRestitution = reducteurRestitution(
+      {} as EtatRestitution,
+      restitutionChargee(restitution),
+    );
+
+    expect(etatRestitution).toStrictEqual({
+      restitution,
+    });
+  });
+
+  it('conserve la rubrique cliquée', () => {
+    const restitution = uneRestitution().construis();
+
+    const etatRestitution = reducteurRestitution(
+      { restitution },
+      rubriqueCliquee('informations'),
+    );
+
+    expect(etatRestitution).toStrictEqual<EtatRestitution>({
+      restitution: restitution,
+      rubrique: 'informations',
+    });
+  });
+});
