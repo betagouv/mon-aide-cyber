6a867994656bcdf1bee18e48396a97008a7468b0
Bertrand Bougon <bertrandbougon@gmail.com>
2024-01-12 16:48:20 +0100

    [REMANIEMENT] Affiche l'alerte de lien copié sous le bouton de copie

--- mon-aide-cyber-ui/src/composants/diagnostic/HeaderDiagnostic.tsx
+++ mon-aide-cyber-ui/src/composants/diagnostic/HeaderDiagnostic.tsx
@@ -1,46 +1,80 @@
+import { useCallback, useState } from 'react';
+
 type ProprietesHeaderDiagnostic = {
   quitter: { quitterLeDiagnostic: () => void };
-  copier: { copier: () => void };
 };
-export const HeaderDiagnostic = (propietes: ProprietesHeaderDiagnostic) => (
-  <header role="banner" className="fr-header diagnostic-header">
-    <div className="fr-header__body">
+export const HeaderDiagnostic = (propietes: ProprietesHeaderDiagnostic) => {
+  const [lienCopie, setLienCopie] = useState(<></>);
+
+  const fermeAlerte = useCallback(() => {
+    setLienCopie(<></>);
+  }, []);
+
+  const copierLienDiagnostic = useCallback(() => {
+    navigator.clipboard.writeText(window.location.href);
+    setLienCopie(() => (
       <div className="fr-container">
-        <div className="fr-header__body-row">
-          <div className="fr-header__brand fr-enlarge-link">
-            <div className="fr-header__brand-top"></div>
-            <div className="fr-header__service">
-              <img
-                className="fr-responsive-img"
-                src="/images/logo_mac.svg"
-                alt="ANSSI"
-              />
-            </div>
+        <div className="fr-grid-row fr-grid-row--right">
+          <div
+            className="fr-alert fr-alert--success fr-mb-1w fr-col-4"
+            role="alert"
+          >
+            <h3 className="fr-alert__title">Lien copié avec succès</h3>
+            <p>Le lien du diagnostic a été copié dans votre presse-papier</p>
+            <button
+              className="fr-btn--close fr-btn"
+              title="Masquer le message"
+              onClick={fermeAlerte}
+            >
+              Masquer le message
+            </button>
           </div>
-          <div className="fr-header__tools">
-            <div className="fr-header__tools-links">
-              <ul className="fr-btns-group">
-                <li className="fr-pr-2w">
-                  <button
-                    className="bouton-mac bouton-mac-primaire"
-                    title="Quitter le diagnostic"
-                    onClick={propietes.quitter.quitterLeDiagnostic}
-                  >
-                    Quitter le diagnostic
-                  </button>
-                </li>
-                <li>
-                  <button
-                    className="bouton-icone-mac bouton-mac-secondaire fr-share__link fr-share__link--copy"
-                    title="Copier le lien du diagnostic"
-                    onClick={propietes.copier.copier}
-                  />
-                </li>
-              </ul>
+        </div>
+      </div>
+    ));
+  }, [fermeAlerte]);
+
+  return (
+    <header role="banner" className="fr-header diagnostic-header">
+      <div className="fr-header__body">
+        <div className="fr-container">
+          <div className="fr-header__body-row">
+            <div className="fr-header__brand fr-enlarge-link">
+              <div className="fr-header__brand-top"></div>
+              <div className="fr-header__service">
+                <img
+                  className="fr-responsive-img"
+                  src="/images/logo_mac.svg"
+                  alt="ANSSI"
+                />
+              </div>
+            </div>
+            <div className="fr-header__tools">
+              <div className="fr-header__tools-links">
+                <ul className="fr-btns-group">
+                  <li className="fr-pr-2w">
+                    <button
+                      className="bouton-mac bouton-mac-primaire"
+                      title="Quitter le diagnostic"
+                      onClick={propietes.quitter.quitterLeDiagnostic}
+                    >
+                      Quitter le diagnostic
+                    </button>
+                  </li>
+                  <li>
+                    <button
+                      className="bouton-icone-mac bouton-mac-secondaire fr-share__link fr-share__link--copy"
+                      title="Copier le lien du diagnostic"
+                      onClick={copierLienDiagnostic}
+                    />
+                  </li>
+                </ul>
+              </div>
             </div>
           </div>
         </div>
       </div>
-    </div>
-  </header>
-);
+      {lienCopie}
+    </header>
+  );
+};
