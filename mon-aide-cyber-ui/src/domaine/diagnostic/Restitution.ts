73c559a11ab2678ddbe44a1e7b1b90e37bbcab58
Bertrand Bougon <bertrandbougon@gmail.com>
2024-01-19 10:28:39 +0100

    [RESTITUTION] Active le bouton Télécharger de la restitution

--- mon-aide-cyber-ui/src/domaine/diagnostic/Restitution.ts
+++ mon-aide-cyber-ui/src/domaine/diagnostic/Restitution.ts
@@ -1,4 +1,19 @@
+type RessourceActionRestituer = {
+  ressource: {
+    url: string;
+    methode: 'PATCH' | 'GET';
+    contentType: 'application/pdf' | 'application/json';
+  };
+};
+type ActionRestituer = {
+  action: 'restituer';
+  types: {
+    [type: string]: RessourceActionRestituer;
+  };
+};
+
 export type Restitution = {
+  actions: ActionRestituer[];
   autresMesures: string;
   indicateurs: string;
   informations: string;
