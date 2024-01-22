a746708d66cdaa67290d60588466cc3527e8a7b0
Bertrand Bougon <bertrandbougon@gmail.com>
2024-01-22 09:06:18 +0100

    [CORRECTION] Configure vitest pour ne pas afficher en console les erreurs remontées par le gestionnaire d'erreurs

--- mon-aide-cyber-api/vitest.config.ts
+++ mon-aide-cyber-api/vitest.config.ts
@@ -1,16 +1,19 @@
-import { defineProject } from "vitest/config";
+import { defineProject } from 'vitest/config';
 
 export default defineProject({
   test: {
     env: {
       URL_SERVEUR_BASE_DONNEES:
         process.env.URL_SERVEUR_BASE_DONNEES ||
-        "postgres://postgres@localhost:5434",
+        'postgres://postgres@localhost:5434',
       URL_JOURNALISATION_BASE_DONNEES:
         process.env.URL_JOURNALISATION_BASE_DONNEES ||
-        "postgres://postgres@localhost:5434",
+        'postgres://postgres@localhost:5434',
     },
-    environment: "node",
+    onConsoleLog(log: string): boolean {
+      return !log.includes('Une erreur est survenue:');
+    },
+    environment: 'node',
     globals: true,
   },
 });
