bb56942c5c69bf29feca1c533e0c32e6ee92d30c
Bertrand Bougon <bertrandbougon@gmail.com>
2024-02-06 15:05:30 +0100

    [CORRECTION] Corrige la faute d'orthographe dans cyberattaque

--- mon-aide-cyber-api/src/infrastructure/adaptateurs/transcripteur/transcripteurReaction.ts
+++ mon-aide-cyber-api/src/infrastructure/adaptateurs/transcripteur/transcripteurReaction.ts
@@ -3,7 +3,7 @@ import { Thematique } from '../../../api/representateurs/types';
 export const transcripteurReaction: Thematique = {
   description:
     "La première phase du traitement d'un incident consiste à le qualifier. Cela implique d'identifier les sources d'informations et les éventuelles anomalies qui sont associées. L'objectif principal est d'établir une vision aussi précise que possible de la situation en cours.",
-  libelle: 'Réaction à une cyber attaque',
+  libelle: 'Réaction à une cyberattaque',
   localisationIconeNavigation:
     '/images/diagnostic/reaction/icone-navigation.svg',
   localisationIllustration: '/images/diagnostic/reaction/illustration.svg',
