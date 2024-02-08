07d9fa85e6ddb53cd769cdcd12272e07afdd28eb
Bertrand Bougon <bertrandbougon@gmail.com>
2024-02-08 11:04:38 +0100

    [API] Récupère l'identifiant de l'utilisateur courant depuis son jeton JWT

--- mon-aide-cyber-api/src/infrastructure/authentification/gestionnaireDeJetonJWT.ts
+++ mon-aide-cyber-api/src/infrastructure/authentification/gestionnaireDeJetonJWT.ts
@@ -2,15 +2,17 @@ import {
   DonneesJetonMAC,
   GestionnaireDeJeton,
   Jeton,
+  JwtMACPayload,
 } from '../../authentification/GestionnaireDeJeton';
-import jwt from 'jsonwebtoken';
+import jwt, { JwtPayload } from 'jsonwebtoken';
 import { FournisseurHorloge } from '../horloge/FournisseurHorloge';
 
 export class GestionnaireDeJetonJWT implements GestionnaireDeJeton {
   constructor(private readonly clef: string) {}
 
-  verifie(token: string): void {
-    jwt.verify(token, this.clef);
+  verifie(jeton: string): JwtMACPayload {
+    const payload: JwtPayload = jwt.verify(jeton, this.clef) as JwtPayload;
+    return { ...payload, identifiant: payload.identifiant };
   }
 
   genereJeton(donnee: DonneesJetonMAC): Jeton {
