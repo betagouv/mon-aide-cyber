d9d4c74f30920088fb105207ea19d7a08cde1762
Bertrand Bougon <bertrandbougon@gmail.com>
2024-02-20 15:27:40 +0100

    [PARCOURS CGU] Prends en compte le changement du mot de passe

--- mon-aide-cyber-api/src/api/routesAPIUtilisateur.ts
+++ mon-aide-cyber-api/src/api/routesAPIUtilisateur.ts
@@ -4,6 +4,19 @@ import { RequeteUtilisateur } from './routesAPI';
 import { NextFunction } from 'express-serve-static-core';
 import { ServiceFinalisationCreationCompte } from '../compte/ServiceFinalisationCompte';
 import { constructeurActionsHATEOAS } from './hateoas/hateoas';
+import {
+  ExpressValidator,
+  FieldValidationError,
+  Meta,
+  Result,
+} from 'express-validator';
+import { ErreurMAC } from '../domaine/erreurMAC';
+import { ErreurFinalisationCompte } from '../authentification/Aidant';
+
+type CorpsRequeteFinalisationCompte = {
+  cguSignees: boolean;
+  motDePasse: string;
+};
 
 export const routesAPIUtilisateur = (configuration: ConfigurationServeur) => {
   const routes = express.Router();
@@ -11,34 +24,82 @@ export const routesAPIUtilisateur = (configuration: ConfigurationServeur) => {
   const { entrepots, adaptateurDeVerificationDeSession: session } =
     configuration;
 
+  const { body, validationResult } = new ExpressValidator({
+    motDePasseTemporaireDifferentDuNouveauMotDePasse: (
+      value: string,
+      { req }: Meta
+    ) => value !== req.body.motDePasse,
+    motDePasseUtilisateur: async (value: string, { req }: Meta) => {
+      const aidant = await entrepots
+        .aidants()
+        .lis(req.identifiantUtilisateurCourant);
+      if (aidant.motDePasse !== value) {
+        throw new Error('Votre mot de passe temporaire est erroné.');
+      }
+      return true;
+    },
+  });
+
   routes.post(
     '/finalise',
     express.json(),
     session.verifie('Finalise la création du compte'),
+    body('motDePasse')
+      .isStrongPassword({
+        minLength: 16,
+        minLowercase: 1,
+        minUppercase: 1,
+        minNumbers: 1,
+        minSymbols: 1,
+      })
+      .withMessage(
+        'Votre nouveau mot de passe ne respecte pas les règles de MonAideCyber.'
+      ),
+    body('motDePasseTemporaire')
+      .notEmpty()
+      .withMessage('Le mot de passe temporaire est obligatoire.')
+      .motDePasseTemporaireDifferentDuNouveauMotDePasse()
+      .withMessage(
+        'Votre nouveau mot de passe doit être différent du mot de passe temporaire.'
+      )
+      .motDePasseUtilisateur(),
     async (
       requete: RequeteUtilisateur,
       reponse: Response,
-      suite: NextFunction,
+      suite: NextFunction
     ) => {
       try {
-        const finalisationCompte: {
-          cguSignees: boolean;
-        } = requete.body;
-        await new ServiceFinalisationCreationCompte(entrepots).finalise({
-          ...finalisationCompte,
-          identifiant: requete.identifiantUtilisateurCourant!,
-        });
-        reponse.status(200).json({
-          ...constructeurActionsHATEOAS()
-            .tableauDeBord()
-            .lancerDiagnostic()
-            .construis(),
-        });
-        reponse.send();
+        const resultatValidation: Result<FieldValidationError> =
+          validationResult(requete) as Result<FieldValidationError>;
+        if (resultatValidation.isEmpty()) {
+          const finalisationCompte: CorpsRequeteFinalisationCompte =
+            requete.body;
+          await new ServiceFinalisationCreationCompte(entrepots).finalise({
+            ...finalisationCompte,
+            identifiant: requete.identifiantUtilisateurCourant!,
+          });
+          reponse.status(200).json({
+            ...constructeurActionsHATEOAS()
+              .tableauDeBord()
+              .lancerDiagnostic()
+              .construis(),
+          });
+          return reponse.send();
+        }
+        const erreursValidation = resultatValidation
+          .array()
+          .map((resultat) => resultat.msg)
+          .filter((erreur): erreur is string => !!erreur);
+        return suite(
+          ErreurMAC.cree(
+            'Finalise la création du compte',
+            new ErreurFinalisationCompte(erreursValidation.join('\n'))
+          )
+        );
       } catch (erreur) {
         suite(erreur);
       }
-    },
+    }
   );
 
   return routes;
--- mon-aide-cyber-api/test/api/routesAPIUtilisateur.spec.ts
+++ mon-aide-cyber-api/test/api/routesAPIUtilisateur.spec.ts
@@ -37,7 +37,11 @@ describe('le serveur MAC sur les routes /api/utilisateur', () => {
         'POST',
         `/api/utilisateur/finalise`,
         donneesServeur.portEcoute,
-        { cguSignees: true },
+        {
+          cguSignees: true,
+          motDePasse: 'EgLw5R0ItVRxkl%#>cPd',
+          motDePasseTemporaire: aidantFinalisantSonCompte.motDePasse,
+        },
       );
 
       expect(reponse.statusCode).toBe(200);
@@ -61,6 +65,277 @@ describe('le serveur MAC sur les routes /api/utilisateur', () => {
       });
     });
 
+    describe('En ce qui concerne le changement du mot de passe', () => {
+      it('vérifie la longueur du mot de passe', async () => {
+        const aidantFinalisantSonCompte = unAidant()
+          .avecCompteEnAttenteDeFinalisation()
+          .construis();
+        await testeurMAC.entrepots
+          .aidants()
+          .persiste(aidantFinalisantSonCompte);
+        testeurMAC.adaptateurDeVerificationDeSession.utilisateurConnecte(
+          aidantFinalisantSonCompte,
+        );
+
+        const reponse = await executeRequete(
+          donneesServeur.app,
+          'POST',
+          `/api/utilisateur/finalise`,
+          donneesServeur.portEcoute,
+          {
+            cguSignees: true,
+            motDePasse: 'EgLwVRxkl%#>cPd',
+            motDePasseTemporaire: aidantFinalisantSonCompte.motDePasse,
+          },
+        );
+
+        expect(reponse.statusCode).toBe(422);
+        expect(await reponse.json()).toStrictEqual({
+          message:
+            'Votre nouveau mot de passe ne respecte pas les règles de MonAideCyber.',
+        });
+      });
+
+      it('vérifie que le mot de passe contient au moins une minuscule', async () => {
+        const aidantFinalisantSonCompte = unAidant()
+          .avecCompteEnAttenteDeFinalisation()
+          .construis();
+        await testeurMAC.entrepots
+          .aidants()
+          .persiste(aidantFinalisantSonCompte);
+        testeurMAC.adaptateurDeVerificationDeSession.utilisateurConnecte(
+          aidantFinalisantSonCompte,
+        );
+
+        const reponse = await executeRequete(
+          donneesServeur.app,
+          'POST',
+          `/api/utilisateur/finalise`,
+          donneesServeur.portEcoute,
+          {
+            cguSignees: true,
+            motDePasse: 'EGLW5R0ITVRXKL%#>CPD',
+            motDePasseTemporaire: aidantFinalisantSonCompte.motDePasse,
+          },
+        );
+
+        expect(reponse.statusCode).toBe(422);
+        expect(await reponse.json()).toStrictEqual({
+          message:
+            'Votre nouveau mot de passe ne respecte pas les règles de MonAideCyber.',
+        });
+      });
+
+      it('vérifie que le mot de passe contient au moins une majuscule', async () => {
+        const aidantFinalisantSonCompte = unAidant()
+          .avecCompteEnAttenteDeFinalisation()
+          .construis();
+        await testeurMAC.entrepots
+          .aidants()
+          .persiste(aidantFinalisantSonCompte);
+        testeurMAC.adaptateurDeVerificationDeSession.utilisateurConnecte(
+          aidantFinalisantSonCompte,
+        );
+
+        const reponse = await executeRequete(
+          donneesServeur.app,
+          'POST',
+          `/api/utilisateur/finalise`,
+          donneesServeur.portEcoute,
+          {
+            cguSignees: true,
+            motDePasse: 'eglw5r0itvrxkl%#>cpd',
+            motDePasseTemporaire: aidantFinalisantSonCompte.motDePasse,
+          },
+        );
+
+        expect(reponse.statusCode).toBe(422);
+        expect(await reponse.json()).toStrictEqual({
+          message:
+            'Votre nouveau mot de passe ne respecte pas les règles de MonAideCyber.',
+        });
+      });
+
+      it('vérifie que le mot de passe contient au moins un chiffre', async () => {
+        const aidantFinalisantSonCompte = unAidant()
+          .avecCompteEnAttenteDeFinalisation()
+          .construis();
+        await testeurMAC.entrepots
+          .aidants()
+          .persiste(aidantFinalisantSonCompte);
+        testeurMAC.adaptateurDeVerificationDeSession.utilisateurConnecte(
+          aidantFinalisantSonCompte,
+        );
+
+        const reponse = await executeRequete(
+          donneesServeur.app,
+          'POST',
+          `/api/utilisateur/finalise`,
+          donneesServeur.portEcoute,
+          {
+            cguSignees: true,
+            motDePasse: 'EgLwCRDItVRxkl%#>cPd',
+            motDePasseTemporaire: aidantFinalisantSonCompte.motDePasse,
+          },
+        );
+
+        expect(reponse.statusCode).toBe(422);
+        expect(await reponse.json()).toStrictEqual({
+          message:
+            'Votre nouveau mot de passe ne respecte pas les règles de MonAideCyber.',
+        });
+      });
+
+      it('vérifie que le mot de passe contient au moins un caractère spécial', async () => {
+        const aidantFinalisantSonCompte = unAidant()
+          .avecCompteEnAttenteDeFinalisation()
+          .construis();
+        await testeurMAC.entrepots
+          .aidants()
+          .persiste(aidantFinalisantSonCompte);
+        testeurMAC.adaptateurDeVerificationDeSession.utilisateurConnecte(
+          aidantFinalisantSonCompte,
+        );
+
+        const reponse = await executeRequete(
+          donneesServeur.app,
+          'POST',
+          `/api/utilisateur/finalise`,
+          donneesServeur.portEcoute,
+          {
+            cguSignees: true,
+            motDePasse: 'EgLwCRD0tVRxklAbCcPd',
+            motDePasseTemporaire: aidantFinalisantSonCompte.motDePasse,
+          },
+        );
+
+        expect(reponse.statusCode).toBe(422);
+        expect(await reponse.json()).toStrictEqual({
+          message:
+            'Votre nouveau mot de passe ne respecte pas les règles de MonAideCyber.',
+        });
+      });
+
+      it('vérifie que le mot de passe temporaire est bien fourni', async () => {
+        const aidantFinalisantSonCompte = unAidant()
+          .avecCompteEnAttenteDeFinalisation()
+          .construis();
+        await testeurMAC.entrepots
+          .aidants()
+          .persiste(aidantFinalisantSonCompte);
+        testeurMAC.adaptateurDeVerificationDeSession.utilisateurConnecte(
+          aidantFinalisantSonCompte,
+        );
+
+        const reponse = await executeRequete(
+          donneesServeur.app,
+          'POST',
+          `/api/utilisateur/finalise`,
+          donneesServeur.portEcoute,
+          {
+            cguSignees: true,
+            motDePasse: 'EgLwCRD0tVRxkl>AbCcPd',
+            motDePasseTemporaire: '',
+          },
+        );
+
+        expect(reponse.statusCode).toBe(422);
+        expect(await reponse.json()).toStrictEqual({
+          message:
+            'Le mot de passe temporaire est obligatoire.\nVotre mot de passe temporaire est erroné.',
+        });
+      });
+
+      it('vérifie que le mot de passe temporaire est bien fourni et que le nouveau mot de passe est valide', async () => {
+        const aidantFinalisantSonCompte = unAidant()
+          .avecCompteEnAttenteDeFinalisation()
+          .construis();
+        await testeurMAC.entrepots
+          .aidants()
+          .persiste(aidantFinalisantSonCompte);
+        testeurMAC.adaptateurDeVerificationDeSession.utilisateurConnecte(
+          aidantFinalisantSonCompte,
+        );
+
+        const reponse = await executeRequete(
+          donneesServeur.app,
+          'POST',
+          `/api/utilisateur/finalise`,
+          donneesServeur.portEcoute,
+          {
+            cguSignees: true,
+            motDePasse: 'EgLwCRD0tVRxklAbCcPd',
+            motDePasseTemporaire: '',
+          },
+        );
+
+        expect(reponse.statusCode).toBe(422);
+        expect(await reponse.json()).toStrictEqual({
+          message:
+            'Votre nouveau mot de passe ne respecte pas les règles de MonAideCyber.\nLe mot de passe temporaire est obligatoire.\nVotre mot de passe temporaire est erroné.',
+        });
+      });
+
+      it('vérifie que le mot de passe temporaire est différent du nouveau mot de passe', async () => {
+        const aidantFinalisantSonCompte = unAidant()
+          .avecCompteEnAttenteDeFinalisation()
+          .construis();
+        await testeurMAC.entrepots
+          .aidants()
+          .persiste(aidantFinalisantSonCompte);
+        testeurMAC.adaptateurDeVerificationDeSession.utilisateurConnecte(
+          aidantFinalisantSonCompte,
+        );
+
+        const reponse = await executeRequete(
+          donneesServeur.app,
+          'POST',
+          `/api/utilisateur/finalise`,
+          donneesServeur.portEcoute,
+          {
+            cguSignees: true,
+            motDePasse: 'EgLwCRD0tVRxkl>AbCcPd',
+            motDePasseTemporaire: 'EgLwCRD0tVRxkl>AbCcPd',
+          },
+        );
+
+        expect(reponse.statusCode).toBe(422);
+        expect(await reponse.json()).toStrictEqual({
+          message:
+            'Votre nouveau mot de passe doit être différent du mot de passe temporaire.\nVotre mot de passe temporaire est erroné.',
+        });
+      });
+
+      it("vérifie que le mot de passe temporaire est bien celui de l'utilisateur", async () => {
+        const aidantFinalisantSonCompte = unAidant()
+          .avecCompteEnAttenteDeFinalisation()
+          .construis();
+        await testeurMAC.entrepots
+          .aidants()
+          .persiste(aidantFinalisantSonCompte);
+        testeurMAC.adaptateurDeVerificationDeSession.utilisateurConnecte(
+          aidantFinalisantSonCompte,
+        );
+
+        const reponse = await executeRequete(
+          donneesServeur.app,
+          'POST',
+          `/api/utilisateur/finalise`,
+          donneesServeur.portEcoute,
+          {
+            cguSignees: true,
+            motDePasse: 'EgLwCRD0tVRxkl>AbCcPd',
+            motDePasseTemporaire: 'mauvais-mdp',
+          },
+        );
+
+        expect(reponse.statusCode).toBe(422);
+        expect(await reponse.json()).toStrictEqual({
+          message: 'Votre mot de passe temporaire est erroné.',
+        });
+      });
+    });
+
     it('renvoie une erreur HTTP 422 si les CGU ne sont pas acceptées', async () => {
       FournisseurHorlogeDeTest.initialise(
         new Date(Date.parse('2024-02-04T13:04:25+01:00')),
@@ -78,7 +353,11 @@ describe('le serveur MAC sur les routes /api/utilisateur', () => {
         'POST',
         `/api/utilisateur/finalise`,
         donneesServeur.portEcoute,
-        { cguSignees: false },
+        {
+          cguSignees: false,
+          motDePasse: 'EgLw5R0ItVRxkl%#>cPd',
+          motDePasseTemporaire: aidantFinalisantSonCompte.motDePasse,
+        },
       );
 
       expect(reponse.statusCode).toBe(422);
