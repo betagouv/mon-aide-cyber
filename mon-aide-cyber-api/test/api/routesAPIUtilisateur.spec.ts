34b73ed0120874670f7facc37d781a961c0afd18
Bertrand Bougon <bertrandbougon@gmail.com>
2024-02-20 15:27:40 +0100

    [PARCOURS CGU] Prends en compte le changement du mot de passe

--- mon-aide-cyber-api/test/api/routesAPIUtilisateur.spec.ts
+++ mon-aide-cyber-api/test/api/routesAPIUtilisateur.spec.ts
@@ -20,7 +20,7 @@ describe('le serveur MAC sur les routes /api/utilisateur', () => {
   });
 
   describe('quand une requête POST est reçue sur /finalise', () => {
-    it('marque les CGU à signées', async () => {
+    it('finalise la création du compte en marquant les CGU à signées et en modifiant le mot de passe', async () => {
       FournisseurHorlogeDeTest.initialise(
         new Date(Date.parse('2024-02-04T13:04:25+01:00')),
       );
@@ -37,7 +37,7 @@ describe('le serveur MAC sur les routes /api/utilisateur', () => {
         'POST',
         `/api/utilisateur/finalise`,
         donneesServeur.portEcoute,
-        { cguSignees: true },
+        { cguSignees: true, motDePasse: 'EgLw5R0ItVRxkl%#>cPd' },
       );
 
       expect(reponse.statusCode).toBe(200);
@@ -47,6 +47,7 @@ describe('le serveur MAC sur les routes /api/utilisateur', () => {
       expect(aidantRetrouve.dateSignatureCGU).toStrictEqual(
         FournisseurHorloge.maintenant(),
       );
+      expect(aidantRetrouve.motDePasse).toStrictEqual('EgLw5R0ItVRxkl%#>cPd');
       expect(
         testeurMAC.adaptateurDeVerificationDeSession.verifiePassage(),
       ).toBe(true);
@@ -58,6 +59,148 @@ describe('le serveur MAC sur les routes /api/utilisateur', () => {
             methode: 'POST',
           },
         },
+      });
+    });
+
+    describe('En ce qui concerne le changement du mot de passe', () => {
+      it('vérifie la longueur du mot de passe', async () => {
+        FournisseurHorlogeDeTest.initialise(
+          new Date(Date.parse('2024-02-04T13:04:25+01:00')),
+        );
+        const aidantFinalisantSonCompte = unAidant()
+          .avecCompteEnAttenteDeFinalisation()
+          .construis();
+        await testeurMAC.entrepots
+          .aidants()
+          .persiste(aidantFinalisantSonCompte);
+        testeurMAC.adaptateurDeVerificationDeSession.utilisateurConnecte(
+          aidantFinalisantSonCompte,
+        );
+
+        const reponse = await executeRequete(
+          donneesServeur.app,
+          'POST',
+          `/api/utilisateur/finalise`,
+          donneesServeur.portEcoute,
+          { cguSignees: true, motDePasse: 'EgLwVRxkl%#>cPd' },
+        );
+
+        expect(reponse.statusCode).toBe(422);
+        expect(await reponse.json()).toStrictEqual({
+          message: 'Le mot de passe est trop simple.',
+        });
+      });
+
+      it('vérifie que le mot de passe contient au moins une minuscule', async () => {
+        FournisseurHorlogeDeTest.initialise(
+          new Date(Date.parse('2024-02-04T13:04:25+01:00')),
+        );
+        const aidantFinalisantSonCompte = unAidant()
+          .avecCompteEnAttenteDeFinalisation()
+          .construis();
+        await testeurMAC.entrepots
+          .aidants()
+          .persiste(aidantFinalisantSonCompte);
+        testeurMAC.adaptateurDeVerificationDeSession.utilisateurConnecte(
+          aidantFinalisantSonCompte,
+        );
+
+        const reponse = await executeRequete(
+          donneesServeur.app,
+          'POST',
+          `/api/utilisateur/finalise`,
+          donneesServeur.portEcoute,
+          { cguSignees: true, motDePasse: 'EGLW5R0ITVRXKL%#>CPD' },
+        );
+
+        expect(reponse.statusCode).toBe(422);
+        expect(await reponse.json()).toStrictEqual({
+          message: 'Le mot de passe est trop simple.',
+        });
+      });
+
+      it('vérifie que le mot de passe contient au moins une majuscule', async () => {
+        FournisseurHorlogeDeTest.initialise(
+          new Date(Date.parse('2024-02-04T13:04:25+01:00')),
+        );
+        const aidantFinalisantSonCompte = unAidant()
+          .avecCompteEnAttenteDeFinalisation()
+          .construis();
+        await testeurMAC.entrepots
+          .aidants()
+          .persiste(aidantFinalisantSonCompte);
+        testeurMAC.adaptateurDeVerificationDeSession.utilisateurConnecte(
+          aidantFinalisantSonCompte,
+        );
+
+        const reponse = await executeRequete(
+          donneesServeur.app,
+          'POST',
+          `/api/utilisateur/finalise`,
+          donneesServeur.portEcoute,
+          { cguSignees: true, motDePasse: 'eglw5r0itvrxkl%#>cpd' },
+        );
+
+        expect(reponse.statusCode).toBe(422);
+        expect(await reponse.json()).toStrictEqual({
+          message: 'Le mot de passe est trop simple.',
+        });
+      });
+
+      it('vérifie que le mot de passe contient au moins un chiffre', async () => {
+        FournisseurHorlogeDeTest.initialise(
+          new Date(Date.parse('2024-02-04T13:04:25+01:00')),
+        );
+        const aidantFinalisantSonCompte = unAidant()
+          .avecCompteEnAttenteDeFinalisation()
+          .construis();
+        await testeurMAC.entrepots
+          .aidants()
+          .persiste(aidantFinalisantSonCompte);
+        testeurMAC.adaptateurDeVerificationDeSession.utilisateurConnecte(
+          aidantFinalisantSonCompte,
+        );
+
+        const reponse = await executeRequete(
+          donneesServeur.app,
+          'POST',
+          `/api/utilisateur/finalise`,
+          donneesServeur.portEcoute,
+          { cguSignees: true, motDePasse: 'EgLwCRDItVRxkl%#>cPd' },
+        );
+
+        expect(reponse.statusCode).toBe(422);
+        expect(await reponse.json()).toStrictEqual({
+          message: 'Le mot de passe est trop simple.',
+        });
+      });
+
+      it('vérifie que le mot de passe contient au moins un caractère spécial', async () => {
+        FournisseurHorlogeDeTest.initialise(
+          new Date(Date.parse('2024-02-04T13:04:25+01:00')),
+        );
+        const aidantFinalisantSonCompte = unAidant()
+          .avecCompteEnAttenteDeFinalisation()
+          .construis();
+        await testeurMAC.entrepots
+          .aidants()
+          .persiste(aidantFinalisantSonCompte);
+        testeurMAC.adaptateurDeVerificationDeSession.utilisateurConnecte(
+          aidantFinalisantSonCompte,
+        );
+
+        const reponse = await executeRequete(
+          donneesServeur.app,
+          'POST',
+          `/api/utilisateur/finalise`,
+          donneesServeur.portEcoute,
+          { cguSignees: true, motDePasse: 'EgLwCRD0tVRxklAbCcPd' },
+        );
+
+        expect(reponse.statusCode).toBe(422);
+        expect(await reponse.json()).toStrictEqual({
+          message: 'Le mot de passe est trop simple.',
+        });
       });
     });
 
@@ -78,7 +221,7 @@ describe('le serveur MAC sur les routes /api/utilisateur', () => {
         'POST',
         `/api/utilisateur/finalise`,
         donneesServeur.portEcoute,
-        { cguSignees: false },
+        { cguSignees: false, motDePasse: 'EgLw5R0ItVRxkl%#>cPd' },
       );
 
       expect(reponse.statusCode).toBe(422);
