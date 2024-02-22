ef224bd4ddcaf4dbba27f4b3fc1a4f52bbb74dd3
tomi <thomas.bennett@caminu.fr>
2024-02-22 17:08:13 +0100

    [COMMANDE] permet de chiffrer et dechiffrer des secrets en ligne de commande

---
+++ mon-aide-cyber-api/src/administration/chiffrement/commande.ts
@@ -0,0 +1,23 @@
+import { program } from 'commander';
+import { adaptateurServiceChiffrement } from '../../infrastructure/adaptateurs/adaptateurServiceChiffrement';
+
+program
+  .option(
+    '-c, --chiffre <chaineAChiffrer>',
+    'chiffre la chaine passée en paramètre',
+  )
+  .option(
+    '-d, --dechiffre <chaineADechiffrer>',
+    'déchiffre la chaine passée en paramètre',
+  )
+  .parse(process.argv);
+
+const options = program.opts();
+
+if (options.chiffre) {
+  console.log(adaptateurServiceChiffrement().chiffre(options.chiffre));
+}
+
+if (options.dechiffre) {
+  console.log(adaptateurServiceChiffrement().dechiffre(options.dechiffre));
+}
