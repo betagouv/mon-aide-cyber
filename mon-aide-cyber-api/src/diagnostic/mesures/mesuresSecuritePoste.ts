d9df5232872e2d07ae22ba9e92eaa8f011557c90
Bertrand Bougon <bertrandbougon@gmail.com>
2024-02-29 09:21:41 +0100

    [DIAGNOSTIC][MESURES] Mets à jour les mesures liées à la sécurité des postes

--- mon-aide-cyber-api/src/diagnostic/mesures/mesuresSecuritePoste.ts
+++ mon-aide-cyber-api/src/diagnostic/mesures/mesuresSecuritePoste.ts
@@ -2,7 +2,7 @@ export const mesuresSecuritePoste = {
   'securite-poste-maj-fonctionnelles-et-securite-deployees': {
     niveau1: {
       titre:
-        'Déployer systématiquement toutes les mises à jour sur les postes de travail dès que celles-ci sont disponibles et hors exceptions spécifiquement identifiées',
+        'Déployer systématiquement toutes les mises à jour sur les postes de travail dès que celles-ci sont disponibles',
       pourquoi:
         '../../mesures/postes/securite-poste-maj-fonctionnelles-et-securite-deployees-niveau1-pourquoi.pug',
       comment:
@@ -21,7 +21,7 @@ export const mesuresSecuritePoste = {
   'securite-poste-si-industriel-maj-fonctionnelles-et-securite-deployees': {
     niveau1: {
       titre:
-        'Déployer systématiquement toutes les mises à jour sur les postes de travail dédiés aux systèmes industriels dès que celles-ci sont disponibles et hors exceptions spécifiquement identifiées',
+        'Déployer systématiquement toutes les mises à jour sur les postes de travail dédiés aux systèmes industriels dès que celles-ci sont disponibles',
       pourquoi:
         '../../mesures/postes/securite-poste-si-industriel-maj-fonctionnelles-et-securite-deployees-niveau1-pourquoi.pug',
       comment:
@@ -40,7 +40,7 @@ export const mesuresSecuritePoste = {
   'securite-poste-antivirus-deploye': {
     niveau1: {
       titre:
-        'Installer de manière systématique un antivirus sur les postes de travail, vérifier régulièrement leur bon fonctionnement et leurs mises à jour.',
+        'Installer de manière systématique un antivirus sur les postes de travail',
       pourquoi:
         '../../mesures/postes/securite-poste-antivirus-deploye-niveau1-pourquoi.pug',
       comment:
@@ -76,7 +76,7 @@ export const mesuresSecuritePoste = {
   'securite-poste-pare-feu-local-active': {
     niveau1: {
       titre:
-        'Activer systématiquement le pare-feu local sur les postes de tavail avec comme règle générale d’interdire par défaut les flux entrants. Vérifier régulièrement leur activation sur tous les postes de travail.',
+        'Activer systématiquement le pare-feu local sur les postes de tavail',
       pourquoi:
         '../../mesures/postes/securite-poste-pare-feu-local-active-niveau1-pourquoi.pug',
       comment:
@@ -104,7 +104,7 @@ export const mesuresSecuritePoste = {
   },
   'securite-poste-r-et-d-disques-chiffres': {
     niveau1: {
-      titre: 'Chiffrer les disques durs des matériels nomades.',
+      titre: 'Chiffrer les disques durs des matériels nomades',
       pourquoi:
         '../../mesures/postes/securite-poste-r-et-d-disques-chiffres-niveau1-pourquoi.pug',
       comment:
