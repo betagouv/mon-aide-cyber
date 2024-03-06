829eb4063abe3395532167d3e36643856fc2fee5
Bertrand Bougon <bertrandbougon@gmail.com>
2024-03-06 12:39:30 +0100

    [CORRECTION] Ajoute un test storybook sur l'authentification

--- mon-aide-cyber-ui/src/stories/AuthentificationTentativeDeConnexion.stories.tsx
+++ mon-aide-cyber-ui/src/stories/AuthentificationTentativeDeConnexion.stories.tsx
@@ -6,13 +6,17 @@ import { ReponseAuthentification } from '../domaine/authentification/Authentific
 import { expect } from '@storybook/jest';
 import { ComposantAffichageErreur } from '../composants/erreurs/ComposantAffichageErreur.tsx';
 import { ErrorBoundary } from 'react-error-boundary';
-import { BrowserRouter } from 'react-router-dom';
+import { MemoryRouter, Route, Routes } from 'react-router-dom';
 import { FournisseurAuthentification } from '../fournisseurs/ContexteAuthentification.tsx';
 import { RequiertAuthentification } from '../fournisseurs/RequiertAuthentification.tsx';
 import { Diagnostic } from '../domaine/diagnostic/Diagnostic.ts';
 import { ContexteMacAPI } from '../fournisseurs/api/ContexteMacAPI.tsx';
 import { ParametresAPI } from '../fournisseurs/api/ConstructeurParametresAPI.ts';
 import { ContexteNavigationMAC } from '../fournisseurs/ContexteNavigationMAC.tsx';
+import { Suspense } from 'react';
+import { ComposantIntercepteur } from '../composants/intercepteurs/ComposantIntercepteur.tsx';
+import { TableauDeBord } from '../composants/TableauDeBord.tsx';
+import { Header } from '../composants/Header.tsx';
 
 const meta = {
   title: 'Authentification',
@@ -25,41 +29,88 @@ const meta = {
 export default meta;
 type Story = StoryObj<typeof meta>;
 
+let estConnecte = false;
 export const ConnexionAMonAideCyber: Story = {
   decorators: [
     (story) => (
-      <BrowserRouter>
+      <MemoryRouter>
         <ContexteMacAPI.Provider
           value={{
             appelle: async <T = Diagnostic, V = void>(
-              _parametresAPI: ParametresAPI<V>,
+              parametresAPI: ParametresAPI<V>,
               _: (contenu: Promise<any>) => Promise<T>,
             ) => {
               const reponseAuthentification: ReponseAuthentification = {
                 nomPrenom: 'Jean Dupont',
-                liens: {},
+                liens: {
+                  'lancer-diagnostic': {
+                    url: '/api/diagnostic',
+                    route: '/tableau-de-bord',
+                  },
+                },
               };
-              return reponseAuthentification as T;
+              if (parametresAPI.url === '/api/utilisateur' && estConnecte) {
+                return reponseAuthentification as T;
+              }
+              if (
+                parametresAPI.url === '/api/token' &&
+                parametresAPI.methode === 'POST'
+              ) {
+                return reponseAuthentification as T;
+              }
+              return Promise.reject({
+                liens: {
+                  'se-connecter': { url: '/api/token', methode: 'POST' },
+                },
+              });
             },
           }}
         >
           <ContexteNavigationMAC.Provider
             value={{
-              etat: { 'se-connecter': { url: '' } },
+              etat: {
+                'se-connecter': { url: '/api/token', methode: 'POST' },
+                'lancer-diagnsotic': {
+                  url: '/api/diagnostic',
+                  methode: 'POST',
+                },
+              },
               setEtat: () => {
                 return;
               },
+              // eslint-disable-next-line @typescript-eslint/no-empty-function
+              navigue: (_moteurDeLiens, _action, _exclusion) => {},
+              // eslint-disable-next-line @typescript-eslint/no-empty-function
+              retourAccueil: () => {},
             }}
           >
             <FournisseurAuthentification>
-              <RequiertAuthentification />
               <ErrorBoundary FallbackComponent={ComposantAffichageErreur}>
-                <PortailModale>{story()}</PortailModale>
+                <PortailModale>
+                  <Routes>
+                    <Route path="/" element={<Header />} />
+                    <Route
+                      element={
+                        <Suspense>
+                          <RequiertAuthentification />
+                        </Suspense>
+                      }
+                    >
+                      <Route
+                        path="/tableau-de-bord"
+                        element={
+                          <ComposantIntercepteur composant={TableauDeBord} />
+                        }
+                      ></Route>
+                    </Route>
+                  </Routes>
+                  {story()}
+                </PortailModale>
               </ErrorBoundary>
             </FournisseurAuthentification>
           </ContexteNavigationMAC.Provider>
         </ContexteMacAPI.Provider>
-      </BrowserRouter>
+      </MemoryRouter>
     ),
   ],
   name: 'Tente de se connecter à MonAideCyber',
@@ -73,7 +124,10 @@ export const ConnexionAMonAideCyber: Story = {
 
     await step("Lorsque l'aidant se connecte", async () => {
       await userEvent.click(
-        canvas.getByRole('link', { name: /Se connecter/i }),
+        canvas.getAllByRole('link', { name: /Se connecter/i })[1],
+      );
+      await waitFor(() =>
+        expect(canvas.getByText(/connectez vous/i)).toBeInTheDocument(),
       );
       const champsAdresseEmail = await waitFor(() =>
         canvas.getByRole('textbox', {
@@ -89,9 +143,17 @@ export const ConnexionAMonAideCyber: Story = {
       userEvent.type(champsAdresseEmail, aidant.identifiant);
       userEvent.type(champsMotDePasse, aidant.motDePasse);
       userEvent.click(canvas.getByRole('button', { name: /se connecter/i }));
+      estConnecte = true;
 
       await waitFor(() =>
-        expect(canvas.queryByText(/connectez vous/i)).not.toBeInTheDocument(),
+        expect(
+          canvas.queryByRole('button', {
+            name: /se connnecter/i,
+          }),
+        ).not.toBeInTheDocument(),
+      );
+      await waitFor(() =>
+        expect(canvas.queryByText(/Jean D\./i)).toBeInTheDocument(),
       );
     });
   },
