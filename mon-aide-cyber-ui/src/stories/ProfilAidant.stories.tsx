ffead0eab7f789e904a1935cfdb2d9195913d081
Bertrand Bougon <bertrandbougon@gmail.com>
2024-03-11 16:30:57 +0100

    [PROFIL] Ajoute un test storybook sur la page profil de l'Aidant

--- mon-aide-cyber-ui/src/stories/ProfilAidant.stories.tsx
+++ mon-aide-cyber-ui/src/stories/ProfilAidant.stories.tsx
@@ -4,19 +4,129 @@ import { ContexteMacAPI } from '../fournisseurs/api/ContexteMacAPI.tsx';
 import { ParametresAPI } from '../fournisseurs/api/ConstructeurParametresAPI.ts';
 import { ContexteNavigationMAC } from '../fournisseurs/ContexteNavigationMAC.tsx';
 import { userEvent, waitFor, within } from '@storybook/testing-library';
-import { ComposantFormulaireModificationMotDePasse } from '../composants/profil/ComposantFormulaireModificationMotDePasse.tsx';
+import { ComposantProfil } from '../composants/profil/ComposantProfil.tsx';
+import { ComposantAffichageErreur } from '../composants/alertes/ComposantAffichageErreur.tsx';
+import { ErrorBoundary } from 'react-error-boundary';
+import { Liens } from '../domaine/Lien.ts';
 
 const meta = {
-  title: "Modification du mot de passe de l'Aidant",
-  component: ComposantFormulaireModificationMotDePasse,
+  title: "Page profil de l'Aidant",
+  component: ComposantProfil,
   parameters: {
     layout: 'fullscreen',
   },
-} satisfies Meta<typeof ComposantFormulaireModificationMotDePasse>;
+} satisfies Meta<typeof ComposantProfil>;
 
 export default meta;
 type Story = StoryObj<typeof meta>;
 let valeursSaisies = {};
+
+export const AffichagePageProfil: Story = {
+  decorators: [
+    (story) => (
+      <ContexteMacAPI.Provider
+        value={{
+          appelle: async <T = any, V = void>(
+            _parametresAPI: ParametresAPI<V>,
+            _: (contenu: Promise<any>) => Promise<T>
+          ) => {
+            return {
+              nomPrenom: 'Jean Dupont',
+              dateSignatureCGU: '11.03.2024',
+              identifiantConnexion: 'j.dup@mail.com',
+              liens: {
+                'lancer-diagnostic': {
+                  url: '/api/diagnostic',
+                  methode: 'POST',
+                },
+                'modifier-mot-de-passe': {
+                  url: '/api/profil/modifier-mot-de-passe',
+                  methode: 'POST',
+                },
+                'se-deconnecter': {
+                  url: '/api/token',
+                  methode: 'DELETE',
+                },
+              },
+            } as T;
+          },
+        }}
+      >
+        <ContexteNavigationMAC.Provider
+          value={{
+            etat: {
+              'afficher-profil': {
+                url: '/api/afficher-profil',
+                methode: 'GET',
+              },
+            },
+            setEtat: () => {
+              return;
+            },
+            ajouteEtat: (_liens: Liens) => {
+              return;
+            },
+            // eslint-disable-next-line @typescript-eslint/no-empty-function
+            navigue: (_moteurDeLiens, _action, _exclusion) => {},
+            // eslint-disable-next-line @typescript-eslint/no-empty-function
+            retourAccueil: () => {},
+          }}
+        >
+          <ErrorBoundary FallbackComponent={ComposantAffichageErreur}>
+            {story()}
+          </ErrorBoundary>
+        </ContexteNavigationMAC.Provider>
+      </ContexteMacAPI.Provider>
+    ),
+  ],
+  name: "Affiche la page profil de l'Aidant",
+  play: async ({ canvasElement, step }) => {
+    const canvas = within(canvasElement);
+
+    await step("Avec les informations de l'Aidant", async () => {
+      await waitFor(() =>
+        expect(
+          canvas.getByText(/compte crée le 11.03.2024/i)
+        ).toBeInTheDocument()
+      );
+      await waitFor(() =>
+        expect(
+          canvas.getByRole('button', { name: /accéder au tableau de bord/i })
+        ).toBeInTheDocument()
+      );
+      await waitFor(() =>
+        expect(canvas.getByText(/jean/i)).toBeInTheDocument()
+      );
+      await waitFor(() =>
+        expect(canvas.getByText(/dupont/i)).toBeInTheDocument()
+      );
+      await waitFor(() =>
+        expect(canvas.getByText(/j.dup@mail.com/i)).toBeInTheDocument()
+      );
+    });
+
+    await step(
+      "Avec le formulaire de modification de mot de passe de l'Aidant",
+      async () => {
+        expect(
+          canvas.getByRole('textbox', {
+            name: /saisissez votre ancien mot de passe/i,
+          })
+        ).toBeInTheDocument();
+        expect(
+          canvas.getByRole('textbox', {
+            name: /choisissez un nouveau mot de passe/i,
+          })
+        ).toBeInTheDocument();
+        expect(
+          canvas.getByRole('textbox', {
+            name: /confirmez votre nouveau mot de passe/i,
+          })
+        ).toBeInTheDocument();
+      }
+    );
+  },
+};
 
 export const ModificationMotDePasseAidant: Story = {
   decorators: [
@@ -25,7 +135,7 @@ export const ModificationMotDePasseAidant: Story = {
         value={{
           appelle: async <T = any, V = void>(
             parametresAPI: ParametresAPI<V>,
-            _: (contenu: Promise<any>) => Promise<T>,
+            _: (contenu: Promise<any>) => Promise<T>
           ) => {
             valeursSaisies = parametresAPI.corps!;
             return Promise.resolve({ liens: { url: '' } }) as Promise<T>;
@@ -43,13 +153,18 @@ export const ModificationMotDePasseAidant: Story = {
             setEtat: () => {
               return;
             },
+            ajouteEtat: (_liens: Liens) => {
+              return;
+            },
             // eslint-disable-next-line @typescript-eslint/no-empty-function
             navigue: (_moteurDeLiens, _action, _exclusion) => {},
             // eslint-disable-next-line @typescript-eslint/no-empty-function
             retourAccueil: () => {},
           }}
         >
-          {story()}
+          <ErrorBoundary FallbackComponent={ComposantAffichageErreur}>
+            {story()}
+          </ErrorBoundary>
         </ContexteNavigationMAC.Provider>
       </ContexteMacAPI.Provider>
     ),
@@ -59,39 +174,18 @@ export const ModificationMotDePasseAidant: Story = {
     const canvas = within(canvasElement);
 
     await step(
-      "Lorsque l'on arrive sur la page de création de l'aidant",
-      async () => {
-        expect(
-          canvas.getByRole('textbox', {
-            name: /saisissez votre ancien mot de passe/i,
-          }),
-        ).toBeInTheDocument();
-        expect(
-          canvas.getByRole('textbox', {
-            name: /choisissez un nouveau mot de passe/i,
-          }),
-        ).toBeInTheDocument();
-        expect(
-          canvas.getByRole('textbox', {
-            name: /confirmez votre nouveau mot de passe/i,
-          }),
-        ).toBeInTheDocument();
-      },
-    );
-
-    await step(
       "L'Aidant clique sur Modifier le mot de passe sans rien remplir",
       async () => {
         userEvent.click(
-          canvas.getByRole('button', { name: /modifier le mot de passe/i }),
+          canvas.getByRole('button', { name: /modifier le mot de passe/i })
         );
 
         await waitFor(() =>
           expect(
-            canvas.getByText(/vous devez saisir vos mots de passe./i),
-          ).toBeInTheDocument(),
+            canvas.getByText(/vous devez saisir vos mots de passe./i)
+          ).toBeInTheDocument()
         );
-      },
+      }
     );
 
     await step("L'Aidant se trompe dans la confirmation", async () => {
@@ -109,15 +203,15 @@ export const ModificationMotDePasseAidant: Story = {
       userEvent.type(champConfirmationMotDePasse, 'c');
 
       userEvent.click(
-        canvas.getByRole('button', { name: /modifier le mot de passe/i }),
+        canvas.getByRole('button', { name: /modifier le mot de passe/i })
       );
 
       await waitFor(() =>
         expect(
           canvas.getByText(
-            /la confirmation de votre mot de passe ne correspond pas au mot de passe saisi./i,
-          ),
-        ).toBeInTheDocument(),
+            /la confirmation de votre mot de passe ne correspond pas au mot de passe saisi./i
+          )
+        ).toBeInTheDocument()
       );
     });
 
@@ -141,17 +235,17 @@ export const ModificationMotDePasseAidant: Story = {
         userEvent.type(champConfirmationMotDePasse, 'a');
 
         userEvent.click(
-          canvas.getByRole('button', { name: /modifier le mot de passe/i }),
+          canvas.getByRole('button', { name: /modifier le mot de passe/i })
         );
 
         await waitFor(() =>
           expect(
             canvas.getByText(
-              /votre nouveau mot de passe doit être différent de votre ancien mot de passe./i,
-            ),
-          ).toBeInTheDocument(),
+              /votre nouveau mot de passe doit être différent de votre ancien mot de passe./i
+            )
+          ).toBeInTheDocument()
         );
-      },
+      }
     );
 
     await step("L'Aidant modifie son mot de passe", async () => {
@@ -172,20 +266,20 @@ export const ModificationMotDePasseAidant: Story = {
       userEvent.type(champConfirmationMotDePasse, 'b');
 
       userEvent.click(
-        canvas.getByRole('button', { name: /modifier le mot de passe/i }),
+        canvas.getByRole('button', { name: /modifier le mot de passe/i })
       );
 
       await waitFor(() =>
         expect(
-          canvas.queryByText(/mot de passe modifié avec succès/i),
-        ).not.toBeInTheDocument(),
+          canvas.queryByText(/mot de passe modifié avec succès/i)
+        ).not.toBeInTheDocument()
       );
       await waitFor(() =>
         expect(valeursSaisies).toStrictEqual({
           ancienMotDePasse: 'a',
           motDePasse: 'b',
           confirmationMotDePasse: 'b',
-        }),
+        })
       );
     });
   },
