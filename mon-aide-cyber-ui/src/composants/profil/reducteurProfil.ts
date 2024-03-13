f188120d91aefb08355285320181578055b63a52
Bertrand Bougon <bertrandbougon@gmail.com>
2024-03-13 12:33:36 +0100

    [CORRECTION] Gère les erreurs lors de l'appel à l'API profil

--- mon-aide-cyber-ui/src/composants/profil/reducteurProfil.ts
+++ mon-aide-cyber-ui/src/composants/profil/reducteurProfil.ts
@@ -10,18 +10,26 @@ export type EtatProfil = {
 
 enum TypeActionProfil {
   PROFIL_CHARGE = 'PROFIL_CHARGE',
+  PROFIL_CHARGE_EN_ERREUR = 'PROFIL_CHARGE_EN_ERREUR',
 }
 
-type ActionProfil = {
-  type: TypeActionProfil.PROFIL_CHARGE;
-  profil: Profil;
-};
+type ActionProfil =
+  | {
+      type: TypeActionProfil.PROFIL_CHARGE;
+      profil: Profil;
+    }
+  | {
+      type: TypeActionProfil.PROFIL_CHARGE_EN_ERREUR;
+    };
 
 export const reducteurProfil = (
   etat: EtatProfil,
   action: ActionProfil,
 ): EtatProfil => {
   switch (action.type) {
+    case TypeActionProfil.PROFIL_CHARGE_EN_ERREUR: {
+      return { ...etat, enCoursDeChargement: false };
+    }
     case TypeActionProfil.PROFIL_CHARGE: {
       const nomPrenomSepare = action.profil.nomPrenom.split(' ');
       const prenom = nomPrenomSepare[0];
@@ -43,5 +51,11 @@ export const profilCharge = (profil: Profil): ActionProfil => {
   return {
     type: TypeActionProfil.PROFIL_CHARGE,
     profil,
+  };
+};
+
+export const profilChargeEnErreur = (): ActionProfil => {
+  return {
+    type: TypeActionProfil.PROFIL_CHARGE_EN_ERREUR,
   };
 };
