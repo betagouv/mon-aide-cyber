5febbbf570b7e8c923b9a7ff589f669c9e95a35b
tomi <thomas.bennett@caminu.fr>
2024-03-14 17:14:16 +0100

    [REMANIEMENT] nettoie les protections de routes avec/sans espace

--- mon-aide-cyber-ui/src/fournisseurs/RequiertAidantSansEspace.tsx
+++ mon-aide-cyber-ui/src/fournisseurs/RequiertAidantSansEspace.tsx
@@ -2,38 +2,21 @@ import { useNavigationMAC } from './hooks.ts';
 import { ReactElement, useEffect, useState } from 'react';
 import { MoteurDeLiens } from '../domaine/MoteurDeLiens.ts';
 import { Outlet } from 'react-router-dom';
-import { Lien } from '../domaine/Lien.ts';
 
 export const RequiertAidantSansEspace = () => {
   const navigationMAC = useNavigationMAC();
-  // const authentification = useAuthentification();
   const [redirection, setRedirection] = useState<ReactElement>(<></>);
 
   useEffect(() => {
     const moteurDeLiens = new MoteurDeLiens(navigationMAC.etat);
     moteurDeLiens.trouve(
       'creer-espace-aidant',
-      (_lien: Lien) => setRedirection(<Outlet />),
+      () => setRedirection(<Outlet />),
       () =>
-        moteurDeLiens.trouve('lancer-diagnostic', (_lien) =>
+        moteurDeLiens.trouve('lancer-diagnostic', () =>
           navigationMAC.navigue(moteurDeLiens, 'lancer-diagnostic'),
         ),
     );
-    // authentification
-    //   .appelleUtilisateur()
-    //   .then((utilisateur) => {
-    //     const moteurDeLiens = new MoteurDeLiens(utilisateur.liens);
-    //     const creerEspaceAidant = moteurDeLiens.trouve('creer-espace-aidant');
-    //
-    //     if (!creerEspaceAidant) {
-    //       navigationMAC.navigue(moteurDeLiens, 'lancer-diagnostic');
-    //     } else {
-    //       setRedirection(<Outlet />);
-    //     }
-    //   })
-    //   .catch(() => {
-    //     navigationMAC.retourAccueil();
-    //   });
   }, [navigationMAC]);
 
   return redirection;
--- mon-aide-cyber-ui/src/fournisseurs/RequiertEspaceAidant.tsx
+++ mon-aide-cyber-ui/src/fournisseurs/RequiertEspaceAidant.tsx
@@ -2,38 +2,21 @@ import { useNavigationMAC } from './hooks.ts';
 import { ReactElement, useEffect, useState } from 'react';
 import { MoteurDeLiens } from '../domaine/MoteurDeLiens.ts';
 import { Outlet } from 'react-router-dom';
-import { Lien } from '../domaine/Lien.ts';
 
 export const RequiertEspaceAidant = () => {
   const navigationMAC = useNavigationMAC();
-  // const authentification = useAuthentification();
   const [redirection, setRedirection] = useState<ReactElement>(<></>);
 
   useEffect(() => {
     new MoteurDeLiens(navigationMAC.etat).trouve(
       'creer-espace-aidant',
-      (_lien: Lien) =>
+      () =>
         navigationMAC.navigue(
           new MoteurDeLiens(navigationMAC.etat),
           'creer-espace-aidant',
         ),
       () => setRedirection(<Outlet />),
     );
-    // authentification
-    //   .appelleUtilisateur()
-    //   .then((utilisateur) => {
-    //     const moteurDeLiens = new MoteurDeLiens(utilisateur.liens);
-    //     const creerEspaceAidant = moteurDeLiens.trouve('creer-espace-aidant');
-    //
-    //     if (creerEspaceAidant) {
-    //       navigationMAC.navigue(moteurDeLiens, 'creer-espace-aidant');
-    //     } else {
-    //       setRedirection(<Outlet />);
-    //     }
-    //   })
-    //   .catch(() => {
-    //     // navigationMAC.retourAccueil();
-    //   });
   }, [navigationMAC]);
 
   return redirection;
