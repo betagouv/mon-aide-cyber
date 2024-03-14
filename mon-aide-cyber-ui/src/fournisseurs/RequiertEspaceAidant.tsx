199366d59a5a454f052541ce2737ae0138220c13
Bertrand Bougon <bertrandbougon@gmail.com>
2024-03-14 15:42:46 +0100

    [REMANIEMENT] Applique la nouvelle signature du moteur de liens pour trouver les actions à
    - ComposantLancerDiagnostic
    - RequiertAidantSansEspace
    - RequiertEspaceAidant

--- mon-aide-cyber-ui/src/fournisseurs/RequiertEspaceAidant.tsx
+++ mon-aide-cyber-ui/src/fournisseurs/RequiertEspaceAidant.tsx
@@ -1,30 +1,40 @@
-import { useAuthentification, useNavigationMAC } from './hooks.ts';
+import { useNavigationMAC } from './hooks.ts';
 import { ReactElement, useEffect, useState } from 'react';
 import { MoteurDeLiens } from '../domaine/MoteurDeLiens.ts';
 import { Outlet } from 'react-router-dom';
+import { Lien } from '../domaine/Lien.ts';
 
 export const RequiertEspaceAidant = () => {
   const navigationMAC = useNavigationMAC();
-  const authentification = useAuthentification();
+  // const authentification = useAuthentification();
   const [redirection, setRedirection] = useState<ReactElement>(<></>);
 
   useEffect(() => {
-    authentification
-      .appelleUtilisateur()
-      .then((utilisateur) => {
-        const moteurDeLiens = new MoteurDeLiens(utilisateur.liens);
-        const creerEspaceAidant = moteurDeLiens.trouve('creer-espace-aidant');
-
-        if (creerEspaceAidant) {
-          navigationMAC.navigue(moteurDeLiens, 'creer-espace-aidant');
-        } else {
-          setRedirection(<Outlet />);
-        }
-      })
-      .catch(() => {
-        navigationMAC.retourAccueil();
-      });
-  }, [authentification, navigationMAC]);
+    new MoteurDeLiens(navigationMAC.etat).trouve(
+      'creer-espace-aidant',
+      (_lien: Lien) =>
+        navigationMAC.navigue(
+          new MoteurDeLiens(navigationMAC.etat),
+          'creer-espace-aidant',
+        ),
+      () => setRedirection(<Outlet />),
+    );
+    // authentification
+    //   .appelleUtilisateur()
+    //   .then((utilisateur) => {
+    //     const moteurDeLiens = new MoteurDeLiens(utilisateur.liens);
+    //     const creerEspaceAidant = moteurDeLiens.trouve('creer-espace-aidant');
+    //
+    //     if (creerEspaceAidant) {
+    //       navigationMAC.navigue(moteurDeLiens, 'creer-espace-aidant');
+    //     } else {
+    //       setRedirection(<Outlet />);
+    //     }
+    //   })
+    //   .catch(() => {
+    //     // navigationMAC.retourAccueil();
+    //   });
+  }, [navigationMAC]);
 
   return redirection;
 };
