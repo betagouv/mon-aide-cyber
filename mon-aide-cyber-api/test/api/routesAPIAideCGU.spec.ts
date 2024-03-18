2aab2e095e732b7d8d62e0b86b4710b8ce666a2b
Bertrand Bougon <bertrandbougon@gmail.com>
2024-03-18 16:52:37 +0100

    [PARCOURS CGU AIDÉ] Vérifie que l’Aidé a bien signé les CGU et renseigné les information nécessaires

--- mon-aide-cyber-api/test/api/routesAPIAideCGU.spec.ts
+++ mon-aide-cyber-api/test/api/routesAPIAideCGU.spec.ts
@@ -1,4 +1,4 @@
-import { describe, expect, it } from 'vitest';
+import { afterEach, beforeEach, describe, expect, it } from 'vitest';
 import { executeRequete } from './executeurRequete';
 import testeurIntegration from './testeurIntegration';
 import { Express } from 'express';
@@ -29,7 +29,7 @@ describe('Le serveur MAC, sur les routes CGU Aidé', () => {
           {
             cguValidees: true,
             email: 'jean.dupont@aide.com',
-            departement: '33000',
+            departement: 'Corse du sud',
             raisonSociale: 'beta-gouv',
           },
         );
@@ -40,6 +40,146 @@ describe('Le serveur MAC, sur les routes CGU Aidé', () => {
         expect(aides[0].dateSignatureCGU).toStrictEqual(
           FournisseurHorloge.maintenant(),
         );
+      });
+
+      describe("En ce qui concerne les informations envoyées par l'Aidé", () => {
+        const testeurMAC = testeurIntegration();
+        let donneesServeur: { portEcoute: number; app: Express };
+
+        beforeEach(() => {
+          donneesServeur = testeurMAC.initialise();
+        });
+
+        afterEach(() => testeurMAC.arrete());
+        it('Vérifie que les CGU sont signées', async () => {
+          const reponse = await executeRequete(
+            donneesServeur.app,
+            'POST',
+            '/api/aide/cgu',
+            donneesServeur.portEcoute,
+            {
+              cguValidees: false,
+              email: 'jean.dupont@aide.com',
+              departement: 'Paris',
+              raisonSociale: 'beta-gouv',
+            },
+          );
+
+          expect(reponse.statusCode).toBe(422);
+          expect(await reponse.json()).toStrictEqual({
+            message: 'Veuillez signer les CGU',
+            liens: {
+              'demander-validation-cgu-aide': {
+                url: '/api/aide/cgu',
+                methode: 'POST',
+              },
+            },
+          });
+        });
+
+        it("Vérifie que l'email est renseigné", async () => {
+          const reponse = await executeRequete(
+            donneesServeur.app,
+            'POST',
+            '/api/aide/cgu',
+            donneesServeur.portEcoute,
+            {
+              cguValidees: true,
+              email: 'ceci-n-est-pas-un-email',
+              departement: 'Gironde',
+              raisonSociale: 'beta-gouv',
+            },
+          );
+
+          expect(reponse.statusCode).toBe(422);
+          expect(await reponse.json()).toStrictEqual({
+            message: 'Veuillez renseigner votre Email',
+            liens: {
+              'demander-validation-cgu-aide': {
+                url: '/api/aide/cgu',
+                methode: 'POST',
+              },
+            },
+          });
+        });
+
+        it('Vérifie que le département est renseigné', async () => {
+          const reponse = await executeRequete(
+            donneesServeur.app,
+            'POST',
+            '/api/aide/cgu',
+            donneesServeur.portEcoute,
+            {
+              cguValidees: true,
+              email: 'jean.dupont@aide.com',
+              departement: '   ',
+              raisonSociale: 'beta-gouv',
+            },
+          );
+
+          expect(reponse.statusCode).toBe(422);
+          expect(await reponse.json()).toStrictEqual({
+            message:
+              "Veuillez renseigner le département de l'entité pour laquelle vous sollicitez une aide",
+            liens: {
+              'demander-validation-cgu-aide': {
+                url: '/api/aide/cgu',
+                methode: 'POST',
+              },
+            },
+          });
+        });
+
+        it('La raison sociale est optionnelle', async () => {
+          FournisseurHorlogeDeTest.initialise(
+            new Date(Date.parse('2024-02-29T14:04:17+01:00')),
+          );
+          const reponse = await executeRequete(
+            donneesServeur.app,
+            'POST',
+            '/api/aide/cgu',
+            donneesServeur.portEcoute,
+            {
+              cguValidees: true,
+              email: 'jean.dupont@aide.com',
+              departement: 'Finistère',
+            },
+          );
+
+          expect(reponse.statusCode).toBe(202);
+          const aides = await testeurMAC.entrepots.aides().tous();
+          expect(aides).toHaveLength(1);
+          expect(aides[0].dateSignatureCGU).toStrictEqual(
+            FournisseurHorloge.maintenant(),
+          );
+        });
+
+        it('La raison sociale, lorsque fournie ne peut pas être vide', async () => {
+          const reponse = await executeRequete(
+            donneesServeur.app,
+            'POST',
+            '/api/aide/cgu',
+            donneesServeur.portEcoute,
+            {
+              cguValidees: true,
+              email: 'jean.dupont@aide.com',
+              departement: 'Bas-Rhin',
+              raisonSociale: '    ',
+            },
+          );
+
+          expect(reponse.statusCode).toBe(422);
+          expect(await reponse.json()).toStrictEqual({
+            message:
+              "Veuillez renseigner la raison sociale de l'entité pour laquelle vous sollicitez une aide",
+            liens: {
+              'demander-validation-cgu-aide': {
+                url: '/api/aide/cgu',
+                methode: 'POST',
+              },
+            },
+          });
+        });
       });
     });
   });
