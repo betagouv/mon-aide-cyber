9d8eaa6f4ce6c9baaa464487e9a8bbbd957fe003
Bertrand Bougon <bertrandbougon@gmail.com>
2024-03-22 16:13:36 +0100

    [REMANIEMENT] Renomme les tests sur le capteur de demande de validation CGU Aidé

--- mon-aide-cyber-api/test/parcours-cgu-aide/CapteurSagaDemandeValidationCGUAide.spec.ts
+++ mon-aide-cyber-api/test/parcours-cgu-aide/CapteurSagaDemandeValidationCGUAide.spec.ts
@@ -1,31 +1,12 @@
-import { describe, it } from 'vitest';
+import { describe, expect, it } from 'vitest';
 import { EntrepotsMemoire } from '../../src/infrastructure/entrepots/memoire/EntrepotsMemoire';
 import { CapteurSagaDemandeValidationCGUAide } from '../../src/parcours-cgu-aide/CapteurSagaDemandeValidationCGUAide';
 import { BusCommandeMAC } from '../../src/infrastructure/bus/BusCommandeMAC';
 import { BusEvenementDeTest } from '../infrastructure/bus/BusEvenementDeTest';
 import { unAide } from '../aide/ConstructeurAide';
-import { BusCommandeTest } from '../infrastructure/bus/BusCommandeTest';
 
-describe('Capteur saga demande de validation de CGU aidés', () => {
-  it("publie la commande de recherche de l'aidé par email", async () => {
-    const busCommande = new BusCommandeTest();
-    const capteur = new CapteurSagaDemandeValidationCGUAide(
-      new EntrepotsMemoire(),
-      busCommande,
-      new BusEvenementDeTest()
-    );
-
-    await capteur.execute({
-      type: 'SagaDemandeValidationCGUAide',
-      cguValidees: true,
-      email: 'un email',
-      departement: 'un departement',
-    });
-
-    expect(busCommande.aRecu('CommandeRechercheAideParEmail')).toBe(true);
-  });
-
-  describe("si l'aidé est connu de MAC", () => {
+describe('Capteur saga demande de validation de CGU Aidé', () => {
+  describe("si l'Aidé est connu de MAC", () => {
     it('interrompt le parcours', async () => {
       const aide = unAide().construis();
       const entrepots = new EntrepotsMemoire();
@@ -33,7 +14,7 @@ describe('Capteur saga demande de validation de CGU aidés', () => {
       const capteur = new CapteurSagaDemandeValidationCGUAide(
         entrepots,
         new BusCommandeMAC(entrepots, busEvenement),
-        busEvenement
+        busEvenement,
       );
       await entrepots.aides().persiste(aide);
 
@@ -50,13 +31,15 @@ describe('Capteur saga demande de validation de CGU aidés', () => {
     });
   });
 
-  describe("si l'aidé n'est pas connu de MAC", () => {
-    it("publie la commande de création de l'aidé", async () => {
-      const busCommande = new BusCommandeTest();
+  describe("si l'Aidé n'est pas connu de MAC", () => {
+    it("crée l'Aidé", async () => {
+      const entrepotsMemoire = new EntrepotsMemoire();
+      const busEvenement = new BusEvenementDeTest();
+      const busCommande = new BusCommandeMAC(entrepotsMemoire, busEvenement);
       const capteur = new CapteurSagaDemandeValidationCGUAide(
-        new EntrepotsMemoire(),
+        entrepotsMemoire,
         busCommande,
-        new BusEvenementDeTest()
+        busEvenement,
       );
 
       await capteur.execute({
@@ -66,7 +49,9 @@ describe('Capteur saga demande de validation de CGU aidés', () => {
         departement: 'un departement',
       });
 
-      expect(busCommande.aRecu('CommandeCreerAide')).toBe(true);
+      expect(
+        await entrepotsMemoire.aides().rechercheParEmail('un email'),
+      ).not.toBeUndefined();
     });
   });
 });
