a720c04bb8384749b017fe2c96249f0046565fe8
Bertrand Bougon <bertrandbougon@gmail.com>
2024-04-12 11:03:41 +0200

    [DIAGNOSTIC] Ajoute un test de cohérence afin de s’assurer que le transcripteur contient toutes les questions du référentiel

---
+++ mon-aide-cyber-api/test/infrastructure/adaptateurs/transcripteur/adaptateurTranscripteur.spec.ts
@@ -0,0 +1,47 @@
+import { describe, expect } from 'vitest';
+import { referentiel } from '../../../../src/diagnostic/donneesReferentiel';
+import { adaptateurTranscripteur } from '../../../../src/infrastructure/adaptateurs/transcripteur/adaptateurTranscripteur';
+
+describe('Cohérence du transcripteur avec le référentiel', () => {
+  const toutesLesQuestions = (): {
+    thematique: string;
+    questions: { question: string }[];
+  }[] => {
+    return Object.entries(referentiel).flatMap(([thematique, questions]) => {
+      return {
+        thematique: thematique,
+        questions: questions.questions.flatMap((q) => ({
+          question: q.identifiant,
+        })),
+      };
+    });
+  };
+
+  const toutesLesQuestionsReferenceesDansLeTranscripteur: {
+    identifiantQuestion: string;
+  }[] = Object.entries(
+    adaptateurTranscripteur().transcripteur().thematiques,
+  ).flatMap(([, thematique]) =>
+    thematique.groupes.flatMap((groupe) =>
+      groupe.questions.flatMap((q) => ({
+        identifiantQuestion: q.identifiant,
+      })),
+    ),
+  );
+
+  describe.each(toutesLesQuestions())(
+    'Pour la thématique "$thematique"',
+    (thematique) => {
+      it.each(thematique.questions)(
+        'la question "$question" est référencée dans le transcripteur',
+        (question) => {
+          expect(
+            toutesLesQuestionsReferenceesDansLeTranscripteur.find(
+              (q) => q.identifiantQuestion === question.question,
+            ),
+          ).not.toBeUndefined();
+        },
+      );
+    },
+  );
+});
