46f03f9396f2af2f5264f82a5e194b5309eca74e
tomi <thomas.bennett@caminu.fr>
2024-04-24 10:47:48 +0200

    [TABLEAU-DE-BORD] Supprime /diagnostics dans l'API

--- mon-aide-cyber-api/src/api/routesAPI.ts
+++ mon-aide-cyber-api/src/api/routesAPI.ts
@@ -1,7 +1,6 @@
 import { routesAPIDiagnostic } from './routesAPIDiagnostic';
 import express, { Request, Router } from 'express';
 import { ConfigurationServeur } from '../serveur';
-import { routesAPIDiagnostics } from './routesAPIDiagnostics';
 import { routesAPIAuthentification } from './routesAPIAuthentification';
 import crypto from 'crypto';
 import { routesAPIUtilisateur } from './routesAPIUtilisateur';
@@ -17,7 +16,6 @@ const routesAPI = (configuration: ConfigurationServeur) => {
   const routes: Router = express.Router();
 
   routes.use('/diagnostic', routesAPIDiagnostic(configuration));
-  routes.use('/diagnostics', routesAPIDiagnostics(configuration));
   routes.use('/token', routesAPIAuthentification(configuration));
   routes.use('/utilisateur', routesAPIUtilisateur(configuration));
   routes.use('/espace-aidant', routesAPIEspaceAidant(configuration));
