5bca14aa27dc79b90fa4eacc6d14d4404d5f609a
Bertrand Bougon <bertrand.bougon@beta.gouv.fr>
2024-06-17 11:48:28 +0200

    [CHARTE AIDANT] Mets à jour la charte de l’Aidant conformément à la version du 20 Mars 2024

--- mon-aide-cyber-ui/src/vues/CharteAidant.tsx
+++ mon-aide-cyber-ui/src/vues/CharteAidant.tsx
@@ -17,31 +17,32 @@ export const CharteAidant = () => {
     <>
       <Header lienMAC={<LienMAC titre="Accueil - MonAideCyber" route="/" />} />
       <main role="main">
-        <div className="fr-container--fluid">
+        <div className="fr-container mac-contenu-charte">
           <div className="fr-grid-row fr-grid-row--gutters">
             <div
-              className="fr-col-offset-2 fr-col-offset-md-3 fr-col-8 fr-col-lg-6"
+              className="fr-col-offset-4 fr-col-offset-md-3 fr-col-8 fr-col-lg-9"
               id="top"
             >
-              <h1>La Charte de l’Aidant</h1>
+              <h3>La Charte de l’Aidant</h3>
               <hr />
               {telechargementCharte}
-              <h3>Démarche gratuite</h3>
+              <h5>Démarche gratuite</h5>
               <p>
-                Je réalise bénévolement et gratuitement l’accompagnement, le
-                diagnostic et un point d’étape à 6 mois (si l’entité aidée le
+                Je réalise bénévolement ou gratuitement, ou sans reste à charge,
+                le diagnostic et un point d’étape à 6 mois (si l’entité aidée le
                 souhaite) après le diagnostic auprès de l’entité aidée via
                 MonAideCyber.
               </p>
-              <h3>Respect de la méthodologie</h3>
+              <h5>Respect de la méthodologie</h5>
               <p>
                 Je m’engage à suivre rigoureusement les principes
                 méthodologiques proposés par MonAideCyber, à savoir :
               </p>
               <ul>
                 <li>
-                  Être dans une posture bienveillante, constructive et faisant
-                  preuve d’empathie et ne pas être dans une posture de contrôle,
+                  Adopter une posture bienveillante, constructive et faire
+                  preuve d’empathie et en étant en aucun cas dans une posture de
+                  contrôle,
                 </li>
                 <li>
                   Mener dans la mesure du possible le diagnostic MonAideCyber en
@@ -49,7 +50,7 @@ export const CharteAidant = () => {
                   informatique principal (si existant) de l’entité aidée,
                 </li>
                 <li>
-                  Réaliser le diagnostic en m’appuyant essentiellement sur la
+                  Réaliser le diagnostic en s’appuyant essentiellement sur la
                   liste des questions énumérées au travers des différentes
                   thématiques abordées par MonAideCyber (contexte, gouvernance,
                   sécurité des accès, sécurité des postes, sécurité des
@@ -57,41 +58,41 @@ export const CharteAidant = () => {
                   cyberattaque),
                 </li>
                 <li>
-                  Restituer les résultats du diagnostic et les 6 mesures de
-                  sécurité prioritaires qu’il est recommandé de mettre en œuvre
-                  dans les 6 mois,
+                  Restituer à l’entité aidée les résultats du diagnostic et les
+                  6 mesures de sécurité prioritaires recensées et conseillées à
+                  être menées sur les 6 prochains mois,
                 </li>
                 <li>
                   Mettre à disposition de l’entité les ressources et les
                   contacts utiles recensés par MonAideCyber,
                 </li>
                 <li>
-                  Proposer d’effectuer à l’entité un point de suivi 6 mois après
-                  le diagnostic,
+                  Proposer à l’entité aidée d’effectuer avec elle un point de
+                  suivi 6 mois après le diagnostic.
                 </li>
                 <li>
                   Ne pas créer de fichier permettant la corrélation entre un
                   identifiant d’une entité aidée et le nom de l’entité aidée,
                   sauf accord exprès de l’entité aidée. Dans ce dernier cas,
                   sécuriser un tel fichier de données de façon à éviter qu’il ne
-                  soit reproduit, altéré ou modifié par des personnes non
-                  autorisées.
+                  soit consulté, reproduit, altéré ou modifié par des personnes
+                  non autorisées.
                 </li>
               </ul>
-              <h3>Rappel des droits de l&apos;Entité aidée</h3>
+              <h5>Rappel des droits de l’Entité aidée</h5>
               <p>
                 En amont de chaque accompagnement via MonAideCyber, je m’engage
                 à rappeler auprès de l’entité aidée ses droits suivants :
               </p>
               <ul>
                 <li>
-                  La gratuité de l’accompagnement et du diagnostic réalisé via
-                  MonAideCyber,
+                  L’accompagnement et le diagnostic réalisé via MonAideCyber
+                  sont entièrement gratuits,
                 </li>
                 <li>
                   La possibilité d’accéder et de prendre connaissance de la
-                  charte de l’aidant MonAideCyber à tout moment sur le site
-                  internet de MonAideCyber,
+                  charte de l’aidant MonAideCyber disponible librement sur le
+                  site internet de MonAideCyber (cf. engagement suivant),
                 </li>
                 <li>
                   La possibilité de demander l’accompagnement d’un autre aidant
@@ -101,32 +102,26 @@ export const CharteAidant = () => {
                   diagnostic,
                 </li>
                 <li>
-                  La possibilité d’être mise en relation avec une entreprise
-                  labellisée Expert Cyber (référencée par le Groupement
-                  d’Intérêt Public Action contre la Cybermalveillance (GIP
-                  ACYMA) en s’appuyant sur le dispositif d’accompagnement de{' '}
-                  <a
-                    href="https://cybermalveillance.gouv.fr"
-                    target="_blank"
-                    rel="noreferrer"
-                  >
-                    cybermalveillance.gouv.fr
-                  </a>
-                  , ou tout autre prestataire de services qualifié par l’ANSSI,
-                  pour être accompagnée dans la mise en œuvre de mes mesures via
-                  le service MonAideCyber,
+                  La possibilité d’être mis en relation auprès d’une entreprise
+                  labellisée Expert Cyber (référencée par l’association ACYMA)
+                  en s’appuyant sur le dispositif d’accompagnement de
+                  cybermalveillance.gouv.fr, ou tout autre prestataire de
+                  services qualifié par l’ANSSI, ou d’un prestataire de
+                  proximité identifié par un Campus Cyber ou un CSIRT régional
+                  pour être accompagné dans la mise en œuvre de mes
+                  recommandations via le service MonAideCyber,
                 </li>
                 <li>
-                  Le fait qu’aucune proposition commerciale ne sera établie à
-                  mon initiative sauf s’il s’agit d’une demande de l’entité
-                  aidée, de sa propre initiative et en dehors du cadre de la
-                  démarche MonAideCyber,
+                  Aucune proposition commerciale ne sera établie à l’initiative
+                  de l’aidant ayant réalisé le diagnostic sauf s’il s’agit d’une
+                  demande de l’entité aidée, de sa propre initiative et en
+                  dehors du cadre de la démarche MonAideCyber,
                 </li>
                 <li>
                   La possibilité de prendre contact auprès d’un agent de l’ANSSI
                   pour remonter toute problématique et/ou écart de conduite de
-                  l’aidant constaté via le mail suivant : monaidecyber [at]
-                  ssi.gouv.fr
+                  l’aidant constaté via le mail suivant :{' '}
+                  <b>monaidecyber [at] ssi [point] gouv [point] fr</b>
                 </li>
                 <li>
                   La communication en fin de diagnostic MonAideCyber d’une
@@ -134,38 +129,37 @@ export const CharteAidant = () => {
                   chantiers prioritaires à mener, une liste de contacts, les
                   ressources complémentaires qui lui sont disponibles, un rappel
                   de ses droits et la procédure de remontée d’un écart de
-                  conduite d’un aidant,
+                  conduite d’un aidant.
                 </li>
                 <li>
                   Le droit de retirer le consentement mentionné au dernier
                   alinéa du paragraphe précédent.
                 </li>
               </ul>
-              <h3>
-                Communication de la charte de l&apos;Aidant auprès des Entités
-                aidées
-              </h3>
+              <h5>
+                Communication de la charte de l’Aidant auprès des Entités aidées
+              </h5>
               <p>
                 Je m’engage à transmettre à chaque entité aidée la présente
                 charte.
               </p>
-              <h3>
+              <h5>
                 Mise à disposition des ressources gratuites et aides
                 complémentaires
-              </h3>
+              </h5>
               <p>
-                Je m’engage à mettre à disposition de l’entité aidée les
-                ressources gratuites, les aides complémentaires et les
-                dispositifs d’accompagnement recensés par MonAideCyber.
+                J’engage à mettre à disposition de l’entité aidée les ressources
+                gratuites, les aides complémentaires et les dispositifs
+                d’accompagnement recensés par MonAideCyber.
               </p>
-              <h3>Obligation de neutralité</h3>
+              <h5>Obligation de neutralité</h5>
               <p>
                 Je m’engage à exercer mon rôle d’aidant MonAideCyber avec
                 dignité, impartialité, intégrité et probité. Je veille à rester
                 neutre et bienveillant auprès des entités aidées via
                 MonAideCyber.
               </p>
-              <h3>Aucune démarche commerciale directe</h3>
+              <h5>Aucune démarche commerciale directe</h5>
               <p>
                 Je ne fais pas de proposition commerciale de services
                 additionnels directement reliés à mon activité professionnelle.
@@ -175,14 +169,14 @@ export const CharteAidant = () => {
                 prendre. Je n’accorde aucun avantage pour des raisons d’ordre
                 privé.
               </p>
-              <h3>Condition pour une démarche commerciale indirecte</h3>
+              <h5>Condition pour une démarche commerciale indirecte</h5>
               <p>
                 Je ne propose pas mes services commerciaux de conseil, de
-                réalisation et de mise en œuvre des mesures sauf si cela est
-                expressément demandé par l’entité aidée de sa propre initiative
-                et en dehors du cadre de la démarche MonAideCyber.
+                réalisation et de mise en œuvre des mesures <b>sauf</b> si cela
+                est demandé par l’entité aidée de sa propre initiative et en
+                dehors du cadre de la démarche MonAideCyber.
               </p>
-              <h3>Aucun conflit d’intérêts</h3>
+              <h5>Aucun conflit d’intérêts</h5>
               <p>
                 Je veille à faire cesser immédiatement ou à prévenir les
                 situations de conflit d’intérêts* dans lesquelles je me trouve
@@ -204,12 +198,12 @@ export const CharteAidant = () => {
                   mon rôle d’aidant MonAideCyber.
                 </i>
               </p>
-              <h3>Devoirs de confidentialité</h3>
+              <h5>Devoirs de confidentialité</h5>
               <p>
                 J’observe un devoir de confidentialité des échanges avec les
                 entités aidées que j’accompagne via MonAideCyber. Je dois faire
                 preuve de discrétion professionnelle pour tous les faits et
-                informations dont j’ai connaissance dans l’exercice ou à
+                informations dont elles ont connaissance dans l’exercice ou à
                 l’occasion de l’exercice de mon rôle d’aidant MonAideCyber. Je
                 m’abstiens de divulguer à quiconque n’ayant ni le droit, ni le
                 besoin d’en connaître, sous quelque forme que ce soit, les
@@ -218,31 +212,34 @@ export const CharteAidant = () => {
                 toutes informations confiées par une entité aidée via
                 MonAideCyber, sauf accord formelle de celle-ci.
               </p>
-              <h3>Remontées auprès du référent MonAideCyber</h3>
+              <h5>Remontées auprès du référent MonAideCyber</h5>
               <p>
-                Je rends compte auprès de monaidecyber [at] ssi.gouv.fr, par
-                oral ou par écrit, de toutes difficultés rencontrées que ce soit
+                Je rends compte auprès de{' '}
+                <b>monaidecyber [at] ssi [point] gouv [point] fr</b>, par oral
+                ou par écrit, de toutes difficultés rencontrées que ce soit
                 durant l’accompagnement d’une entité aidée (ex : écart de
                 conduite de l’entité aidée durant le diagnostic), lors
                 d’interactions avec la communauté MonAideCyber ou dans la simple
-                utilisation du service MonAideCyber.
+                utilisation du service numérique.
               </p>
-              <h3>Disponibilité</h3>
+              <h5>Disponibilité</h5>
               <p>
                 Je m’engage à me rendre disponible pour réaliser à minima 2
                 diagnostics sur une durée de 12 mois.
               </p>
-              <h3>Engagement</h3>
+              <h5>Engagement</h5>
               <p>
                 Tous les ans et lorsque le service MonAideCyber me le demande, 1
-                mois avant la date anniversaire de mon engagement, je renouvelle
-                mon engagement dans cette démarche pour les 12 mois suivants. A
-                défaut de renouvellement 30 jours après que le service
+                mois avant anniversaire, je renouvelle tacitement mon engagement
+                dans cette démarche pour les 12 mois suivants. Si je ne
+                renouvelle pas mon engagement 30 jours après que le service
                 MonAideCyber me le demande, mon accès au service sera suspendu
-                automatiquement. Si je le souhaite, j’ai la possibilité de
-                retirer mon engagement à tout moment.
+                automatiquement.
+                <br />
+                Si je le souhaite, j’ai la possibilité de retirer mon engagement
+                à tout moment.
               </p>
-              <h3>Évolution de la démarche et de la présente charte</h3>
+              <h5>Évolution de la démarche et de la présente charte</h5>
               <p>
                 Je prendrai en compte les évolutions de la démarche MonAideCyber
                 et de la présente charte lorsque ces évolutions me seront
@@ -250,13 +247,13 @@ export const CharteAidant = () => {
                 proposées ne conviennent pas, j’ai la possibilité de retirer mon
                 engagement.
               </p>
-              <h3>Conditions de bannissement</h3>
+              <h5>Conditions de bannissement</h5>
               <p>
                 J’atteste être conscient que tout comportement ou manquement de
                 ma part qui serait contraire à la présente charte pourra
                 entraîner mon déréférencement de la communauté d’aidants
-                MonAideCyber, à la désactivation voire à la suppression de mes
-                accès au service MonAideCyber.
+                MonAideCyber, la désactivation voire suppression de mes accès au
+                service MonAideCyber.
               </p>
               <div className="carte fr-col-md-6">
                 <p>Télécharger la charte de l’Aidant au format pdf</p>
