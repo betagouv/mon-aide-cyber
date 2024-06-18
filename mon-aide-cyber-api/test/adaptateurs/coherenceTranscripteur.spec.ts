c796ae66dd709ad7e394922a0a51253714f041c5
Bertrand Bougon <bertrand.bougon@beta.gouv.fr>
2024-06-18 12:07:32 +0200

    [REFERENTIEL][TRANSCRIPTEUR] S’assure de la cohérence du transcripteur
    Le but est de s’assurer que toute modification du transcriteur (suppression / modification / ajout d’une question) est retrocompatible pour les anciens diagnostics

---
+++ mon-aide-cyber-api/test/adaptateurs/coherenceTranscripteur.spec.ts
@@ -0,0 +1,26 @@
+import { describe, expect } from 'vitest';
+import { adaptateurTranscripteur } from '../../src/infrastructure/adaptateurs/transcripteur/adaptateurTranscripteur';
+import fs from 'fs';
+
+describe('Cohérence du transcripteur', () => {
+  it('Assure la rétro compatibilité des identifiants des questions du transcripteur', () => {
+    const resultat = Object.entries(
+      adaptateurTranscripteur().transcripteur().thematiques
+    ).flatMap(([_, thematique]) => {
+      return thematique.groupes.flatMap((groupe) => {
+        return groupe.questions.flatMap((question) => {
+          return question.identifiant;
+        });
+      });
+    });
+
+    expect(resultat).toStrictEqual(
+      JSON.parse(
+        fs.readFileSync(
+          './test/adaptateurs/coherence-transcripteur.json',
+          'utf-8'
+        )
+      )
+    );
+  });
+});
