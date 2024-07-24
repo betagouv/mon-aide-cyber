fd2d5ce880a029a5da1b48cdb3e0f15cf7c5b54f
tomi <thomas.bennett@caminu.fr>
2024-07-24 19:06:58 +0200

    [DEVENIR AIDANT][RECHERCHE DES COT] Complète l'annuaire COT

--- mon-aide-cyber-api/test/gestion-demandes/ServiceRechercheCOT.spec.ts
+++ mon-aide-cyber-api/test/gestion-demandes/ServiceRechercheCOT.spec.ts
@@ -4,16 +4,100 @@ class ServiceRechercheCOT {
   chercheMailParDepartement(nomDepartement: string): string | undefined {
     const annuaireCOT = [
       {
+        codeRegion: '01',
+        mail: 'outre-mer@ssi.gouv.fr',
+      },
+      {
+        codeRegion: '02',
+        mail: 'outre-mer@ssi.gouv.fr',
+      },
+      {
+        codeRegion: '03',
+        mail: 'outre-mer@ssi.gouv.fr',
+      },
+      {
+        codeRegion: '04',
+        mail: 'outre-mer@ssi.gouv.fr',
+      },
+      {
+        codeRegion: '06',
+        mail: 'outre-mer@ssi.gouv.fr',
+      },
+      {
+        codeRegion: '978',
+        mail: 'outre-mer@ssi.gouv.fr',
+      },
+      {
+        codeRegion: '977',
+        mail: 'outre-mer@ssi.gouv.fr',
+      },
+      {
+        codeRegion: '975',
+        mail: 'outre-mer@ssi.gouv.fr',
+      },
+      {
+        codeRegion: '986',
+        mail: 'outre-mer@ssi.gouv.fr',
+      },
+      {
+        codeRegion: '987',
+        mail: 'outre-mer@ssi.gouv.fr',
+      },
+      {
+        codeRegion: '988',
+        mail: 'outre-mer@ssi.gouv.fr',
+      },
+      {
         codeRegion: '11',
         mail: 'ile-de-france@ssi.gouv.fr',
       },
       {
-        codeRegion: '94',
-        mail: 'corse@ssi.gouv.fr',
+        codeRegion: '24',
+        mail: 'centre-val-de-loire@ssi.gouv.fr',
+      },
+      {
+        codeRegion: '27',
+        mail: 'bourgogne-franche-comte@ssi.gouv.fr',
+      },
+      {
+        codeRegion: '28',
+        mail: 'normandie@ssi.gouv.fr',
+      },
+      {
+        codeRegion: '32',
+        mail: 'hauts-de-france@ssi.gouv.fr',
+      },
+      {
+        codeRegion: '44',
+        mail: 'grand-est@ssi.gouv.fr',
+      },
+      {
+        codeRegion: '52',
+        mail: 'pays-de-la-loire@ssi.gouv.fr',
       },
       {
         codeRegion: '53',
         mail: 'bretagne@ssi.gouv.fr',
+      },
+      {
+        codeRegion: '75',
+        mail: 'nouvelle-aquitaine@ssi.gouv.fr',
+      },
+      {
+        codeRegion: '76',
+        mail: 'occitanie@ssi.gouv.fr',
+      },
+      {
+        codeRegion: '84',
+        mail: 'auvergne-rhone-alpes@ssi.gouv.fr',
+      },
+      {
+        codeRegion: '93',
+        mail: 'paca@ssi.gouv.fr',
+      },
+      {
+        codeRegion: '94',
+        mail: 'corse@ssi.gouv.fr',
       },
     ];
 
