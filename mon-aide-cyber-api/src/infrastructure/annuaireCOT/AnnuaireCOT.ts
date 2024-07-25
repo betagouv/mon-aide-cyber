d405cd461dfff11451ebc02f3b684cbdfe46d94b
tomi <thomas.bennett@caminu.fr>
2024-07-25 12:01:21 +0200

    [REMANIEMENT][DEVENIR AIDANT][RECHERCHE COT] Déplace l'annuaire et le service de recherche dans les sources

---
+++ mon-aide-cyber-api/src/infrastructure/annuaireCOT/AnnuaireCOT.ts
@@ -0,0 +1,75 @@
+export type COT = {
+  codesRegions: string[];
+  mail: string;
+};
+
+export const annuaireCOT: COT[] = [
+  {
+    codesRegions: [
+      '01',
+      '02',
+      '03',
+      '04',
+      '06',
+      '978',
+      '977',
+      '975',
+      '986',
+      '987',
+      '988',
+    ],
+    mail: 'outre-mer@ssi.gouv.fr',
+  },
+  {
+    codesRegions: ['11'],
+    mail: 'ile-de-france@ssi.gouv.fr',
+  },
+  {
+    codesRegions: ['24'],
+    mail: 'centre-val-de-loire@ssi.gouv.fr',
+  },
+  {
+    codesRegions: ['27'],
+    mail: 'bourgogne-franche-comte@ssi.gouv.fr',
+  },
+  {
+    codesRegions: ['28'],
+    mail: 'normandie@ssi.gouv.fr',
+  },
+  {
+    codesRegions: ['32'],
+    mail: 'hauts-de-france@ssi.gouv.fr',
+  },
+  {
+    codesRegions: ['44'],
+    mail: 'grand-est@ssi.gouv.fr',
+  },
+  {
+    codesRegions: ['52'],
+    mail: 'pays-de-la-loire@ssi.gouv.fr',
+  },
+  {
+    codesRegions: ['53'],
+    mail: 'bretagne@ssi.gouv.fr',
+  },
+  {
+    codesRegions: ['75'],
+    mail: 'nouvelle-aquitaine@ssi.gouv.fr',
+  },
+  {
+    codesRegions: ['76'],
+    mail: 'occitanie@ssi.gouv.fr',
+  },
+  {
+    codesRegions: ['84'],
+    mail: 'auvergne-rhone-alpes@ssi.gouv.fr',
+  },
+  {
+    codesRegions: ['93'],
+    mail: 'paca@ssi.gouv.fr',
+  },
+  {
+    codesRegions: ['94'],
+    mail: 'corse@ssi.gouv.fr',
+  },
+];
