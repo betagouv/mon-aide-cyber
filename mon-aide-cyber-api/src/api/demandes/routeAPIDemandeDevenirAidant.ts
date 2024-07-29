7eac36e8bee288aaf8d0a84358dd24a452ff1295
tomi <thomas.bennett@caminu.fr>
2024-07-29 15:54:12 +0200

    [DEVENIR AIDANT] Valide la présence et le bon format du nom de l'aidant dans la requête

--- mon-aide-cyber-api/src/api/demandes/routeAPIDemandeDevenirAidant.ts
+++ mon-aide-cyber-api/src/api/demandes/routeAPIDemandeDevenirAidant.ts
@@ -1,10 +1,25 @@
 import express, { NextFunction, Request, Response, Router } from 'express';
+import { body, validationResult } from 'express-validator';
 
 export const routesAPIDemandesDevenirAidant = () => {
   const routes: Router = express.Router();
   routes.post(
     '/',
-    async (_requete: Request, reponse: Response, _suite: NextFunction) => {
+    express.json(),
+    body('nom').exists().notEmpty().trim(),
+    async (requete: Request, reponse: Response, _suite: NextFunction) => {
+      const resultatValidation = validationResult(requete);
+
+      if (!resultatValidation.isEmpty()) {
+        reponse.status(422);
+
+        reponse.json({
+          message: 'Veuillez renseigner votre nom',
+        });
+
+        return reponse.send();
+      }
+
       reponse.status(200);
 
       return reponse.send();
