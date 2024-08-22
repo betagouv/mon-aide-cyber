1b99a92c78d7002a5a20baf60dc1e5dbf9f50aab
marc.etourneau <marc.etourneau@beta.gouv.fr>
2024-08-22 14:35:37 +0200

    [COMPOSANTS REUTILISABLES] Créé le composant Button avec ses principaux variants
    [BOUTON] Créé une story storybook pour le composant
    
    [BOUTON] Ajoute deux stories supplémentaires
    
    [BOUTON] Créé le variant Lien
    
    [COMPOSANT BOUTON] Met à disposition sur le bouton une gestion de thème
    
    [COMPOSANT BOUTON] Ajoute le variant texte

---
+++ mon-aide-cyber-ui/src/composants/atomes/Button/Button.tsx
@@ -0,0 +1,47 @@
+import { PropsWithChildren } from 'react';
+
+export type ButtonVariants =
+  | 'default'
+  | 'primary'
+  | 'secondary'
+  | 'link'
+  | 'text';
+export type Theme = 'dark' | 'light';
+
+export type ButtonProps = PropsWithChildren<
+  React.ButtonHTMLAttributes<HTMLButtonElement>
+> & {
+  variant: ButtonVariants;
+  theme?: Theme;
+};
+
+function Button({
+  children,
+  variant,
+  theme = 'light',
+  ...restProps
+}: ButtonProps) {
+  const { className, title } = restProps;
+  const buttonVariants: Map<ButtonVariants, string> = new Map([
+    ['default', 'bouton-mac-primaire'],
+    ['primary', 'bouton-mac-primaire'],
+    ['secondary', 'bouton-mac-secondaire'],
+    ['link', 'bouton-mac-lien'],
+    ['text', 'bouton-mac-texte'],
+  ]);
+
+  const classNames = [
+    'bouton-mac',
+    buttonVariants.get(variant),
+    className,
+    theme,
+  ].join(' ');
+
+  return (
+    <button {...restProps} className={classNames}>
+      {children ? children : title}
+    </button>
+  );
+}
+
+export default Button;
