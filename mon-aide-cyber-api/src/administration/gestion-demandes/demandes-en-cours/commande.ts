6491d3ec4946fa2684a7feb68cbd88ffee5f834c
Bertrand Bougon <bertrand.bougon@beta.gouv.fr>
2024-09-09 11:04:11 +0200

    [DEVENIR AIDANT] Ajoute une commande d’administration pour générer un rapport sur les demandes faites sur une période donnée

---
+++ mon-aide-cyber-api/src/administration/gestion-demandes/demandes-en-cours/commande.ts
@@ -0,0 +1,55 @@
+import { program } from 'commander';
+import { EntrepotDemandeDevenirAidantPostgres } from '../../../infrastructure/entrepots/postgres/EntrepotDemandeDevenirAidantPostgres';
+import { compareAsc, endOfMonth, format } from 'date-fns';
+import { adaptateurServiceChiffrement } from '../../../infrastructure/adaptateurs/adaptateurServiceChiffrement';
+import fs from 'fs';
+
+const command = program
+  .description('Obtenir la liste des demandes pour un mois donné')
+  .argument(
+    '<dateDebutPeriode>',
+    'Une date au format YYYY-MM-DD pour obtenir les demandes pour un mois donné (i.e : 2024-09-01 pour les demandes du mois de Septembre 2024)'
+  );
+
+command.action(async (...args: any[]) => {
+  const dateDebutPeriode: Date = new Date(Date.parse(args[0].toLowerCase()));
+
+  const rapport: string[] = ['Date;Nom - Prénom;Département;\n'];
+
+  const entrepot = new EntrepotDemandeDevenirAidantPostgres(
+    adaptateurServiceChiffrement()
+  );
+
+  const demandes = await entrepot.tous().then((demandes) =>
+    demandes
+      .filter(
+        (d) =>
+          d.date >= dateDebutPeriode && d.date <= endOfMonth(dateDebutPeriode)
+      )
+      .sort((demandeA, demandeB) => compareAsc(demandeA.date, demandeB.date))
+      .map((demande) => ({
+        date: demande.date,
+        nom: demande.nom,
+        prenom: demande.prenom,
+        departement: demande.departement.nom,
+      }))
+  );
+
+  demandes.forEach((demande) => {
+    rapport.push(
+      `${format(demande.date, 'dd-MM-yyyy')};${demande.nom} ${demande.prenom};${demande.departement};\n`
+    );
+  });
+
+  fs.writeFileSync(
+    `/tmp/${format(dateDebutPeriode, 'yyyy_MM')}_demandes_aidant.csv`,
+    rapport.join(''),
+    {
+      encoding: 'utf-8',
+    }
+  );
+
+  process.exit(0);
+});
+
+program.parse();
