62ea3bca80c07991a0b6a13941d8239cebfb2f5b
Bertrand Bougon <bertrand.bougon@beta.gouv.fr>
2024-09-13 11:47:16 +0200

    [REMANIEMENT][NAVIGATION] Retire le composant BarreDeNavigation inutilisé

--- mon-aide-cyber-ui/src/composants/layout/LayoutPublic.tsx
+++ mon-aide-cyber-ui/src/composants/layout/LayoutPublic.tsx
@@ -1,4 +1,4 @@
-import { Link, matchPath, Outlet, useLocation } from 'react-router-dom';
+import { Outlet } from 'react-router-dom';
 import { Header } from './Header';
 import { LienMAC } from '../LienMAC';
 import { Footer } from './Footer';
@@ -26,38 +26,6 @@ export const liensNavigation: LienNavigation[] = [
     nom: 'Kit de communication',
   },
 ];
-
-export const BarreDeNavigation = () => {
-  const location = useLocation();
-
-  const estCheminCourant = (cheminATester: string) =>
-    !!matchPath(location.pathname, cheminATester);
-
-  return (
-    <nav
-      className="fr-nav"
-      id="navigation-494"
-      role="navigation"
-      aria-label="Menu principal"
-    >
-      <div className="fr-container barre-navigation">
-        <ul className="fr-nav__list">
-          {liensNavigation.map((lien) => (
-            <li
-              className={`fr-nav__item ${estCheminCourant(lien.route) ? 'lien actif' : 'lien'}`}
-              key={lien.nom}
-            >
-              <Link className="fr-nav__link" to={lien.route}>
-                {lien.nom}
-              </Link>
-            </li>
-          ))}
-        </ul>
-      </div>
-    </nav>
-  );
-};
-
 export const LayoutPublic = () => {
   return (
     <>
