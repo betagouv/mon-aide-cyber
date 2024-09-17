f7c7c943ecbc3784e3a43a5913e441a8069f279a
marc.etourneau <marc.etourneau@beta.gouv.fr>
2024-09-17 10:02:35 +0200

    [CORRIGE] Réinitialise la position du scroll lors d'une navigation avec react-router

--- mon-aide-cyber-ui/src/composants/layout/LayoutPublic.tsx
+++ mon-aide-cyber-ui/src/composants/layout/LayoutPublic.tsx
@@ -1,7 +1,8 @@
-import { Outlet } from 'react-router-dom';
+import { Outlet, useLocation } from 'react-router-dom';
 import { Header } from './Header';
 import { LienMAC } from '../LienMAC';
 import { Footer } from './Footer';
+import { useLayoutEffect } from 'react';
 
 export type LienNavigation = {
   route: string;
@@ -27,6 +28,13 @@ export const liensNavigation: LienNavigation[] = [
   },
 ];
 export const LayoutPublic = () => {
+  const location = useLocation();
+
+  // scroll to top of page after a page transition.
+  useLayoutEffect(() => {
+    document.documentElement.scrollTo({ top: 0, left: 0, behavior: 'instant' });
+  }, [location.pathname]);
+
   return (
     <>
       <Header lienMAC={<LienMAC titre="Accueil - MonAideCyber" route="/" />} />
