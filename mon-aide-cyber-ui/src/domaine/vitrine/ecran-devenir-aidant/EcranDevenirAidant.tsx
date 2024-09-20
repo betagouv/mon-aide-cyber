092a93650935b8662a5a9462954a13e15f3911a9
marc.etourneau <marc.etourneau@beta.gouv.fr>
2024-09-20 14:49:58 +0200

    [VERBATIMS] Rajoute les témoignages manquants et déplace la liste dans le dossier infrastructure

--- mon-aide-cyber-ui/src/domaine/vitrine/ecran-devenir-aidant/EcranDevenirAidant.tsx
+++ mon-aide-cyber-ui/src/domaine/vitrine/ecran-devenir-aidant/EcranDevenirAidant.tsx
@@ -1,28 +1,14 @@
 import { ActionsPiedDePage } from '../../../composants/communs/ActionsPiedDePage';
 import { FormulaireDeContact } from '../../../composants/communs/FormulaireDeContact/FormulaireDeContact';
 import useDefilementFluide from '../../../hooks/useDefilementFluide';
+import { verbatims } from '../../../infrastructure/donnees/Verbatims';
 import { FormulaireDevenirAidant } from '../../gestion-demandes/devenir-aidant/formulaire-devenir-aidant/FormulaireDevenirAidant';
 import { CommentDevenirAidant } from './composants/CommentDevenirAidant';
 import { HeroDevenirAidant } from './composants/HeroDevenirAidant';
 import { LesEngagementsDeLAidant } from './composants/LesEngagementsDeLAidant';
 import { QuiEstConcerne } from './composants/QuiEstConcerne';
-import { Temoignages, Verbatim } from './composants/Temoignages';
+import { Temoignages } from './composants/Temoignages';
 import './ecran-devenir-aidant.scss';
-
-const verbatims: Verbatim[] = [
-  {
-    id: 1,
-    auteur: 'Un Aidant de la Réunion (974)',
-    commentaire:
-      "Encore merci pour cet outil qui, une fois la communauté d'aidants réunionnaise structurée et formée, nous aidera grandement dans le passage à l'échelle.",
-  },
-  {
-    id: 2,
-    auteur: 'Un Aidant, réserviste de la Police, du Rhône (69)',
-    commentaire:
-      'MonAideCyber remplit très bien sa mission, et le fait de pouvoir tout de suite donner un rapport aux interlocteurs est un réel atout.',
-  },
-];
 
 export const EcranDevenirAidant = () => {
   useDefilementFluide();
