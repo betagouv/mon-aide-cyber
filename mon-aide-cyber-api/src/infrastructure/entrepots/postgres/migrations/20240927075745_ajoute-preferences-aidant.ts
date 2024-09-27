0be93d3246abe81a0092155eb2cb616473e83d5c
Bertrand Bougon <bertrand.bougon@beta.gouv.fr>
2024-09-27 12:15:19 +0200

    [ESPACE AIDANT][PREFERENCES] Ajoute le script de migration pour ajouter les préférences aux Aidants

---
+++ mon-aide-cyber-api/src/infrastructure/entrepots/postgres/migrations/20240927075745_ajoute-preferences-aidant.ts
@@ -0,0 +1,56 @@
+import { Knex } from 'knex';
+import crypto from 'crypto';
+import { fabriqueEntrepots } from '../../../../adaptateurs/fabriqueEntrepots';
+
+type AidantDTO = {
+  type: 'AIDANT';
+  dateSignatureCharte?: string;
+  dateSignatureCGU?: string;
+  identifiantConnexion: string;
+  nomPrenom: string;
+  motDePasse: string;
+};
+type RepresentationAidant = {
+  id: crypto.UUID;
+  donnees: AidantDTO;
+};
+
+type Preferences = {
+  secteursActivite: string[];
+  departements: string[];
+  typesEntites: string[];
+};
+
+type NouvelAidant = AidantDTO & {
+  preferences: Preferences;
+};
+
+export async function up(knex: Knex): Promise<void | number[]> {
+  return knex('utilisateurs').then(async (lignes: RepresentationAidant[]) => {
+    const misesAJour = lignes.map(async (ligne) => {
+      const nouvellesDonneesAidant: NouvelAidant = {
+        ...ligne.donnees,
+        preferences: {
+          secteursActivite: [],
+          departements: [],
+          typesEntites: [],
+        },
+      };
+      const demandes = await fabriqueEntrepots().demandesDevenirAidant().tous();
+      demandes
+        .filter((d) => nouvellesDonneesAidant.identifiantConnexion === d.mail)
+        .forEach((d) =>
+          nouvellesDonneesAidant.preferences.departements.push(
+            d.departement.nom
+          )
+        );
+      return knex('utilisateurs').where('id', ligne.id).update({
+        donnees: nouvellesDonneesAidant,
+      });
+    });
+    return Promise.all(misesAJour);
+  });
+}
+
+// eslint-disable-next-line @typescript-eslint/no-empty-function
+export async function down(__knex: Knex): Promise<void> {}
