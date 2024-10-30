9a91ecda3967ce6963d967e2f45132518b7549ef
Marc ETOURNEAU <marc.etourneau@beta.gouv.fr>
2024-10-30 09:58:30 +0100

    [RECUPERATION CONTEXTE VITIRINE HATEOAS][REMANIEMENT] Remplace le useEffect par un useQuery pour exécuter l'appel

--- mon-aide-cyber-ui/src/hooks/useRecupereContexteNavigation.ts
+++ mon-aide-cyber-ui/src/hooks/useRecupereContexteNavigation.ts
@@ -1,29 +1,28 @@
-import { useEffect, useState } from 'react';
+import { useEffect } from 'react';
 import { ReponseHATEOAS } from '../domaine/Lien';
 import { useMACAPI } from '../fournisseurs/api/useMACAPI';
 import { useContexteNavigation } from './useContexteNavigation';
 import { useNavigationMAC } from '../fournisseurs/hooks';
+import { useQuery } from '@tanstack/react-query';
 
 export const useRecupereContexteNavigation = (contexte: string) => {
   const macAPI = useMACAPI();
   const navigationMAC = useNavigationMAC();
   const navigationUtilisateur = useContexteNavigation(macAPI);
-  const [contexteRecupere, setContexteRecupere] = useState(false);
+
+  const { data: contexteRecuperee, isLoading: estEnCoursDeChargement } =
+    useQuery({
+      queryKey: ['recupere-contexte', contexte],
+      queryFn: () =>
+        navigationUtilisateur.recupereContexteNavigation({
+          contexte: contexte,
+        }),
+    });
 
   useEffect(() => {
-    if (contexteRecupere) return;
-    navigationUtilisateur
-      .recupereContexteNavigation({
-        contexte: contexte,
-      })
-      .then((reponse) =>
-        navigationMAC.ajouteEtat((reponse as ReponseHATEOAS).liens)
-      )
-      .catch()
-      .finally(() => {
-        setContexteRecupere(true);
-      });
-  }, [...contexte]);
+    if (contexteRecuperee)
+      navigationMAC.ajouteEtat((contexteRecuperee as ReponseHATEOAS).liens);
+  }, [contexteRecuperee]);
 
-  return { contexteRecupere };
+  return { contexteRecuperee, estEnCoursDeChargement };
 };
