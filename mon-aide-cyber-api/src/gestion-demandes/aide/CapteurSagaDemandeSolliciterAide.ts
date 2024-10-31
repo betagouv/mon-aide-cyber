bd8a2effeff35bd3986488b899f0655e7a54afa2
Marc ETOURNEAU <marc.etourneau@beta.gouv.fr>
2024-10-31 10:47:54 +0100

    [SOLLICITER AIDANT] Publie un événement dès que la demande est créé et que les emails sont envoyés

--- mon-aide-cyber-api/src/gestion-demandes/aide/CapteurSagaDemandeSolliciterAide.ts
+++ mon-aide-cyber-api/src/gestion-demandes/aide/CapteurSagaDemandeSolliciterAide.ts
@@ -4,7 +4,11 @@ import crypto from 'crypto';
 import { AdaptateurEnvoiMail } from '../../adaptateurs/AdaptateurEnvoiMail';
 import { adaptateurCorpsMessage } from './adaptateurCorpsMessage';
 import { adaptateurEnvironnement } from '../../adaptateurs/adaptateurEnvironnement';
+import { BusEvenement, Evenement } from '../../domaine/BusEvenement';
 import { ServiceAidant } from '../../authentification/ServiceAidant';
+import { FournisseurHorloge } from '../../infrastructure/horloge/FournisseurHorloge';
+import { rechercheParNomDepartement } from '../departements';
+import { Aide } from '../../aide/Aide';
 
 export type SagaDemandeSolliciterAide = Omit<Saga, 'type'> & {
   email: string;
@@ -14,11 +18,18 @@ export type SagaDemandeSolliciterAide = Omit<Saga, 'type'> & {
   type: 'SagaDemandeSolliciterAide';
 };
 
+export type DemandeSolliciterAideCree = Evenement<{
+  identifiantAide: crypto.UUID;
+  identifiantAidant: crypto.UUID;
+  departement: string;
+}>;
+
 export class CapteurSagaDemandeSolliciterAide
   implements CapteurSaga<SagaDemandeSolliciterAide, void>
 {
   constructor(
     private readonly busCommande: BusCommande,
+    private readonly busEvenement: BusEvenement,
     private readonly adaptateurEnvoiMail: AdaptateurEnvoiMail,
     private readonly serviceAidant: ServiceAidant
   ) {}
@@ -30,63 +41,79 @@ export class CapteurSagaDemandeSolliciterAide
       email: saga.email,
       ...(saga.raisonSociale && { raisonSociale: saga.raisonSociale }),
     };
-    return this.busCommande.publie<CommandeCreerAide, void>(commande).then(() =>
-      this.serviceAidant
-        .parIdentifiant(saga.identifiantAidant)
-        .then((aidant) =>
-          Promise.all([
-            this.adaptateurEnvoiMail.envoie(
-              {
+    return this.busCommande
+      .publie<CommandeCreerAide, Aide>(commande)
+      .then((aide: Aide) =>
+        this.serviceAidant
+          .parIdentifiant(saga.identifiantAidant)
+          .then((aidant) =>
+            Promise.all([
+              this.adaptateurEnvoiMail.envoie(
+                {
+                  corps: adaptateurCorpsMessage
+                    .notificationAidantSollicitation()
+                    .genereCorpsMessage({
+                      departement: saga.departement,
+                      mailEntite: saga.email,
+                      nomPrenom: aidant!.nomUsage,
+                      ...(saga.raisonSociale && {
+                        raisonSocialeEntite: saga.raisonSociale,
+                      }),
+                    }),
+                  destinataire: { email: aidant!.email },
+                  objet:
+                    'MonAideCyber - Une entité vous sollicite depuis l’annuaire des Aidants cyber.',
+                },
+                'INFO'
+              ),
+              this.adaptateurEnvoiMail.envoie({
                 corps: adaptateurCorpsMessage
-                  .notificationAidantSollicitation()
+                  .recapitulatifMAC()
                   .genereCorpsMessage({
+                    aidant: aidant!.nomUsage,
                     departement: saga.departement,
                     mailEntite: saga.email,
-                    nomPrenom: aidant!.nomUsage,
                     ...(saga.raisonSociale && {
-                      raisonSocialeEntite: saga.raisonSociale,
+                      raisonSociale: saga.raisonSociale,
                     }),
                   }),
-                destinataire: { email: aidant!.email },
+                destinataire: {
+                  email: adaptateurEnvironnement.messagerie().emailMAC(),
+                },
                 objet:
-                  'MonAideCyber - Une entité vous sollicite depuis l’annuaire des Aidants cyber.',
-              },
-              'INFO'
-            ),
-            this.adaptateurEnvoiMail.envoie({
-              corps: adaptateurCorpsMessage
-                .recapitulatifMAC()
-                .genereCorpsMessage({
-                  aidant: aidant!.nomUsage,
-                  departement: saga.departement,
-                  mailEntite: saga.email,
-                  ...(saga.raisonSociale && {
-                    raisonSociale: saga.raisonSociale,
-                  }),
-                }),
-              destinataire: {
-                email: adaptateurEnvironnement.messagerie().emailMAC(),
-              },
-              objet:
-                'MonAideCyber - Une entité a sollicité un Aidant depuis l’annuaire des Aidants cyber',
-            }),
-            this.adaptateurEnvoiMail.envoie(
-              {
-                corps: adaptateurCorpsMessage
-                  .recapitulatifSollicitationAide({
-                    departement: saga.departement,
-                    nomPrenom: aidant!.nomUsage,
-                  })
-                  .genereCorpsMessage(),
-                destinataire: { email: saga.email },
-                objet:
-                  'MonAideCyber - Mise en relation avec l’Aidant cyber de votre choix',
-              },
-              'INFO'
-            ),
-          ])
-        )
-        .then(() => Promise.resolve())
-    );
+                  'MonAideCyber - Une entité a sollicité un Aidant depuis l’annuaire des Aidants cyber',
+              }),
+              this.adaptateurEnvoiMail.envoie(
+                {
+                  corps: adaptateurCorpsMessage
+                    .recapitulatifSollicitationAide({
+                      departement: saga.departement,
+                      nomPrenom: aidant!.nomUsage,
+                    })
+                    .genereCorpsMessage(),
+                  destinataire: { email: saga.email },
+                  objet:
+                    'MonAideCyber - Mise en relation avec l’Aidant cyber de votre choix',
+                },
+                'INFO'
+              ),
+            ])
+          )
+          .then(() => {
+            return this.busEvenement
+              .publie<DemandeSolliciterAideCree>({
+                identifiant: aide.identifiant,
+                type: 'AIDE_VIA_SOLLICITATION_AIDANT_CREE',
+                date: FournisseurHorloge.maintenant(),
+                corps: {
+                  identifiantAide: aide.identifiant,
+                  identifiantAidant: saga.identifiantAidant,
+                  departement: rechercheParNomDepartement(saga.departement)
+                    .code,
+                },
+              })
+              .then(() => Promise.resolve());
+          })
+      );
   }
 }
