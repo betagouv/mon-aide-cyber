4f518cb8bff14df39040e92c4984db039540a94c
Bertrand Bougon <bertrand.bougon@gmail.com>
2024-11-01 09:44:25 +0100

    [ANNUAIRE AIDANTS] Retourne la liste des Aidants de manière aléatoire

--- mon-aide-cyber-api/src/annuaire-aidants/ServiceAnnuaireAidants.ts
+++ mon-aide-cyber-api/src/annuaire-aidants/ServiceAnnuaireAidants.ts
@@ -4,6 +4,17 @@ export type CriteresDeRecherche = {
   departement: string;
 };
 
+const tableauAleatoire = (aidants: Aidant[]) => {
+  const tableauAMelanger = [...aidants];
+  for (let i = tableauAMelanger.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    const aidantCourant = tableauAMelanger[i];
+    tableauAMelanger[i] = tableauAMelanger[j];
+    tableauAMelanger[j] = aidantCourant;
+  }
+  return tableauAMelanger;
+};
+
 export class ServiceAnnuaireAidants {
   constructor(private readonly entrepotAidant: EntrepotAnnuaireAidants) {}
 
@@ -13,18 +24,22 @@ export class ServiceAnnuaireAidants {
   }
 
   recherche(
-    criteresDeRecherche: CriteresDeRecherche | undefined
+    criteresDeRecherche: CriteresDeRecherche | undefined,
+    melange: (aidants: Aidant[]) => Aidant[] = tableauAleatoire
   ): Promise<Aidant[] | undefined> {
     if (!criteresDeRecherche?.departement) {
       return Promise.resolve(undefined);
     }
+
     return this.entrepotAidant
       .rechercheParCriteres(criteresDeRecherche)
       .then((aidants) => {
-        return aidants.map((aidant) => ({
-          ...aidant,
-          nomPrenom: this.formateLeNom(aidant.nomPrenom),
-        }));
+        return melange(
+          aidants.map((aidant) => ({
+            ...aidant,
+            nomPrenom: this.formateLeNom(aidant.nomPrenom),
+          }))
+        );
       });
   }
 }
