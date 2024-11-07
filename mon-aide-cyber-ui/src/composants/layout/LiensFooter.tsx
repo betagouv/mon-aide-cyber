57f59dae160cd3820401cb60a0469e2904de6457
Bertrand Bougon <bertrand.bougon@gmail.com>
2024-11-07 14:32:00 +0100

    [FAQ] Ajoute le lien vers la FAQ CRISP

---
+++ mon-aide-cyber-ui/src/composants/layout/LiensFooter.tsx
@@ -0,0 +1,34 @@
+export const LiensFooter = () => (
+  <ul className="fr-footer__bottom-list">
+    <li className="fr-footer__bottom-item">
+      <a className="fr-footer__bottom-link" href="/accessibilite">
+        Accessibilité : non conforme
+      </a>
+    </li>
+    <li className="fr-footer__bottom-item">
+      <a className="fr-footer__bottom-link" href="/mentions-legales">
+        Mentions légales
+      </a>
+    </li>
+    <li className="fr-footer__bottom-item">
+      <a className="fr-footer__bottom-link" href="/charte-aidant">
+        La charte de l&apos;aidant
+      </a>
+    </li>
+    <li className="fr-footer__bottom-item">
+      <a className="fr-footer__bottom-link" href="/cgu">
+        Les CGU
+      </a>
+    </li>
+    <li className="fr-footer__bottom-item">
+      <a
+        className="fr-footer__bottom-link"
+        href="https://monaidecyber.crisp.help/fr/"
+        target="_blank"
+        rel="noreferrer"
+      >
+        FAQ
+      </a>
+    </li>
+  </ul>
+);
