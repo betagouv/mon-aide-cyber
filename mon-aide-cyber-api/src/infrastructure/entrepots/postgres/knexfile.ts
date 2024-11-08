a52b51206a1fa6bab73f5e1226a73f59cbc1ee92
Bertrand Bougon <bertrand.bougon@gmail.com>
2024-11-08 17:49:21 +0100

    [TECHNIQUE] Crée des variables d’environnement pour le pool de la base de données
    Afin d’augmenter le pool maximum dans les tests

--- mon-aide-cyber-api/src/infrastructure/entrepots/postgres/knexfile.ts
+++ mon-aide-cyber-api/src/infrastructure/entrepots/postgres/knexfile.ts
@@ -1,6 +1,13 @@
 export default {
   client: 'pg',
   connection: process.env.URL_SERVEUR_BASE_DONNEES!,
-  pool: { min: 2, max: 10 },
+  pool: {
+    min: process.env.BASE_DE_DONNEES_MIN_POOL
+      ? parseInt(process.env.BASE_DE_DONNEES_MIN_POOL)
+      : 2,
+    max: process.env.BASE_DE_DONNEES_MAX_POOL
+      ? parseInt(process.env.BASE_DE_DONNEES_MAX_POOL)
+      : 10,
+  },
   migrations: { tableName: 'knex_migrations' },
 };
