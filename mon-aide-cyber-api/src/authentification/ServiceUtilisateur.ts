ba35a329fbbbf2323376759b6ba2339828731c69
Bertrand Bougon <bertrand.bougon@gmail.com>
2024-11-13 09:14:37 +0100

    [AIDANT][REINITIALISATION MOT DE PASSE] Publie les événements REINITIALISATION_MOT_DE_PASSE_FAITE et  REINITIALISATION_MOT_DE_PASSE_ERRONEE

--- mon-aide-cyber-api/src/authentification/ServiceUtilisateur.ts
+++ mon-aide-cyber-api/src/authentification/ServiceUtilisateur.ts
@@ -3,6 +3,9 @@ import { FournisseurHorloge } from '../infrastructure/horloge/FournisseurHorloge
 import { differenceInMinutes } from 'date-fns';
 import { ErreurMAC } from '../domaine/erreurMAC';
 import { sommeDeControle } from './sommeDeControle';
+import { BusEvenement, Evenement } from '../domaine/BusEvenement';
+import crypto, { UUID } from 'crypto';
+import { adaptateurUUID } from '../infrastructure/adaptateurs/adaptateurUUID';
 
 type Token = {
   identifiant: string;
@@ -21,7 +24,10 @@ export class ErreurReinitialisationMotDePasse extends Error {}
 const VINGT_MINUTES = 20;
 
 export class ServiceUtilisateur {
-  constructor(private readonly entrepotUtilisateur: EntrepotUtilisateur) {}
+  constructor(
+    private readonly entrepotUtilisateur: EntrepotUtilisateur,
+    private readonly busEvenement: BusEvenement
+  ) {}
 
   modifieMotDePasse(
     modificationMotDePasse: ModificationMotDePasse
@@ -31,7 +37,10 @@ export class ServiceUtilisateur {
       modificationMotDePasse.token.date
     );
     if (isNaN(lapsDeTemps) || lapsDeTemps > VINGT_MINUTES) {
-      return this.rejette();
+      return this.rejette(
+        modificationMotDePasse.token.identifiant,
+        'DATE_ECHUE'
+      );
     }
     return this.entrepotUtilisateur
       .lis(modificationMotDePasse.token.identifiant)
@@ -40,21 +49,58 @@ export class ServiceUtilisateur {
           sommeDeControle(utilisateur.motDePasse) !==
           modificationMotDePasse.token.sommeDeControle
         ) {
-          return this.rejette();
+          return this.rejette(
+            modificationMotDePasse.token.identifiant,
+            'REINITIALISATION_DEJA_FAITE'
+          );
         }
         utilisateur.motDePasse = modificationMotDePasse.motDePasse;
         return this.entrepotUtilisateur.persiste(utilisateur);
-      });
+      })
+      .then(() =>
+        this.busEvenement.publie<ReinitialisationMotDePasseFaite>({
+          identifiant: adaptateurUUID.genereUUID(),
+          type: 'REINITIALISATION_MOT_DE_PASSE_FAITE',
+          date: FournisseurHorloge.maintenant(),
+          corps: {
+            identifiant: modificationMotDePasse.token.identifiant as UUID,
+          },
+        })
+      );
   }
 
-  private rejette() {
-    return Promise.reject(
-      ErreurMAC.cree(
-        'Réinitialisation mot de passe',
-        new ErreurReinitialisationMotDePasse(
-          'Le lien de réinitialisation du mot de passe n’est plus valide.'
+  private rejette(
+    identifiant: string,
+    raison: 'DATE_ECHUE' | 'REINITIALISATION_DEJA_FAITE'
+  ) {
+    return this.busEvenement
+      .publie<ReinitialisationMotDePasseErronee>({
+        identifiant: adaptateurUUID.genereUUID(),
+        type: 'REINITIALISATION_MOT_DE_PASSE_ERRONEE',
+        date: FournisseurHorloge.maintenant(),
+        corps: {
+          identifiant: identifiant as UUID,
+          raison,
+        },
+      })
+      .then(() =>
+        Promise.reject(
+          ErreurMAC.cree(
+            'Réinitialisation mot de passe',
+            new ErreurReinitialisationMotDePasse(
+              'Le lien de réinitialisation du mot de passe n’est plus valide.'
+            )
+          )
         )
-      )
-    );
+      );
   }
 }
+
+export type ReinitialisationMotDePasseFaite = Evenement<{
+  identifiant: crypto.UUID;
+}>;
+
+export type ReinitialisationMotDePasseErronee = Evenement<{
+  identifiant: crypto.UUID;
+  raison: 'DATE_ECHUE' | 'REINITIALISATION_DEJA_FAITE';
+}>;
