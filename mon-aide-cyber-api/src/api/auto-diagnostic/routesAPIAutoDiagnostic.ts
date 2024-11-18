2c99d538be1646f396f757cfe750800700cd5057
Bertrand Bougon <bertrand.bougon@gmail.com>
2024-11-18 15:07:26 +0100

    [AUTO DIAGNOSTIC] Expose la route GET /api/auto-diagnostic/:id pour retourner un auto diagnostic

--- mon-aide-cyber-api/src/api/auto-diagnostic/routesAPIAutoDiagnostic.ts
+++ mon-aide-cyber-api/src/api/auto-diagnostic/routesAPIAutoDiagnostic.ts
@@ -1,10 +1,15 @@
-import express, { Response } from 'express';
+import express, { Request, Response } from 'express';
 import { ConfigurationServeur } from '../../serveur';
-import { RequeteUtilisateur } from '../routesAPI';
 import { NextFunction } from 'express-serve-static-core';
 import crypto from 'crypto';
 import { SagaLanceAutoDiagnostic } from '../../auto-diagnostic/CapteurSagaLanceAutoDiagnostic';
 import { FournisseurHorloge } from '../../infrastructure/horloge/FournisseurHorloge';
+import { ServiceDiagnostic } from '../../diagnostic/ServiceDiagnostic';
+import { representeLeDiagnosticPourLeClient } from '../representateurs/representateurDiagnostic';
+import { ReponseHATEOAS } from '../hateoas/hateoas';
+import { RepresentationDiagnostic } from '../representateurs/types';
+
+type CorpsReponseAutoDiagnostic = ReponseHATEOAS & RepresentationDiagnostic;
 
 export const routesAPIAutoDiagnostic = (
   configuration: ConfigurationServeur
@@ -15,7 +20,7 @@ export const routesAPIAutoDiagnostic = (
 
   routes.post(
     '/',
-    (requete: RequeteUtilisateur, reponse: Response, _suite: NextFunction) => {
+    (requete: Request, reponse: Response, _suite: NextFunction) => {
       return busCommande
         .publie<
           SagaLanceAutoDiagnostic,
@@ -27,6 +32,34 @@ export const routesAPIAutoDiagnostic = (
             .appendHeader('Link', `${requete.originalUrl}/${idDiagnostic}`)
             .send()
         );
+    }
+  );
+
+  routes.get(
+    '/:id',
+    (
+      requete: Request,
+      reponse: Response<CorpsReponseAutoDiagnostic>,
+      suite: NextFunction
+    ) => {
+      const { id } = requete.params;
+      new ServiceDiagnostic(configuration.entrepots)
+        .diagnostic(id as crypto.UUID)
+        .then((diagnostic) =>
+          reponse.json({
+            ...representeLeDiagnosticPourLeClient(
+              diagnostic,
+              configuration.adaptateurTranscripteurDonnees.transcripteur()
+            ),
+            liens: {
+              'repondre-diagnostic': {
+                url: `/api/auto-diagnostic/${diagnostic.identifiant}`,
+                methode: 'PATCH',
+              },
+            },
+          })
+        )
+        .catch((erreur) => suite(erreur));
     }
   );
 
