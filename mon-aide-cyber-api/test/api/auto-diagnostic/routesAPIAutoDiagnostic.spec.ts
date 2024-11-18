75b274683c2620df8009d5759741ae76621e1d02
Bertrand Bougon <bertrand.bougon@gmail.com>
2024-11-18 15:07:26 +0100

    [AUTO DIAGNOSTIC] Expose la route GET /api/auto-diagnostic/:id pour retourner un auto diagnostic

--- mon-aide-cyber-api/test/api/auto-diagnostic/routesAPIAutoDiagnostic.spec.ts
+++ mon-aide-cyber-api/test/api/auto-diagnostic/routesAPIAutoDiagnostic.spec.ts
@@ -1,10 +1,18 @@
-import { afterEach, beforeEach, describe, expect } from 'vitest';
+import { afterEach, beforeEach, describe, expect, it } from 'vitest';
 import testeurIntegration from '../testeurIntegration';
 import { Express } from 'express';
-import { unReferentiel } from '../../constructeurs/constructeurReferentiel';
+import {
+  uneQuestion,
+  unReferentiel,
+} from '../../constructeurs/constructeurReferentiel';
 import { executeRequete } from '../executeurRequete';
 import { adaptateurUUID } from '../../../src/infrastructure/adaptateurs/adaptateurUUID';
 import crypto from 'crypto';
+import { unDiagnostic } from '../../constructeurs/constructeurDiagnostic';
+import { RepresentationDiagnostic } from '../../../src/api/representateurs/types';
+import { ReponseDiagnostic } from '../../../src/api/routesAPIDiagnostic';
+import { Diagnostic } from '../../../src/diagnostic/Diagnostic';
+import { LiensHATEOAS } from '../../../src/api/hateoas/hateoas';
 
 describe('Le serveur MAC sur les routes /api/auto-diagnostic', () => {
   const testeurMAC = testeurIntegration();
@@ -58,4 +66,94 @@ describe('Le serveur MAC sur les routes /api/auto-diagnostic', () => {
       );
     });
   });
+
+  describe('Quand une requête GET est reçue sur /', () => {
+    it('Retourne le diagnostic', async () => {
+      const diagnostic = unDiagnostic()
+        .avecUnReferentiel(
+          unReferentiel()
+            .ajouteUneQuestionAuContexte(uneQuestion().construis())
+            .construis()
+        )
+        .construis();
+      await testeurMAC.entrepots.diagnostic().persiste(diagnostic);
+
+      const reponse = await executeRequete(
+        donneesServeur.app,
+        'GET',
+        `/api/auto-diagnostic/${diagnostic.identifiant}`,
+        donneesServeur.portEcoute
+      );
+
+      expect(reponse.statusCode).toBe(200);
+      const diagnosticRecu: RepresentationDiagnostic = await reponse.json();
+      expect(diagnosticRecu).toStrictEqual<ReponseDiagnostic>(
+        forgeReponseDiagnostic(diagnostic, {
+          'repondre-diagnostic': {
+            url: `/api/auto-diagnostic/${diagnostic.identifiant}`,
+            methode: 'PATCH',
+          },
+        })
+      );
+    });
+
+    it("Renvoie une erreur HTTP 404 diagnostic non trouvé si le diagnostic n'existe pas", async () => {
+      const reponse = await executeRequete(
+        donneesServeur.app,
+        'GET',
+        `/api/auto-diagnostic/id-inexistant`,
+        donneesServeur.portEcoute
+      );
+
+      expect(reponse.statusCode).toBe(404);
+      const newVar = await reponse.json();
+      expect(newVar).toStrictEqual({
+        message: "Le diagnostic demandé n'existe pas.",
+      });
+    });
+  });
 });
+
+const forgeReponseDiagnostic = (
+  diagnostic: Diagnostic,
+  liens: LiensHATEOAS,
+  reponseDonnee?: string
+): ReponseDiagnostic => {
+  return {
+    identifiant: diagnostic.identifiant,
+    referentiel: {
+      contexte: {
+        description: 'Description du contexte',
+        libelle: 'Contexte',
+        styles: {
+          navigation: 'navigation-contexte',
+        },
+        localisationIllustration: '/chemin/illustration/contexte',
+        groupes: [
+          {
+            numero: 1,
+            questions: diagnostic.referentiel.contexte.questions.map((q) => {
+              return {
+                identifiant: q.identifiant,
+                libelle: q.libelle,
+                reponseDonnee: {
+                  valeur: reponseDonnee ? reponseDonnee : null,
+                  reponses: [],
+                },
+                reponsesPossibles: q.reponsesPossibles.map((r) => {
+                  return {
+                    identifiant: r.identifiant,
+                    libelle: r.libelle,
+                    ordre: r.ordre,
+                  };
+                }),
+                type: 'choixUnique',
+              };
+            }),
+          },
+        ],
+      },
+    },
+    liens,
+  };
+};
