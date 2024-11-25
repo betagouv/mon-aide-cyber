d522c5119ba79f479636f5aa6ada98ec3ac03b6a
Marc ETOURNEAU <marc.etourneau@beta.gouv.fr>
2024-11-25 19:11:32 +0100

    [DIAGNOSTIC LIBRE ACCÈS] Retire l'email du formulaire FRONT

--- mon-aide-cyber-ui/src/domaine/auto-diagnostic/EcranDemandeAutodiagnostic.tsx
+++ mon-aide-cyber-ui/src/domaine/auto-diagnostic/EcranDemandeAutodiagnostic.tsx
@@ -1,51 +1,37 @@
-import HeroBloc from '../../composants/communs/HeroBloc.tsx';
-import { TypographieH5 } from '../../composants/communs/typographie/TypographieH5/TypographieH5.tsx';
-import { TypographieH2 } from '../../composants/communs/typographie/TypographieH2/TypographieH2.tsx';
 import { CapteurFormulaireDemandeAutodiagnostic } from './formulaire-demande-auto-diagnostic/CapteurFormulaireDemandeAutodiagnostic.tsx';
+import './ecran-demande-autodiagnostic.scss';
+import illustrationAutodiag from '../../../public/images/illustration-autodiag.svg';
+import { TypographieH3 } from '../../composants/communs/typographie/TypographieH3/TypographieH3.tsx';
+import { Link } from 'react-router-dom';
 
 export const EcranDemandeAutodiagnostic = () => {
   return (
-    <main role="main" className="demande-aide">
-      <HeroBloc>
-        <div className="contenu">
-          <TypographieH2 className="fr-mb-2w">
-            Utiliser l&apos;outil de diagnostic
-          </TypographieH2>
-          <p>Bénéficier du service sans faire appel à un Aidant cyber</p>
+    <main role="main" className="ecran-demande-autodiagnostic">
+      <div className="formulaire-colonne-gauche">
+        <TypographieH3 className="text-center violet-fonce">
+          Utiliser l&apos;Outil de diagnostic de MonAideCyber
+        </TypographieH3>
+        <p>
+          Pour utiliser l&apos;outil de diagnostic MonAideCyber, veuillez
+          accepter les conditions générales d&apos;utilisation
+        </p>
+        <CapteurFormulaireDemandeAutodiagnostic />
+        <div className="text-center">
+          <p>
+            Vous préférez faire un diagnostic accompagné ?
+            <br />
+            <Link to="/beneficier-du-dispositif/etre-aide#formulaire-demande-aide">
+              Faire une demande en ligne
+            </Link>
+          </p>
         </div>
-      </HeroBloc>
-      <section className="fond-clair-mac">
-        <div className="fr-container">
-          <div className="fr-grid-row fr-grid-row--center">
-            <div className="fr-col-md-8 fr-col-sm-12 section">
-              <div className="fr-mt-2w">
-                <div>
-                  <TypographieH5>
-                    Vous souhaitez accéder à l'outil de diagnostic de
-                    MonAideCyber
-                  </TypographieH5>
-                  <p>
-                    Veuillez compléter les informations ci-dessous pour
-                    formaliser votre demande.
-                  </p>
-                  <p style={{ fontSize: '12px' }}>
-                    Les informations que vous fournissez via ce formulaire sont
-                    strictement confidentielles et respectent nos conditions
-                    générales d&apos;utilisation
-                  </p>
-                </div>
-                <div className="champs-obligatoire">
-                  <span className="asterisque">*</span>
-                  <span> Champ obligatoire</span>
-                </div>
-              </div>
-              <div className="fr-mt-2w">
-                <CapteurFormulaireDemandeAutodiagnostic />
-              </div>
-            </div>
-          </div>
-        </div>
-      </section>
+      </div>
+      <div className="fond-clair-mac icone-colonne-droite">
+        <img
+          src={illustrationAutodiag}
+          alt="Une personne devant un écran d'ordinateur"
+        />
+      </div>
     </main>
   );
 };
--- mon-aide-cyber-ui/src/domaine/auto-diagnostic/formulaire-demande-auto-diagnostic/FormulaireDemandeAutodiagnostic.tsx
+++ mon-aide-cyber-ui/src/domaine/auto-diagnostic/formulaire-demande-auto-diagnostic/FormulaireDemandeAutodiagnostic.tsx
@@ -1,17 +1,14 @@
 import Button from '../../../composants/atomes/Button/Button.tsx';
 import './formulaire-demande-autodiagnostic.scss';
-import { Input } from '../../../composants/atomes/Input/Input.tsx';
 import { FormEvent, useReducer } from 'react';
 import { ChampValidationCGUs } from '../../../composants/formulaires/ChampValidationCGUs.tsx';
 import {
-  adresseElectroniqueSaisie,
   cguCliquees,
   initialiseFormulaireDemandeAutodiagnostic,
   reducteurFormulaireDemandeAutodiagnostic,
 } from './reducteurFormulaireDemandeAutodiagnostic.ts';
 
 export type TypeFormulaireSaisieEmail = {
-  email: string;
   cguSignees: boolean;
 };
 
@@ -29,7 +26,6 @@ export const FormulaireDemandeAutodiagnostic = ({
     e.preventDefault();
 
     surSoumission({
-      email: etatFormulaire.email,
       cguSignees: etatFormulaire.cguValidees,
     });
   };
@@ -39,45 +35,15 @@ export const FormulaireDemandeAutodiagnostic = ({
       className="formulaire-demande-autodiagnostic"
       onSubmit={soumetFormulaire}
     >
-      <div className="fr-col-12">
-        <div
-          className={`fr-input-group ${
-            etatFormulaire.erreurs?.adresseElectronique
-              ? 'fr-input-group--error'
-              : ''
-          }`}
-        >
-          <label className="fr-label" htmlFor="adresse-electronique">
-            <span className="asterisque">*</span>
-            <span> Votre adresse électronique</span>
-          </label>
-          <Input
-            type="text"
-            id="adresse-electronique"
-            name="adresse-electronique"
-            onBlur={(e) =>
-              declencheChangement(adresseElectroniqueSaisie(e.target.value))
-            }
-            onChange={(e) =>
-              declencheChangement(adresseElectroniqueSaisie(e.target.value))
-            }
-          />
-          {etatFormulaire.erreurs?.adresseElectronique ? (
-            <p className="fr-error-text">
-              {etatFormulaire.erreurs?.adresseElectronique}
-            </p>
-          ) : null}
-        </div>
-      </div>
-      <div className="fr-col-12">
+      <fieldset>
         <ChampValidationCGUs
           sontValidees={etatFormulaire.cguValidees}
           surCguCliquees={() => declencheChangement(cguCliquees())}
         />
-      </div>
+      </fieldset>
       <div className="actions">
         <Button type="submit" disabled={!etatFormulaire.pretPourEnvoi}>
-          Accéder au diagnostic
+          Je commence le diagnostic
         </Button>
       </div>
     </form>
