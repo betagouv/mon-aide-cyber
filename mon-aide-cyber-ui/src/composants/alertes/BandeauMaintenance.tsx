57d4fc309d51978229c70b641a0e29cdff226939
Marc ETOURNEAU <marc.etourneau@beta.gouv.fr>
2024-12-10 17:08:32 +0100

    [REMANIEMENT][BANDEAU MAINTENANCE] Modifie la couleur du bandeau et permet de le masquer

--- mon-aide-cyber-ui/src/composants/alertes/BandeauMaintenance.tsx
+++ mon-aide-cyber-ui/src/composants/alertes/BandeauMaintenance.tsx
@@ -1,8 +1,26 @@
-export const BandeauMaintenance = (props: { creneauDeMaintenance: string }) => (
-  <div className="bandeau-maintenance">
-    <div className="fr-container">
-      MonAideCyber sera en maintenance <b>{props.creneauDeMaintenance}</b>. La
-      plate-forme sera indisponible durant ce créneau.
+import { useState } from 'react';
+
+export const BandeauMaintenance = (props: { creneauDeMaintenance: string }) => {
+  const [bandeauEstAffiche, afficheOuMasqueBandeau] = useState(true);
+
+  if (!bandeauEstAffiche) return null;
+
+  return (
+    <div className="bandeau-maintenance">
+      <div className="fr-container">
+        <div className="bandeau-maintenance-contenu">
+          <p>
+            MonAideCyber sera en maintenance <b>{props.creneauDeMaintenance}</b>
+            . La plate-forme sera indisponible durant ce créneau.
+          </p>
+
+          <span
+            onClick={() => afficheOuMasqueBandeau((precedent) => !precedent)}
+            className="fr-icon-close-line"
+            aria-hidden="true"
+          ></span>
+        </div>
+      </div>
     </div>
-  </div>
-);
+  );
+};
