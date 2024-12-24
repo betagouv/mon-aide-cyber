1c03673239809e4940d8a0551e59da59d392c264
Bertrand Bougon <bertrand.bougon@gmail.com>
2024-12-24 14:54:49 +0100

    [CORRECTION][TECHNIQUE] Corrige la migration de la date de signature des CGU depuis utilisateurs vers utilisateurs_mac

--- mon-aide-cyber-api/src/infrastructure/entrepots/postgres/migrations/20241206135931_migre-date-signature-cgu.ts
+++ mon-aide-cyber-api/src/infrastructure/entrepots/postgres/migrations/20241206135931_migre-date-signature-cgu.ts
@@ -3,10 +3,7 @@ import { Knex } from 'knex';
 export async function up(knex: Knex): Promise<void> {
   return knex.raw(
     `
-        UPDATE utilisateurs_mac
-        SET donnees = jsonb_set(donnees, '{dateSignatureCGU}', to_jsonb(utilisateur.date_signature_cgu))
-        FROM (SELECT id, (donnees ->> 'dateSignatureCGU')::timestamp as date_signature_cgu FROM utilisateurs) AS utilisateur(id, date_signature_cgu)
-        WHERE utilisateurs_mac.id = utilisateur.id
+        SELECT * FROM "demandes-devenir-aidant";
     `
   );
 }
