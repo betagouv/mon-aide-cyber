ae2c794ba68fc3fdc391716ecfdc82c9c7195be9
Bertrand Bougon <bertrand.bougon@gmail.com>
2024-12-30 16:02:50 +0100

    [ADMINISTRATION][RECHERCHE DEMANDE DEVENIR AIDANT] Ajoute une option pour faire une recherche sur l'email

--- mon-aide-cyber-api/src/administration/gestion-demandes/recherche/commande.ts
+++ mon-aide-cyber-api/src/administration/gestion-demandes/recherche/commande.ts
@@ -2,27 +2,39 @@ import { program } from 'commander';
 import { EntrepotDemandeDevenirAidantPostgres } from '../../../infrastructure/entrepots/postgres/EntrepotDemandeDevenirAidantPostgres';
 import { compareAsc } from 'date-fns';
 import { adaptateurServiceChiffrement } from '../../../infrastructure/adaptateurs/adaptateurServiceChiffrement';
+import { DemandeDevenirAidant } from '../../../gestion-demandes/devenir-aidant/DemandeDevenirAidant';
 
 const command = program
   .description('Obtenir le détail de la demande pour un Aidant donné')
-  .argument('<nomPrenomAidant>', 'Le nom ou prénom de l’Aidant');
+  .option('-n, --nom <nom>', 'Le nom ou prénom de l’Aidant')
+  .option('-m, --mail <mail>', "Tout ou partie de l'email recherché");
 
-command.action(async (...args: any[]) => {
-  const nomPrenom: string = args[0].toLowerCase().trim();
+type Predicat = (d: DemandeDevenirAidant) => boolean;
+
+command.action(async (options) => {
+  console.log('Options', options);
 
   const entrepot = new EntrepotDemandeDevenirAidantPostgres(
     adaptateurServiceChiffrement()
   );
+  let predicat: Predicat;
+  if (options.nom) {
+    const nomPrenom: string = options.nom.toLowerCase().trim();
+    predicat = (d: DemandeDevenirAidant) =>
+      d.nom.toLowerCase().trim().includes(nomPrenom) ||
+      d.prenom.toLowerCase().trim().includes(nomPrenom);
+  }
+  if (options.mail) {
+    const mail: string = options.mail.toLowerCase().trim();
+    predicat = (d: DemandeDevenirAidant) =>
+      d.mail.toLowerCase().trim().includes(mail);
+  }
 
   const demandes = await entrepot
     .tous()
     .then((demandes) =>
       demandes
-        .filter(
-          (d) =>
-            d.nom.toLowerCase().trim().includes(nomPrenom) ||
-            d.prenom.toLowerCase().trim().includes(nomPrenom)
-        )
+        .filter((d) => predicat(d))
         .sort((demandeA, demandeB) => compareAsc(demandeA.date, demandeB.date))
     );
 
