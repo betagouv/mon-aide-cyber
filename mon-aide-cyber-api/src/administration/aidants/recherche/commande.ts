e780cdcac8dfdb95cf594a47607d901dc6d4f883
Marc ETOURNEAU <marc.etourneau@beta.gouv.fr>
2025-01-31 11:12:56 +0100

    [COMMANDE ADMINISTRATION][RECHERCHE UTILISATEUR] Permet de rechercher un utilisateurMAC par email

--- mon-aide-cyber-api/src/administration/aidants/recherche/commande.ts
+++ mon-aide-cyber-api/src/administration/aidants/recherche/commande.ts
@@ -5,20 +5,28 @@ import { EntrepotUtilisateurMACPostgres } from '../../../infrastructure/entrepot
 program
   .description('Recherche les aidants')
   .option('-n, --nom <nom>', 'le nom ou partie du nom recherché')
+  .option('-m, --mail <mail>', 'le mail ou partie du mail recherché')
   .action(async (options) => {
     console.log('Recherche des aidants en cours');
     const aidants = await new EntrepotUtilisateurMACPostgres(
       adaptateurServiceChiffrement()
     ).tous();
 
-    const aidantsTrouves = aidants.filter((aidant) =>
-      options.nom
-        ? aidant.nomPrenom
-            .toLowerCase()
-            .trim()
-            .includes(options.nom.toLowerCase().trim())
-        : true
-    );
+    const aidantsTrouves = aidants.filter((aidant) => {
+      if (options.nom) {
+        return aidant.nomPrenom
+          .toLowerCase()
+          .trim()
+          .includes(options.nom.toLowerCase().trim());
+      }
+      if (options.mail) {
+        return aidant.email
+          .toLowerCase()
+          .trim()
+          .includes(options.mail.toLowerCase().trim());
+      }
+      return true;
+    });
     console.log(
       `Il y a %s aidant(s) et %s trouvé(s)`,
       aidants.length,
