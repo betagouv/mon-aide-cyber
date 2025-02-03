23df576e7e7ed6934c402a12bed9d6fe54497d1d
Bertrand Bougon <bertrand.bougon@gmail.com>
2025-02-03 08:51:10 +0100

    [ADMINISTRATION] Ajoute un service de recherche des comptes utilisateurs

---
+++ mon-aide-cyber-api/src/administration/utilisateurs/recherche/commande.ts
@@ -0,0 +1,99 @@
+import { program } from 'commander';
+import { adaptateurServiceChiffrement } from '../../../infrastructure/adaptateurs/adaptateurServiceChiffrement';
+import {
+  DTO,
+  EntrepotPostgres,
+} from '../../../infrastructure/entrepots/postgres/EntrepotPostgres';
+import { Aggregat } from '../../../domaine/Aggregat';
+import { ServiceDeChiffrement } from '../../../securite/ServiceDeChiffrement';
+
+type CompteUtilisateur = Aggregat & {
+  nomPrenom: string;
+  email: string;
+};
+
+type CompteUtilisateurDTO = DTO & {
+  nom_prenom: string;
+  mail: string;
+};
+
+class EntrepotCompteUtilisateur extends EntrepotPostgres<
+  CompteUtilisateur,
+  CompteUtilisateurDTO
+> {
+  constructor(private readonly serviceChiffrement: ServiceDeChiffrement) {
+    super();
+  }
+
+  async tous(): Promise<CompteUtilisateur[]> {
+    return this.knex
+      .raw(
+        `
+            SELECT id, donnees ->> 'nomPrenom' as nom_prenom, donnees ->> 'identifiantConnexion' as email
+            FROM utilisateurs
+        `
+      )
+      .then(({ rows }: { rows: CompteUtilisateurDTO[] }) => {
+        return rows;
+      })
+      .then((dtos) => {
+        return dtos.map((dto) => this.deDTOAEntite(dto));
+      });
+  }
+
+  protected champsAMettreAJour(_entiteDTO: DTO): Partial<DTO> {
+    throw new Error('Method not implemented.');
+  }
+
+  protected nomTable(): string {
+    return 'utilisateurs';
+  }
+
+  protected deEntiteADTO(_entite: Aggregat): CompteUtilisateurDTO {
+    throw new Error('Method not implemented.');
+  }
+
+  protected deDTOAEntite(dto: CompteUtilisateurDTO): CompteUtilisateur {
+    return {
+      identifiant: dto.id,
+      email: this.serviceChiffrement.dechiffre(dto.mail),
+      nomPrenom: this.serviceChiffrement.dechiffre(dto.nom_prenom),
+    };
+  }
+}
+
+program
+  .description('Recherche les comptes utilisateurs')
+  .option('-n, --nom <nom>', 'le nom ou partie du nom recherché')
+  .option('-m, --mail <mail>', 'le mail ou partie du mail recherché')
+  .action(async (options) => {
+    console.log('Recherche des aidants en cours');
+    const utilisateurs = await new EntrepotCompteUtilisateur(
+      adaptateurServiceChiffrement()
+    ).tous();
+
+    const utilisateursTrouves = utilisateurs.filter((utilisateur) => {
+      if (options.nom) {
+        return utilisateur.nomPrenom
+          .toLowerCase()
+          .trim()
+          .includes(options.nom.toLowerCase().trim());
+      }
+      if (options.mail) {
+        return utilisateur.email
+          .toLowerCase()
+          .trim()
+          .includes(options.mail.toLowerCase().trim());
+      }
+      return true;
+    });
+    console.log(
+      `Il y a %s utilisateur(s) et %s trouvé(s)`,
+      utilisateurs.length,
+      utilisateursTrouves.length
+    );
+    console.log(JSON.stringify(utilisateursTrouves, undefined, 2));
+    process.exit(0);
+  });
+
+program.parse();
