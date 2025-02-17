3036f6c327e9b56d035fc69b3e344aad90c697c1
Marc ETOURNEAU <marc.etourneau@beta.gouv.fr>
2025-02-17 17:11:56 +0100

    [POC][TESTS DE BOUT EN BOUT] Créé un premier test de visite de la page d'accueil du site

---
+++ mon-aide-cyber-ui/cypress/e2e/site-vitrine/Accueil.spec.cy.ts
@@ -0,0 +1,14 @@
+describe('La landing page', () => {
+  it('doit s‘afficher', () => {
+    cy.visit('/');
+  });
+
+  it('peut se rendre sur la page de demande d‘aide', () => {
+    cy.visit('/');
+
+    cy.contains(
+      '.fr-btn.bouton-mac.bouton-mac-primaire',
+      "Je fais une demande d'aide"
+    ).click();
+  });
+});
