0ffbc06bb3e1679232243ff6f87092fe34e5c442
Marc ETOURNEAU <marc.etourneau@beta.gouv.fr>
2025-02-18 10:02:56 +0100

    [POC][TESTS DE BOUT EN BOUT] Déplace le dossier cypress dans test

--- mon-aide-cyber-ui/cypress.config.ts
+++ mon-aide-cyber-ui/cypress.config.ts
@@ -3,6 +3,8 @@ import { defineConfig } from 'cypress';
 export default defineConfig({
   e2e: {
     baseUrl: 'http://localhost:8081',
+    supportFile: false,
+    specPattern: 'test/cypress/e2e/**/*.cy.{js,jsx,ts,tsx}',
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
