f42869898f207d6aab3c4ef0f5c60295241ba16c
Bertrand Bougon <bertrand.bougon@gmail.com>
2025-02-28 14:44:06 +0100

    [DEMANDE AIDE] Mets à jour une demande d'aide

---
+++ mon-aide-cyber-api/src/gestion-demandes/aide/CapteurCommandeMettreAJourDemandeAide.ts
@@ -0,0 +1,29 @@
+import { CapteurCommande, Commande } from '../../domaine/commande';
+import crypto from 'crypto';
+import { Departement } from '../departements';
+import { EntrepotDemandeAide } from './DemandeAide';
+import { FournisseurHorloge } from '../../infrastructure/horloge/FournisseurHorloge';
+
+export type CommandeMettreAJourDemandeAide = Commande & {
+  identifiant: crypto.UUID;
+  departement: Departement;
+  email: string;
+  raisonSociale?: string;
+  relationAidant: boolean;
+};
+
+export class CapteurCommandeMettreAJourDemandeAide
+  implements CapteurCommande<CommandeMettreAJourDemandeAide, void>
+{
+  constructor(private readonly entrepotDemandeAide: EntrepotDemandeAide) {}
+
+  execute(commande: CommandeMettreAJourDemandeAide): Promise<void> {
+    return this.entrepotDemandeAide.persiste({
+      identifiant: commande.identifiant,
+      email: commande.email,
+      departement: commande.departement,
+      ...(commande.raisonSociale && { raisonSociale: commande.raisonSociale }),
+      dateSignatureCGU: FournisseurHorloge.maintenant(),
+    });
+  }
+}
