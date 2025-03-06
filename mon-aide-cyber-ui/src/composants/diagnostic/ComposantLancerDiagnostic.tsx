d3d8866f068bfc99317b62c3393c578ecfcfeda7
Bertrand Bougon <bertrand.bougon@gmail.com>
2025-03-06 09:33:35 +0100

    [LANCER DIAGNOSTIC] Ajoute un champ email de l’entité afin de permettre la recherche de la demande correspondante

--- mon-aide-cyber-ui/src/composants/diagnostic/ComposantLancerDiagnostic.tsx
+++ mon-aide-cyber-ui/src/composants/diagnostic/ComposantLancerDiagnostic.tsx
@@ -1,5 +1,4 @@
 import { ReactElement, useCallback, useState } from 'react';
-import { useErrorBoundary } from 'react-error-boundary';
 import { useModale, useNavigationMAC } from '../../fournisseurs/hooks.ts';
 
 import { constructeurParametresAPI } from '../../fournisseurs/api/ConstructeurParametresAPI.ts';
@@ -11,6 +10,9 @@ import { Lien } from '../../domaine/Lien.ts';
 import { useMACAPI } from '../../fournisseurs/api/useMACAPI.ts';
 import Button from '../atomes/Button/Button.tsx';
 import { useNavigueVersModifierDiagnostic } from '../../fournisseurs/ContexteNavigationMAC.tsx';
+import { Input } from '../atomes/Input/Input.tsx';
+import './lancer-diagnostic.scss';
+import { ChampsErreur } from '../alertes/Erreurs.tsx';
 
 type ProprietesComposant = {
   surClick: () => void;
@@ -48,124 +50,121 @@ export const ComposantLienCreerDiagnostic = ({
     </Button>
   );
 };
-
-function ValidationCGU(proprietesValidationCGU: {
+const RealiserUnDiagnostic = (proprietesRealiserUnDiagnostic: {
   surFermeture: () => void;
-  surValidation: () => void;
-}) {
-  const [cguValidees, setCguValidees] = useState(false);
+  surValidation: (emailEntiteAidee: string) => void;
+  erreur: ReactElement | undefined;
+}) => {
+  const [emailEntiteAidee, setEmailEntiteAidee] = useState('');
 
-  const validationCGU = useCallback(() => {
-    setCguValidees(!cguValidees);
-  }, [cguValidees]);
+  const surSaisieEmailEntiteAidee = useCallback(
+    (email: string) => setEmailEntiteAidee(email),
+    []
+  );
 
   return (
     <>
       <section>
         <div>
-          Vous vous apprêtez à réaliser un diagnostic.
-          <br />
-          Est-ce-que le bénéficiaire a rempli le formulaire de demande
-          d&apos;aide sur MonAideCyber ?
-        </div>
-        <div className="fr-radio-group mac-radio-group fr-pt-4w">
-          <input
-            type="radio"
-            name="radio-validation-cgu"
-            id="radio-validation-cgu-oui"
-            onChange={() => validationCGU()}
-          />
-          <label className="fr-label" htmlFor="radio-validation-cgu-oui">
+          <fieldset>
             <div>
-              Le bénéficiaire confirme avoir rempli{' '}
-              <a href="/beneficier-du-dispositif/etre-aide">
-                le formulaire de demande d&apos;aide
-              </a>
-              .
+              <b>L’entité bénéficiaire a fait une demande en ligne</b>
             </div>
-          </label>
-        </div>
-        <div className="fr-radio-group mac-radio-group fr-pt-4w">
-          <input
-            type="radio"
-            name="radio-validation-cgu"
-            id="radio-validation-cgu-non"
-            onChange={() => setCguValidees(false)}
-          />
-          <label className="fr-label" htmlFor="radio-validation-cgu-non">
-            <div>
-              Le bénéficiaire n&apos;a pas encore rempli{' '}
-              <a href="/beneficier-du-dispositif/etre-aide">
-                le formulaire de demande d&apos;aide
-              </a>
-              .
-              <br />
-              <br />
-              <b>
-                Vous devez faire valider les CGU au bénéficiaire de
-                l&apos;entité aidée.
-              </b>
-              <br />
-              L&apos;utilisation du diagnostic nécessite l&apos;acceptation des
-              CGU par le bénéficiaire. Veuillez l&apos;orienter vers{' '}
-              <a href="/beneficier-du-dispositif/etre-aide">
-                le lien du formulaire
-              </a>{' '}
-              afin qu&apos;il puisse accepter les CGU avant de démarrer le
-              diagnostic
-              <br />
-              <a
-                href={`${import.meta.env['VITE_URL_MAC']}/beneficier-du-dispositif/etre-aide`}
+            <div className="fr-pt-2w">
+              Si l’entité bénéficiaire a complété le formulaire de demande
+              d’aide, veuillez indiquer l’adresse email utilisée ci-dessous.
+            </div>
+            <div className="fr-input-group">
+              <label
+                className="fr-label fr-pt-2w"
+                htmlFor="saisie-email-entite-aidee"
               >
-                {`${import.meta.env['VITE_URL_MAC']}/beneficier-du-dispositif/etre-aide`}
-              </a>
+                Adresse électronique de l’entité :
+              </label>
+              <Input
+                type="text"
+                className="fr-pt-2w"
+                id="saisie-email-entite-aidee"
+                name="saisie-email-entite-aidee"
+                onChange={(e) => surSaisieEmailEntiteAidee(e.target.value)}
+              />
+              {proprietesRealiserUnDiagnostic.erreur}
             </div>
-          </label>
+          </fieldset>
+          <div className="texte-centre fr-pt-2w">
+            <hr className="separation-realiser-diagnostic" />
+          </div>
+          <div>
+            <div>
+              <b>
+                Vous initiez un diagnostic à une entité qui n’a pas fait de
+                demande en ligne
+              </b>
+            </div>
+            <div className="fr-pt-2w">
+              Si l’entité bénéficiaire n’a pas fait de demande en ligne,
+              veuillez lui communiquer le lien vers le formulaire en ligne, et
+              lui indiquer de renseigner votre adresse électronique afin que la
+              demande vous soit attribuée.
+            </div>
+            <div className="fr-pt-2w">
+              Le lien à communiquer :
+              <br />-
+              <a
+                href={`${import.meta.env['VITE_URL_MAC']}/beneficier-du-dispositif/etre-aide#formulaire-demande-aide`}
+              >{`${import.meta.env['VITE_URL_MAC']}/beneficier-du-dispositif/etre-aide#formulaire-demande-aide`}</a>
+            </div>
+          </div>
         </div>
         <div className="fr-pt-4w alignement-droite">
-          <button
+          <Button
             type="button"
+            variant="secondary"
             key="annule-validation-cgu-entite"
-            className="fr-btn bouton-mac bouton-mac-secondaire fr-mr-2w"
-            onClick={proprietesValidationCGU.surFermeture}
+            className="fr-mr-2w"
+            onClick={proprietesRealiserUnDiagnostic.surFermeture}
           >
             Retour à la liste des diagnostics
-          </button>
-          <button
-            disabled={!cguValidees}
+          </Button>
+          <Button
+            disabled={!(emailEntiteAidee.trim().length > 0)}
             type="button"
             key="validation-cgu-entite"
-            className="fr-btn bouton-mac bouton-mac-primaire"
             onClick={() =>
-              cguValidees && proprietesValidationCGU.surValidation()
+              emailEntiteAidee.trim().length > 0 &&
+              proprietesRealiserUnDiagnostic.surValidation(emailEntiteAidee)
             }
           >
             Commencer le diagnostic
-          </button>
+          </Button>
         </div>
       </section>
     </>
   );
-}
+};
 
 export type FormatLien = `/api/${string}`;
+type CorpsLancerDiagnostic = {
+  emailEntiteAidee: string;
+};
 export const ComposantLancerDiagnostic = ({
   composant,
 }: ProprietesComposantLancerDiagnostic) => {
-  const { showBoundary } = useErrorBoundary();
   const navigationMAC = useNavigationMAC();
   const macAPI = useMACAPI();
   const { navigue } = useNavigueVersModifierDiagnostic(
     `${ROUTE_MON_ESPACE}/diagnostic`
   );
+  const [erreur, setErreur] = useState<ReactElement | undefined>(undefined);
 
   const { affiche, ferme } = useModale();
 
   const lanceDiagnostic = useCallback(
-    (lien: Lien) => {
+    (lien: Lien, emailEntiteAidee: string) => {
       macAPI
-        .execute<string, FormatLien>(
-          constructeurParametresAPI()
+        .execute<string, FormatLien, CorpsLancerDiagnostic>(
+          constructeurParametresAPI<CorpsLancerDiagnostic>()
+            .corps({ emailEntiteAidee })
             .url(lien.url)
             .methode(lien.methode!)
             .construis(),
@@ -177,28 +176,32 @@ export const ComposantLancerDiagnostic = ({
             methode: 'GET',
           });
         })
-        .catch((erreur) => showBoundary(erreur));
+        .catch((erreur) => setErreur(<ChampsErreur erreur={erreur} />));
     },
-    [navigationMAC, showBoundary]
+    [navigationMAC]
   );
 
-  const lancerDiagnostic = useCallback(async () => {
-    new MoteurDeLiens(navigationMAC.etat).trouve(
-      'lancer-diagnostic',
-      (lien: Lien) => lanceDiagnostic(lien)
-    );
-  }, [navigationMAC.etat, lanceDiagnostic]);
+  const lancerDiagnostic = useCallback(
+    async (emailEntiteAidee: string) => {
+      new MoteurDeLiens(navigationMAC.etat).trouve(
+        'lancer-diagnostic',
+        (lien: Lien) => lanceDiagnostic(lien, emailEntiteAidee)
+      );
+    },
+    [navigationMAC.etat, lanceDiagnostic]
+  );
 
   const afficherModale = () => {
     affiche({
-      titre: 'Prérequis à la réalisation du diagnostic',
+      titre: 'Réaliser un diagnostic',
       corps: (
-        <ValidationCGU
+        <RealiserUnDiagnostic
           surFermeture={ferme}
-          surValidation={async () => {
+          surValidation={async (emailEntiteAidee) => {
             ferme();
-            await lancerDiagnostic();
+            await lancerDiagnostic(emailEntiteAidee);
           }}
+          erreur={erreur}
         />
       ),
       taille: 'moyenne',
