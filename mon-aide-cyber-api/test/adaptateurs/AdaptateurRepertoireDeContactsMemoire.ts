4114c21a7e0f4d8496a4f7fab054e5a533bca691
Christophe Cadilhac <christophe.cadilhac@beta.gouv.fr>
2025-03-13 15:09:12 +0100

    [UTILISATEUR INSCRIT] Utilise le répertoire de contacts pour créer les utilisateurs inscrits à la première connexion
    On empile sur l'abstraction `RepertoireDeContacts`.

--- mon-aide-cyber-api/test/adaptateurs/AdaptateurRepertoireDeContactsMemoire.ts
+++ mon-aide-cyber-api/test/adaptateurs/AdaptateurRepertoireDeContactsMemoire.ts
@@ -1,10 +1,12 @@
 import { RepertoireDeContacts } from '../../src/contacts/RepertoireDeContacts';
 import { Aidant } from '../../src/espace-aidant/Aidant';
+import { UtilisateurInscrit } from '../../src/espace-utilisateur-inscrit/UtilisateurInscrit';
 import { DemandeAide } from '../../src/gestion-demandes/aide/DemandeAide';
 
 export class RepertoireDeContactsMemoire implements RepertoireDeContacts {
   public aidants: string[] = [];
   public aides: string[] = [];
+  public utilisateursInscrits: string[] = [];
 
   async creeAidant(aidant: Aidant): Promise<void> {
     this.aidants.push(aidant.email);
@@ -12,5 +14,9 @@ export class RepertoireDeContactsMemoire implements RepertoireDeContacts {
 
   async creeAide(aide: DemandeAide): Promise<void> {
     this.aides.push(aide.email);
+  }
+
+  async creeUtilisateurInscrit(utilisateur: UtilisateurInscrit): Promise<void> {
+    this.utilisateursInscrits.push(utilisateur.email);
   }
 }
