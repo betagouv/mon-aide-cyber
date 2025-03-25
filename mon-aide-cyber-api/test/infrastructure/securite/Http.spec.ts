d36530efe0cfbe2085262e2c4d6d87a6634de189
Christophe Cadilhac <christophe.cadilhac@beta.gouv.fr>
2025-03-25 15:12:01 +0100

    [UI-KIT] Génère un nonce à chaque requête HTTP
    … pour le mettre dans la CSP `style-src` d'une part et dans la balise `<script>` de l'UI Kit d'autre part.
    
    De cette façon, l'UI Kit peut positionner ce nonce sur sa propre balise `<style>` et ainsi être autorisé par le navigateur à appliquer du style dans les WebComponents.

--- mon-aide-cyber-api/test/infrastructure/securite/Http.spec.ts
+++ mon-aide-cyber-api/test/infrastructure/securite/Http.spec.ts
@@ -1,20 +1,21 @@
 import { describe, expect, it, beforeEach } from 'vitest';
-import {
-  ContientHeader,
-  positionneLesCsp,
-} from '../../../src/infrastructure/securite/Http';
+import { Response } from 'express';
+import { positionneLesCsp } from '../../../src/infrastructure/securite/Http';
+
+type ResponsePourExpect = Response & { headers: Record<string, string> };
 
 describe('Le sécurité HTTP', () => {
   describe('sur le positionnement des CSP (Content-Security-Policy)', () => {
-    let reponseSpy: { headers: Record<string, string> } & ContientHeader;
+    let reponseSpy: ResponsePourExpect;
 
     beforeEach(() => {
       reponseSpy = {
         headers: {},
+        locals: {},
         setHeader: (cle: string, valeur: string) => {
           reponseSpy.headers[cle] = valeur;
         },
-      };
+      } as unknown as ResponsePourExpect;
     });
 
     it("positionne une valeur « * » si aucune valeur n'est passée", () => {
@@ -29,6 +30,20 @@ describe('Le sécurité HTTP', () => {
       expect(reponseSpy.headers['Content-Security-Policy']).toBe(
         "default-src 'self';"
       );
+    });
+
+    it('ajoute un nonce aléatoire à « style-src »', () => {
+      positionneLesCsp(reponseSpy, "default-src 'self'; style-src 'self';");
+
+      expect(reponseSpy.headers['Content-Security-Policy']).toMatch(
+        /default-src 'self'; style-src 'self' 'nonce-[A-Za-z0-9+/=]{24}';/
+      );
+    });
+
+    it("stocke le nonce aléatoire du style dans `reponse.locals` pour permettre aux suivants d'y avoir accès", () => {
+      positionneLesCsp(reponseSpy, "default-src 'self'; style-src 'self';");
+
+      expect(reponseSpy.locals.nonce).toMatch(/[A-Za-z0-9+/=]{24}/);
     });
   });
 });
