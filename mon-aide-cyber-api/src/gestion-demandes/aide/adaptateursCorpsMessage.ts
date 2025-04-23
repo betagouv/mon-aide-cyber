9e40b04b31efba5ae5cecc4a8b8988d826838018
Bertrand Bougon <bertrand.bougon@gmail.com>
2025-04-23 11:06:56 +0200

    [MISE EN RELATION] Supprime la sollicitation du backend

--- mon-aide-cyber-api/src/gestion-demandes/aide/adaptateursCorpsMessage.ts
+++ mon-aide-cyber-api/src/gestion-demandes/aide/adaptateursCorpsMessage.ts
@@ -1,97 +1,5 @@
-import { adaptateurEnvironnement } from '../../adaptateurs/adaptateurEnvironnement';
 import { FournisseurHorloge } from '../../infrastructure/horloge/FournisseurHorloge';
-import { Departement } from '../departements';
 import { DemandeAide } from './DemandeAide';
-
-export type ProprietesMessageAidant = {
-  nomPrenom: string;
-  departement: Departement;
-  mailEntite: string;
-  raisonSocialeEntite?: string;
-};
-
-const genereCorpsNotificationAidantSollicitation = (
-  proprietesMessage: ProprietesMessageAidant
-): string => {
-  return (
-    '<html lang="fr">' +
-    '<body>' +
-    `Bonjour ${proprietesMessage.nomPrenom},\n` +
-    '\n' +
-    `Une entité ${proprietesMessage.raisonSocialeEntite ? `(${proprietesMessage.raisonSocialeEntite})` : ''} vous a sollicité sur l’annuaire des Aidants cyber car elle souhaite bénéficier avec vous d’un diagnostic MonAideCyber dans le département <b>${proprietesMessage.departement.nom}</b>.\n` +
-    '\n' +
-    'Voici ses coordonnées afin de programmer ensemble la réalisation du prochain diagnostic :' +
-    '\n' +
-    `${proprietesMessage.mailEntite}` +
-    '\n' +
-    'Au cours de sa demande, l’entité a accepté les CGU.\n' +
-    '\n' +
-    'Lors du rendez-vous, vous pourrez créer le diagnostic sur votre espace aidant comme vous le faites actuellement.\n' +
-    'Il est conseillé de réaliser le diagnostic en présentiel, la visioconférence est cependant tolérée.\n' +
-    '\n' +
-    `En cas d’empêchement de votre part, nous vous remercions d’envoyer un mail à l’adresse <a href="mailto:contact@monaidecyber.beta.gouv.fr?subject=[MonAideCyber] Indisponibilité réalisation diagnostic dans le département ${proprietesMessage.departement.nom}">contact@monaidecyber.beta.gouv.fr</a> afin que la demande soit attribuée à un autre Aidant cyber.\n` +
-    '\n' +
-    'Toute l’équipe reste à votre disposition,\n' +
-    '\n' +
-    '<b>L’équipe MonAideCyber</b>' +
-    '</body>' +
-    '</html>'
-  );
-};
-
-export type ProprietesMessageRecapitulatifSollicitationAide = {
-  nomPrenom: string;
-  departement: Departement;
-};
-
-const genereRecapitulatifSollicitationAide = (
-  proprietesMessage: ProprietesMessageRecapitulatifSollicitationAide
-) =>
-  '<html lang="fr">' +
-  '<body>' +
-  'Bonjour\n' +
-  '\n' +
-  `Vous avez sollicité l'Aidant cyber ${proprietesMessage.nomPrenom} référencé sur l’annuaire MonAideCyber pour la réalisation d’un diagnostic dans le département ${proprietesMessage.departement.nom}.\n` +
-  '\n' +
-  "Votre demande a bien été affectée à l'Aidant cyber qui prendra contact avec vous prochainement en vue de convenir d’un rendez-vous.\n" +
-  '\n' +
-  "En cas d’empêchement de l'Aidant cyber, nous allons automatiquement attribuer votre demande à un Aidant cyber disponible sur le même département.\n" +
-  '\n' +
-  'Vous serez informé de ce changement par mail.\n' +
-  '\n' +
-  '<h3>Quelques conseils pour préparer son rdv</h3>\n' +
-  '\t - Prévoyez un <b>créneau d’1H30</b> en moyenne\n' +
-  '\t - Il est fortement recommandé qu’un membre du comité de direction et votre prestataire informatique (si existant) soient également présents durant le diagnostic\n' +
-  `\t - Consultez notre <a href="${adaptateurEnvironnement.mac().urlMAC()}/kit-de-communication" target="_blank">kit de communication</a> pour mieux comprendre le dispositif MonAideCyber` +
-  '\n' +
-  'Toute l’équipe reste à votre disposition,\n' +
-  '\n' +
-  "<b>L'équipe MonAideCyber</b>" +
-  '</body>' +
-  '</html>';
-
-export type ProprietesMessageRecapitulatif = {
-  raisonSociale?: string;
-  mailEntite: string;
-  aidant: string;
-  departement: Departement;
-};
-
-export type MessagesSollicitation = {
-  notificationAidantSollicitation: () => {
-    genereCorpsMessage: (proprietesMessage: ProprietesMessageAidant) => string;
-  };
-  recapitulatifMAC: () => {
-    genereCorpsMessage: (
-      proprietesMessage: ProprietesMessageRecapitulatif
-    ) => string;
-  };
-  recapitulatifSollicitationAide: (
-    proprietesMessage: ProprietesMessageRecapitulatifSollicitationAide
-  ) => {
-    genereCorpsMessage: () => string;
-  };
-};
 
 export type MessagesDemande = {
   recapitulatifDemandeAide: () => {
@@ -103,7 +11,6 @@ export type MessagesDemande = {
 };
 
 export type AdaptateurCorpsDeMessageAide = {
-  sollicitation: () => MessagesSollicitation;
   demande: () => MessagesDemande;
 };
 
@@ -133,35 +40,6 @@ const genereCorpsRecapitulatifDemandeAide = (
   );
 };
 const adaptateursCorpsMessage: AdaptateurCorpsDeMessageAide = {
-  sollicitation: (): MessagesSollicitation => ({
-    notificationAidantSollicitation: () => ({
-      genereCorpsMessage: (
-        proprietesMessage: ProprietesMessageAidant
-      ): string =>
-        genereCorpsNotificationAidantSollicitation(proprietesMessage),
-    }),
-    recapitulatifMAC: () => ({
-      genereCorpsMessage: (proprietes: ProprietesMessageRecapitulatif) => {
-        return (
-          '<html lang="fr">' +
-          '<body>' +
-          'Bonjour,\n' +
-          '\n' +
-          `Aujourd’hui, l’entité ${proprietes.raisonSociale ? `(${proprietes.raisonSociale})` : ''} sous l’adresse ${proprietes.mailEntite} a sollicité l’Aidant cyber ${proprietes.aidant} situé dans le département ${proprietes.departement.nom}.\n` +
-          '\n' +
-          "L'équipe MonAideCyber" +
-          '</body>' +
-          '</html>'
-        );
-      },
-    }),
-    recapitulatifSollicitationAide: (
-      proprietesMessage: ProprietesMessageRecapitulatifSollicitationAide
-    ) => ({
-      genereCorpsMessage: (): string =>
-        genereRecapitulatifSollicitationAide(proprietesMessage),
-    }),
-  }),
   demande: (): MessagesDemande => ({
     recapitulatifDemandeAide: () => ({
       genereCorpsMessage: (aide, relationUtilisateur): string =>
--- mon-aide-cyber-api/test/gestion-demandes/aide/ConstructeurAdaptateurDeCorpsDeMessage.ts
+++ mon-aide-cyber-api/test/gestion-demandes/aide/ConstructeurAdaptateurDeCorpsDeMessage.ts
@@ -2,21 +2,12 @@ import { Constructeur } from '../../constructeurs/constructeur';
 import {
   AdaptateurCorpsDeMessageAide,
   MessagesDemande,
-  MessagesSollicitation,
-  ProprietesMessageAidant,
-  ProprietesMessageRecapitulatif,
-  ProprietesMessageRecapitulatifSollicitationAide,
 } from '../../../src/gestion-demandes/aide/adaptateursCorpsMessage';
 import { DemandeAide } from '../../../src/gestion-demandes/aide/DemandeAide';
 
 class ConstructeurAdaptateurDeCorpsDeMessage
   implements Constructeur<AdaptateurCorpsDeMessageAide>
 {
-  private _notificationAidant: (proprietes: ProprietesMessageAidant) => string =
-    (_proprietes) => 'Bonjour Aidant!';
-  private _recapitulatifMAC: (
-    proprietes: ProprietesMessageRecapitulatif
-  ) => string = (_proprietes) => 'Bonjour MAC!';
   private _recapitulatifDemandeAide: (
     _aide: DemandeAide,
     _relationUtilisateur: string | undefined
@@ -24,20 +15,6 @@ class ConstructeurAdaptateurDeCorpsDeMessage
     _aide: DemandeAide,
     _relationUtilisateur: string | undefined
   ) => 'Bonjour une entité a fait une demande d’aide';
-
-  notificationAidant(
-    notification: (proprietes: ProprietesMessageAidant) => string
-  ): ConstructeurAdaptateurDeCorpsDeMessage {
-    this._notificationAidant = notification;
-    return this;
-  }
-
-  recapitulatifMac(
-    recapitulatif: (proprietes: ProprietesMessageRecapitulatif) => string
-  ): ConstructeurAdaptateurDeCorpsDeMessage {
-    this._recapitulatifMAC = recapitulatif;
-    return this;
-  }
 
   recapitulatifDemandeAide(
     recapitulatif: (
@@ -51,22 +28,6 @@ class ConstructeurAdaptateurDeCorpsDeMessage
 
   construis(): AdaptateurCorpsDeMessageAide {
     return {
-      sollicitation: (): MessagesSollicitation => ({
-        notificationAidantSollicitation: () => ({
-          genereCorpsMessage: (proprietesMessage: ProprietesMessageAidant) =>
-            this._notificationAidant(proprietesMessage),
-        }),
-        recapitulatifMAC: () => ({
-          genereCorpsMessage: (
-            proprietesMessage: ProprietesMessageRecapitulatif
-          ) => this._recapitulatifMAC(proprietesMessage),
-        }),
-        recapitulatifSollicitationAide: (
-          _proprietesMessage: ProprietesMessageRecapitulatifSollicitationAide
-        ) => ({
-          genereCorpsMessage: () => 'Bonjour Aidé!',
-        }),
-      }),
       demande: (): MessagesDemande => ({
         recapitulatifDemandeAide: () => ({
           genereCorpsMessage: (aide, relationUtilisateur) =>
