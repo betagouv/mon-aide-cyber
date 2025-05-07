ab47c6520eee5fe7205d16e7ed77d85091394a67
Bertrand Bougon <bertrand.bougon@gmail.com>
2025-05-07 10:14:20 +0200

    [MISE EN RELATION][METABASE] S’assure que tous les identifiants sont hashés
    notamment dans les objets structurés imbriqués ce qui n’était pas le cas jusqu’à présent :
    - {chaine1: 'uuid-1', objet2: {chaine2: 'uuid-2'}} => {chaine1: 'hash-1', objet2: {chaine2: 'hash-2'}}

--- mon-aide-cyber-api/test/infrastructure/entrepots/postgres/EntrepotJournalisationPostgres.spec.ts
+++ mon-aide-cyber-api/test/infrastructure/entrepots/postgres/EntrepotJournalisationPostgres.spec.ts
@@ -21,6 +21,11 @@ class ConstructeurPublication {
       type: this.type,
     };
   }
+
+  avecLesDonnees(donnees: object): ConstructeurPublication {
+    this.donnees = donnees;
+    return this;
+  }
 }
 
 function unePublication() {
@@ -45,6 +50,13 @@ class EntrepotJournalisationPostgresTests extends EntrepotJournalisationPostgres
 }
 
 describe('Entrepot Journalisation Postgres', () => {
+  const entrepotJournalisation = new EntrepotJournalisationPostgres(
+    configurationJournalisation
+  );
+  const entrepotLecture = new EntrepotJournalisationPostgresTests(
+    configurationJournalisation
+  );
+
   afterEach(async () => {
     await nettoieLaBaseDeDonneesJournal();
   });
@@ -52,15 +64,47 @@ describe('Entrepot Journalisation Postgres', () => {
   it('persiste une publication', async () => {
     const publication = unePublication().construis();
 
-    await new EntrepotJournalisationPostgres(
-      configurationJournalisation
-    ).persiste(publication);
+    await entrepotJournalisation.persiste(publication);
 
-    const entrepotJournalisationPostgresLecture =
-      new EntrepotJournalisationPostgresTests(configurationJournalisation);
+    expect(await entrepotLecture.lis(publication.identifiant)).toStrictEqual(
+      publication
+    );
+  });
+
+  it('hashe les UUID présents dans le payload', async () => {
+    const publication = unePublication()
+      .avecLesDonnees({
+        id1: '85319266-9752-409f-86c6-0dd0fb9bf91f',
+        tableau: ['1', '2', '3'],
+        objet1: {
+          id2: '53a40fb9-1440-4086-bd13-d37c67240442',
+          objet2: { id3: 'bb8475d2-1a69-4e8c-b636-84892f1013f9' },
+          chaine: 'une-chaine',
+          tableau: ['1', '2', '3'],
+        },
+      })
+      .construis();
+
+    await entrepotJournalisation.persiste(publication);
 
     expect(
-      await entrepotJournalisationPostgresLecture.lis(publication.identifiant)
-    ).toStrictEqual(publication);
+      await entrepotLecture.lis(publication.identifiant)
+    ).toStrictEqual<Publication>({
+      identifiant: publication.identifiant,
+      date: publication.date,
+      type: publication.type,
+      donnees: {
+        id1: '84a0fec36f5c4ad7b30387ac267fdaff25c4fca87b9ad473d80f8608e8860a14',
+        tableau: ['1', '2', '3'],
+        objet1: {
+          id2: 'caa61c8f3be62e7a8d0c1b53f815731be6b283d35d1d9387dae26cec156652b3',
+          objet2: {
+            id3: '01bc91195b06c8307528aab19e64839b807b993869a3b71a48786165ebe28b80',
+          },
+          chaine: 'une-chaine',
+          tableau: ['1', '2', '3'],
+        },
+      },
+    });
   });
 });
