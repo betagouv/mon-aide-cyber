e46f826aad6e517afd82283979601f0eab3c9177
Bertrand Bougon <bertrand.bougon@gmail.com>
2025-05-19 17:24:08 +0200

    [POSTULER] Émets un événement avec le statut DEJA_POURVUE lorsque la demande est déjà attribuée

--- mon-aide-cyber-api/test/gestion-demandes/aide/CapteurCommandeAttribueDemandeAide.spec.ts
+++ mon-aide-cyber-api/test/gestion-demandes/aide/CapteurCommandeAttribueDemandeAide.spec.ts
@@ -1,4 +1,4 @@
-import { describe, expect, it } from 'vitest';
+import { assert, describe, expect, it } from 'vitest';
 import { AdaptateurRelationsMAC } from '../../../src/relation/AdaptateurRelationsMAC';
 import { EntrepotRelationMemoire } from '../../../src/relation/infrastructure/EntrepotRelationMemoire';
 import {
@@ -93,5 +93,43 @@ describe("Capteur de commande d'attribution de demande d'aide", () => {
         identifiantAidant: deuxiemeAidant,
       })
     ).rejects.toThrow(new DemandeAideDejaPourvue());
+  });
+
+  it('Publie un événement de « DEMANDE_AIDE_POURVUE » avec un statut à « DEJA_POURVUE » lorsque la demande est déjà attribuée', async () => {
+    const bus = new BusEvenementDeTest();
+    const maintenant = new Date();
+    FournisseurHorlogeDeTest.initialise(maintenant);
+    const relations = new AdaptateurRelationsMAC(new EntrepotRelationMemoire());
+    await relations.attribueDemandeAAidant(
+      '22222222-2222-2222-2222-222222222222',
+      'AAAAAAAA-1111-1111-1111-111111111111'
+    );
+
+    const deuxiemeAidant = 'BBBBBBBB-1111-1111-1111-111111111111';
+    const capteur = new CapteurCommandeAttribueDemandeAide(
+      new AdaptateurEnvoiMailMemoire(),
+      relations,
+      bus
+    );
+    try {
+      await capteur.execute({
+        type: 'CommandeAttribueDemandeAide',
+        identifiantDemande: '22222222-2222-2222-2222-222222222222',
+        emailDemande: 'demande@societe.fr',
+        identifiantAidant: deuxiemeAidant,
+      });
+      assert.fail('Ce test doit échouer car la demande est déjà pourvue');
+    } catch (_erreur) {
+      expect(bus.evenementRecu).toStrictEqual<DemandeAidePourvue>({
+        identifiant: expect.any(String),
+        type: 'DEMANDE_AIDE_POURVUE',
+        date: maintenant,
+        corps: {
+          identifiantDemande: '22222222-2222-2222-2222-222222222222',
+          identifiantAidant: deuxiemeAidant,
+          statut: 'DEJA_POURVUE',
+        },
+      });
+    }
   });
 });
