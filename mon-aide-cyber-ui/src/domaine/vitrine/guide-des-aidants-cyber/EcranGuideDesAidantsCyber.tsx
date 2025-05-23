0745809d1bfd2fef2df7a3c599d634ccccb2c9bf
Christophe Cadilhac <christophe.cadilhac@beta.gouv.fr>
2025-05-23 11:22:30 +0200

    [CRISP] Intègre le menu desktop

--- mon-aide-cyber-ui/src/domaine/vitrine/guide-des-aidants-cyber/EcranGuideDesAidantsCyber.tsx
+++ mon-aide-cyber-ui/src/domaine/vitrine/guide-des-aidants-cyber/EcranGuideDesAidantsCyber.tsx
@@ -51,15 +51,26 @@ export const EcranGuideDesAidantsCyber = () => {
   return (
     <main role="main" className="page-crisp">
       <div className="chapeau">
-        <h1>{data?.titre}</h1>
+        <h1 className="fr-container">{data?.titre}</h1>
       </div>
       <MenuLateralCrispMobile
         tableDesMatieres={data?.tableDesMatieres.filter(
           (e) => e.profondeur === 2
         )}
       />
-      <div className="article">
+      <div className="article fr-container">
         <div className="contenu-section">
+          <div className="sommaire sommaire-deplie">
+            <ul>
+              {data?.tableDesMatieres
+                .filter((e) => e.profondeur === 2)
+                .map((entree) => (
+                  <li key={entree.id}>
+                    <a href={`#${entree.id}`}>{entree.texte}</a>
+                  </li>
+                ))}
+            </ul>
+          </div>
           <div
             className="contenu"
             dangerouslySetInnerHTML={{ __html: data?.contenu || '' }}
