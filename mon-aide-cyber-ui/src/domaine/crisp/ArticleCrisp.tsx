60e9fc085afef41c38e5cd03c2a56b368148b417
Christophe Cadilhac <christophe.cadilhac@beta.gouv.fr>
2025-05-23 12:38:55 +0200

    [CRISP] Gère la section active de menu via `IntersectionObserver`
    … qui est littéralement fait pour ça.
    Donc, on cesse d'utiliser le `.js.` qui a un handler sur l'événement `scroll`

--- mon-aide-cyber-ui/src/domaine/crisp/ArticleCrisp.tsx
+++ mon-aide-cyber-ui/src/domaine/crisp/ArticleCrisp.tsx
@@ -1,12 +1,39 @@
+import { useEffect } from 'react';
 import { ReponseArticle } from './Crisp.types.ts';
 import { MenuCrispMobile } from './MenuCrispMobile.tsx';
 import { MenuCrispDesktop } from './MenuCrispDesktop.tsx';
 import './article-crisp.scss';
-import './synchro-menu-crisp.js';
 import useDefilementFluide from '../../hooks/useDefilementFluide.ts';
+
+const synchroniseMenuEtContenu = () => {
+  const observateurDIntersection = new IntersectionObserver(
+    (entries) => {
+      entries.forEach((section) => {
+        const titreDeLaSection = section.target.querySelector('h2');
+        const lesLiens = document.querySelectorAll(
+          `.sommaire ul li a[href='#${titreDeLaSection!.id}']`
+        );
+
+        if (!lesLiens) return;
+
+        if (section.isIntersecting)
+          lesLiens.forEach((l) => l.parentElement!.classList.add('actif'));
+        else
+          lesLiens.forEach((l) => l.parentElement!.classList.remove('actif'));
+      });
+    },
+    { rootMargin: '-30% 0% -62% 0%' }
+  );
+
+  const lesSections = document.querySelectorAll('.contenu section');
+  lesSections.forEach((s) => observateurDIntersection.observe(s));
+  return () =>
+    lesSections.forEach((s) => observateurDIntersection.unobserve(s));
+};
 
 export function ArticleCrisp(props: { article: ReponseArticle }) {
   useDefilementFluide();
+  useEffect(() => synchroniseMenuEtContenu(), []);
 
   return (
     <main role="main" className="page-crisp">
