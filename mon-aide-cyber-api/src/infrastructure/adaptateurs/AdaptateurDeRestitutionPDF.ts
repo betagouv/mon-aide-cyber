3b50c5f894ab20e2890147f586fdcea3d603bfa3
Bertrand Bougon <bertrand.bougon@gmail.com>
2025-05-27 15:31:10 +0200

    [RESTITUTION] Utilise le service de PDF déporté pour générer la restitution
    Et n’utilise plus la génération en local avec le chromium installé
    
    Le commit suivant supprimera totalement la présence de chromium en local

--- mon-aide-cyber-api/src/infrastructure/adaptateurs/AdaptateurDeRestitutionPDF.ts
+++ mon-aide-cyber-api/src/infrastructure/adaptateurs/AdaptateurDeRestitutionPDF.ts
@@ -14,8 +14,31 @@ const forgeIdentifiant = (identifiant: string): string =>
     6
   )} ${identifiant.substring(6, 8)}`.toUpperCase();
 
+type GenerationPDFExterne = {
+  verifieConfiguration: () => void;
+  endpointWebsocket: () => string;
+};
+const generationPdfExterne = (): GenerationPDFExterne => {
+  const { GENERATION_PDF_URL_DU_SERVICE, GENERATION_PDF_TOKEN_DU_SERVICE } =
+    process.env;
+
+  return {
+    verifieConfiguration: () => {
+      const urlEstDefinie = !!GENERATION_PDF_URL_DU_SERVICE;
+      const tokenEstDefini = !!GENERATION_PDF_TOKEN_DU_SERVICE;
+      if (!urlEstDefinie || !tokenEstDefini)
+        throw new Error(
+          "La génération externe de PDF est activée. Mais il manque la configuration de l'URL et du token."
+        );
+    },
+    endpointWebsocket: () =>
+      `${GENERATION_PDF_URL_DU_SERVICE}?token=${GENERATION_PDF_TOKEN_DU_SERVICE}`,
+  };
+};
+
 export class AdaptateurDeRestitutionPDF extends AdaptateurDeRestitution<Buffer> {
   private identifiant = '';
+
   constructor(traductionThematiques: Map<string, string>) {
     super(traductionThematiques);
   }
@@ -159,50 +182,10 @@ const formatPdfA4 = (enteteHtml: string, piedPageHtml: string): PDFOptions => ({
   margin: { bottom: '15mm', left: '15mm', right: '15mm', top: '15mm' },
 });
 
-const lanceNavigateur = (): Promise<Browser> =>
-  puppeteer.launch({
-    // Documentation des arguments : https://peter.sh/experiments/chromium-command-line-switches/
-    // Inspiration pour la liste utilisée ici : https://www.bannerbear.com/blog/ways-to-speed-up-puppeteer-screenshots/
-    // Le but est d'avoir un Puppeteer le plus léger et rapide à lancer possible.
-    args: [
-      '--no-sandbox',
-      '--font-render-hinting=none',
-      '--disable-gpu',
-      '--autoplay-policy=user-gesture-required',
-      '--disable-accelerated-2d-canvas',
-      '--disable-background-networking',
-      '--disable-background-timer-throttling',
-      '--disable-backgrounding-occluded-windows',
-      '--disable-breakpad',
-      '--disable-client-side-phishing-detection',
-      '--disable-component-update',
-      '--disable-default-apps',
-      '--disable-dev-shm-usage',
-      '--disable-domain-reliability',
-      '--disable-extensions',
-      '--disable-features=AudioServiceOutOfProcess',
-      '--disable-hang-monitor',
-      '--disable-ipc-flooding-protection',
-      '--disable-notifications',
-      '--disable-offer-store-unmasked-wallet-cards',
-      '--disable-popup-blocking',
-      '--disable-print-preview',
-      '--disable-prompt-on-repost',
-      '--disable-renderer-backgrounding',
-      '--disable-setuid-sandbox',
-      '--disable-speech-api',
-      '--disable-sync',
-      '--hide-scrollbars',
-      '--ignore-gpu-blacklist',
-      '--metrics-recording-only',
-      '--mute-audio',
-      '--no-default-browser-check',
-      '--no-first-run',
-      '--no-pings',
-      '--no-zygote',
-      '--password-store=basic',
-      '--single-process',
-      '--use-gl=swiftshader',
-      '--use-mock-keychain',
-    ],
+const lanceNavigateur = (): Promise<Browser> => {
+  const generationPDFExterne = generationPdfExterne();
+  generationPDFExterne.verifieConfiguration();
+  return puppeteer.connect({
+    browserWSEndpoint: generationPDFExterne.endpointWebsocket(),
   });
+};
