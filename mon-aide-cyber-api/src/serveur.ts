e678aed9d46631dd452e18e3ce7eb101a32b0edc
Bertrand Bougon <bertrand.bougon@gmail.com>
2025-06-06 12:26:07 +0200

    [SECURITE] Redirige vers / si la route demandée par le client est inconnue

--- mon-aide-cyber-api/src/serveur.ts
+++ mon-aide-cyber-api/src/serveur.ts
@@ -137,9 +137,7 @@ const creeApp = (config: ConfigurationServeur) => {
   app.use('/contact', routeContact(config));
   app.use('/statistiques', routesStatistiques(config));
 
-  app.get('*', (_: Request, reponse: Response) =>
-    envoieIndexAvecNonce(reponse)
-  );
+  app.get('*', (_: Request, reponse: Response) => reponse.redirect('/'));
 
   app.use(
     gestionnaireErreurGeneralisee(config.gestionnaireErreurs.consignateur())
