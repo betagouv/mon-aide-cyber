ccb247dae9798d37fab7b4a3bbe9b08bda3419ff
Bertrand Bougon <bertrand.bougon@gmail.com>
2025-06-10 17:52:19 +0200

    [RESTITUTION][ENVOI AUTOMATISE] Retourne l’action HATEOAS envoyer-restitution-entite-aidee pour permettre d’afficher le bouton dans la restitution

--- mon-aide-cyber-api/test/api/hateoas/hateoas.spec.ts
+++ mon-aide-cyber-api/test/api/hateoas/hateoas.spec.ts
@@ -1,9 +1,18 @@
 import { beforeEach, describe, expect, it } from 'vitest';
 import { lesContextesUtilisateursNouveauParcoursExistants } from './lesContextesUtilisateursExistants';
-import { constructeurActionsHATEOAS } from '../../../src/api/hateoas/hateoas';
+import {
+  constructeurActionsHATEOAS,
+  Options,
+} from '../../../src/api/hateoas/hateoas';
 import { InformationsContexte } from '../../../src/adaptateurs/AdaptateurDeVerificationDeSession';
 import { adaptateurEnvironnement } from '../../../src/adaptateurs/adaptateurEnvironnement';
 import { FournisseurHorlogeDeTest } from '../../infrastructure/horloge/FournisseurHorlogeDeTest';
+import { AdaptateurRelationsMAC } from '../../../src/relation/AdaptateurRelationsMAC';
+import { relieUneEntiteAideeAUnDiagnostic } from '../../constructeurs/relationsUtilisateursMACDiagnostic';
+import { unDiagnostic } from '../../constructeurs/constructeurDiagnostic';
+import { EntrepotsMemoire } from '../../../src/infrastructure/entrepots/memoire/EntrepotsMemoire';
+import { EntrepotRelationMemoire } from '../../../src/relation/infrastructure/EntrepotRelationMemoire';
+import { ServiceDeChiffrementClair } from '../../infrastructure/securite/ServiceDeChiffrementClair';
 
 describe('HATEOAS', () => {
   describe('Nouveau parcours', async () => {
@@ -27,5 +36,38 @@ describe('HATEOAS', () => {
         });
       }
     );
+  });
+
+  describe('Accès au diagnostic', () => {
+    it('retourne l’action envoyer-restitution-entite-aidee', async () => {
+      const entrepots = new EntrepotsMemoire();
+      const diagnostic = unDiagnostic().construis();
+      await entrepots.diagnostic().persiste(diagnostic);
+      const adaptateurRelationsMAC = new AdaptateurRelationsMAC(
+        new EntrepotRelationMemoire(),
+        new ServiceDeChiffrementClair()
+      );
+      const relationDiagnosticDemandeAide =
+        await relieUneEntiteAideeAUnDiagnostic(
+          'email-entite@aidee.com',
+          diagnostic.identifiant,
+          entrepots,
+          adaptateurRelationsMAC
+        );
+
+      const reponseHATEOAS = (
+        await constructeurActionsHATEOAS().demandeLaRestitution(
+          relationDiagnosticDemandeAide.diagnostic.identifiant,
+          adaptateurRelationsMAC
+        )
+      ).construis();
+
+      expect(
+        reponseHATEOAS.liens['envoyer-restitution-entite-aidee']
+      ).toStrictEqual<Options>({
+        url: `/api/diagnostic/${diagnostic.identifiant}/restitution/demande-envoi-mail-restitution`,
+        methode: 'POST',
+      });
+    });
   });
 });
