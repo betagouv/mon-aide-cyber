0dfa21eadaa211d253c2b078cb4c2812d3595c80
Marc ETOURNEAU <marc.etourneau@beta.gouv.fr>
2025-06-25 10:11:26 +0200

    [HEADER SITE VITRINE] Ajoute une icone utilisateur devant le lien "Se connecter" du Header

--- mon-aide-cyber-ui/src/domaine/authentification/SeConnecter.tsx
+++ mon-aide-cyber-ui/src/domaine/authentification/SeConnecter.tsx
@@ -1,6 +1,7 @@
 export const SeConnecter = () => {
   return (
     <a className="fr-btn entete-lien-se-connecter" href="/connexion">
+      <span className="fr-icon-user-line" aria-hidden="true"></span>
       Se connecter
     </a>
   );
