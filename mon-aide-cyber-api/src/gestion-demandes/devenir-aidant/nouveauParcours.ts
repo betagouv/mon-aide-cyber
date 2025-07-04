bdb5c087974b9f31a32a5c5f213750d179fcf71c
Bertrand Bougon <bertrand.bougon@gmail.com>
2025-07-04 15:55:38 +0200

    [DEMANDE DEVENIR AIDANT] Envoie le mail de participation à un atelier en utilisant le template prévu

--- mon-aide-cyber-api/src/gestion-demandes/devenir-aidant/nouveauParcours.ts
+++ mon-aide-cyber-api/src/gestion-demandes/devenir-aidant/nouveauParcours.ts
@@ -13,6 +13,3 @@ export const estDateNouveauParcoursDemandeDevenirAidant = () => {
     )
   );
 };
-
-export const estAvantDateNouveauParcours = () =>
-  !estDateNouveauParcoursDemandeDevenirAidant();
