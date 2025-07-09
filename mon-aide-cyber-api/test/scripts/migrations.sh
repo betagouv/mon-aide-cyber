685e61759b588128002284fa246b8a98a90043ff
Pamplemousse <git@xaviermaso.com>
2025-07-09 11:17:29 +0200

    [SOIN] Communique l'état des migrations au lancement des tests

--- mon-aide-cyber-api/test/scripts/migrations.sh
+++ mon-aide-cyber-api/test/scripts/migrations.sh
@@ -1,4 +1,9 @@
 #!/bin/bash -e
 
+echo "Installation des dépendances"
 npm -f install
+
+echo "Lancement des migrations"
 npx knex --knexfile ./src/infrastructure/entrepots/postgres/knexfile.ts migrate:latest
+
+echo "Migrations terminées"
