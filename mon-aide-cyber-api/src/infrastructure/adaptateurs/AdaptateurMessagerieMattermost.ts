f2967500d22df05bc4a57eb6ee5d51f0eef1b357
Marc ETOURNEAU <marc.etourneau@beta.gouv.fr>
2025-07-18 11:50:58 +0200

    [ACTIVATION AIDANT] Implémente la messagerie mattermost

--- mon-aide-cyber-api/src/infrastructure/adaptateurs/AdaptateurMessagerieMattermost.ts
+++ mon-aide-cyber-api/src/infrastructure/adaptateurs/AdaptateurMessagerieMattermost.ts
@@ -1,9 +1,17 @@
+import { adaptateurEnvironnement } from '../../adaptateurs/adaptateurEnvironnement';
+
 export interface Messagerie {
   envoieMessageMarkdown(message: string): Promise<void>;
 }
 
 export class AdaptateurMessagerieMattermost implements Messagerie {
-  envoieMessageMarkdown(_message: string): Promise<void> {
-    throw new Error('Method not implemented.');
+  async envoieMessageMarkdown(message: string): Promise<void> {
+    const urlWebhook = adaptateurEnvironnement.mattermost().webhook();
+    await fetch(urlWebhook, {
+      method: 'POST',
+      body: JSON.stringify({
+        text: message,
+      }),
+    });
   }
 }
