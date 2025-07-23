11392710d28fa53a065c2bb29b78fd8ede7ebd9e
Marc ETOURNEAU <marc.etourneau@beta.gouv.fr>
2025-07-23 11:29:34 +0200

    [AIDANT][GENDARMERIE] Met à jour les événements AIDANT_CREE des gendarmes dont il manque l'information

--- mon-aide-cyber-api/src/administration/gendarmes/rattrapage/metAJourLesGendarmesSansEntite.ts
+++ mon-aide-cyber-api/src/administration/gendarmes/rattrapage/metAJourLesGendarmesSansEntite.ts
@@ -5,14 +5,20 @@ export async function metAJourLesGendarmesSansEntite(
   gendarmesSansEntite: Aidant[],
   entrepotAidants: EntrepotAidant
 ) {
-  return gendarmesSansEntite.map(async (gendarmeSansEntite) => {
-    return entrepotAidants.persiste({
-      ...gendarmeSansEntite,
-      entite: {
-        nom: 'DIRECTION GENERALE DE LA GENDARMERIE NATIONALE (DGGN)',
-        siret: adaptateurEnvironnement.siretsEntreprise().gendarmerie()!,
-        type: 'ServiceEtat',
-      },
-    });
+  return gendarmesSansEntite.map(async (gendarmeSansEntite, index) => {
+    return entrepotAidants
+      .persiste({
+        ...gendarmeSansEntite,
+        entite: {
+          nom: 'DIRECTION GENERALE DE LA GENDARMERIE NATIONALE (DGGN)',
+          siret: adaptateurEnvironnement.siretsEntreprise().gendarmerie()!,
+          type: 'ServiceEtat',
+        },
+      })
+      .then(() => {
+        console.log(
+          `${((index + 1) / gendarmesSansEntite.length) * 100}% (${index + 1} / ${gendarmesSansEntite.length}) des utilisateurs_mac modifiés`
+        );
+      });
   });
 }
