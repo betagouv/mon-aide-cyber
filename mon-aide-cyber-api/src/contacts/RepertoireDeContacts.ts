f0a2bd16fdf01bd8cb9f0506ec19fae4f695ab9e
Christophe Cadilhac <christophe.cadilhac@beta.gouv.fr>
2025-08-01 16:36:01 +0200

    [WIP][ADMIN] Propage la mise à jour de l'email chez Brevo
    … pour éviter la désynchro entre notre BDD et Brevo.
    
    `uneRequeteBrevoPut()` : Ce commit rajoute un peu de généricité dans
    AdaptateurBrevo pour
    permettre de facilement faire de nouvelles requêtes sans déclarer
    plusieurs niveaux d'abstractions.

--- mon-aide-cyber-api/src/contacts/RepertoireDeContacts.ts
+++ mon-aide-cyber-api/src/contacts/RepertoireDeContacts.ts
@@ -2,6 +2,7 @@ export interface RepertoireDeContacts {
   creeAidant(email: string): Promise<void>;
   creeAide(email: string): Promise<void>;
   creeUtilisateurInscrit(email: string): Promise<void>;
+  modifieEmail(ancienEmail: string, nouvelEmail: string): Promise<void>;
   emetsEvenement(evenement: Evenement): Promise<void>;
 }
 
