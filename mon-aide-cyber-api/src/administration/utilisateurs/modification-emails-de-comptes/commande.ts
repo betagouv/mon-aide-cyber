e40d07cbe1f37db3aad4329dbdb14a6dcf080a9e
Christophe Cadilhac <christophe.cadilhac@beta.gouv.fr>
2025-08-04 11:17:32 +0200

    [ADMIN] Modifie le mail de connexion s'il existe côté MAC
    … pour ne pas désynchroniser la table de Profil et la table
    d'authentification.

--- mon-aide-cyber-api/src/administration/utilisateurs/modification-emails-de-comptes/commande.ts
+++ mon-aide-cyber-api/src/administration/utilisateurs/modification-emails-de-comptes/commande.ts
@@ -3,6 +3,8 @@ import { adaptateurServiceChiffrement } from '../../../infrastructure/adaptateur
 import { EntrepotAidantPostgres } from '../../../infrastructure/entrepots/postgres/EntrepotAidantPostgres';
 import { Aidant } from '../../../espace-aidant/Aidant';
 import { AdaptateurRepertoireDeContactsBrevo } from '../../../infrastructure/adaptateurs/AdaptateurRepertoireDeContactsBrevo';
+import { EntrepotUtilisateurPostgres } from '../../../infrastructure/entrepots/postgres/EntrepotUtilisateurPostgres';
+import { Utilisateur } from '../../../authentification/Utilisateur';
 
 const l = console.log;
 
@@ -37,12 +39,13 @@ program
 
     l(`🚚 C'est parti…`);
 
-    const entrepotAidants = new EntrepotAidantPostgres(
-      adaptateurServiceChiffrement()
-    );
+    const chiffrement = adaptateurServiceChiffrement();
+    const entrepotAidants = new EntrepotAidantPostgres(chiffrement);
+    const comptesUtilisateursMAC = new EntrepotUtilisateurPostgres(chiffrement);
 
     const brevo = new AdaptateurRepertoireDeContactsBrevo();
     const tousLesAidants: Aidant[] = await entrepotAidants.tous();
+    const tousLesComptes = await comptesUtilisateursMAC.tous();
 
     for (const { ancien, nouveau } of todo) {
       const utilisateur = tousLesAidants.find(
@@ -55,13 +58,31 @@ program
       }
 
       try {
-        l(`⚫️⚫️ ${ancien} : démarrage de la modification vers ${nouveau}`);
+        l(`⚫️⚫️⚫️ ${ancien} : démarrage de la modification vers ${nouveau}`);
+
         // On commence par Brevo car ça semble le plus fragile…
         await brevo.modifieEmail(ancien, nouveau);
-        l(`🟢⚫️ ${ancien} : email modifié en ${nouveau} chez Brevo`);
+        l(`🟢⚫️⚫️ ${ancien} : email modifié en ${nouveau} chez Brevo`);
 
         await entrepotAidants.persiste({ ...utilisateur, email: nouveau });
-        l(`🟢🟢 ${ancien} : email modifié en ${nouveau} chez nous`);
+        l(`🟢🟢⚫️ ${ancien} : email modifié en ${nouveau} chez nous`);
+
+        const compteMAC = tousLesComptes.find(
+          (c) =>
+            c.identifiantConnexion.toLowerCase().trim() ===
+            ancien.toLowerCase().trim()
+        ) as Utilisateur;
+        if (compteMAC) {
+          const aJour = { ...compteMAC, identifiantConnexion: nouveau };
+          await comptesUtilisateursMAC.persiste(aJour);
+          l(
+            `🟢🟢🟢️ ${ancien} : compte d'authentification modifié en ${nouveau} chez nous`
+          );
+        } else {
+          l(
+            `🟢🟢🔵 ${ancien} : pas de compte d'authentification à modifier chez nous`
+          );
+        }
       } catch (e) {
         l(
           `💥 ${ancien} : problème lors de la mise à jour… sûrement à cause de Brevo`
