adba97bfedd8d34bc2968914538052472144fa2e
Bertrand Bougon <bertrand.bougon@gmail.com>
2026-02-05 11:03:44 +0100

    [LIVESTORM] Ajoute un log de toute la requete

--- mon-aide-cyber-api/src/adaptateurs/AdaptateurSignatureRequeteHTTP.ts
+++ mon-aide-cyber-api/src/adaptateurs/AdaptateurSignatureRequeteHTTP.ts
@@ -31,7 +31,7 @@ class FournisseurSignatureTally implements FournisseurSignature {
 
 class FournisseurSignatureLivestorm implements FournisseurSignature {
   verifie(requete: Request): boolean {
-    console.log('VÉRIFICATION SIGNATURE LIVESTORM');
+    console.log(`VÉRIFICATION SIGNATURE LIVESTORM ${requete}`);
     const secretLivestormFinAtelier = adaptateurEnvironnement
       .signatures()
       .livestorm().finAtelier;
