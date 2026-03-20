09da7919d275cb68b77c9d56201af5df6f2edd90
Bertrand Bougon <bertrand.bougon@gmail.com>
2026-03-20 11:41:29 +0100

    Écris le résultat des aides non pourvues dans un fichier
    … et imprime le nombre de demandes non pourvues sur la sortie

--- mon-aide-cyber-api/src/administration/gestion-demandes/aides/commande.ts
+++ mon-aide-cyber-api/src/administration/gestion-demandes/aides/commande.ts
@@ -2,6 +2,8 @@ import { program } from 'commander';
 import { lesAidesNonPourvues } from './aidesNonPourvues';
 import { fabriqueEntrepots } from '../../../adaptateurs/fabriqueEntrepots';
 import { AdaptateurRelationsMAC } from '../../../relation/AdaptateurRelationsMAC';
+import * as fs from 'node:fs/promises';
+import path from 'node:path';
 
 const command = program.description(
   'Obtenir la liste des demandes d’Aides non pourvues'
@@ -15,7 +17,28 @@ command.action(async () => {
     new AdaptateurRelationsMAC()
   ).recherche();
 
-  console.log(JSON.stringify(aidesNonPourvues, undefined, 2));
+  console.log(`Obtention des aides non pourvues terminée :`);
+  console.log(`- ${aidesNonPourvues.length} demandes d’aides non pourvues`);
+  const cheminFichier = path.join(
+    process.cwd(),
+    'tmp',
+    'aides-non-pourvues.json'
+  );
+  try {
+    await fs.mkdir(path.dirname(cheminFichier), { recursive: true });
+    await fs.writeFile(
+      cheminFichier,
+      JSON.stringify(aidesNonPourvues, undefined, 2),
+      'utf-8'
+    );
+    console.log(`Résultat écrit dans le fichier : ${cheminFichier}`);
+  } catch (erreur) {
+    console.error(
+      `Erreur lors de l'écriture du fichier ${cheminFichier}`,
+      erreur
+    );
+  }
+
   process.exit(0);
 });
 
