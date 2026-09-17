27d4cf337299cbe65a4880387947bf1a63db0b5d
Bertrand Bougon <bertrand.bougon@gmail.com>
2026-09-17 11:56:12 +0200

    [CORRECTION] Corrige la génération IV

--- mon-aide-cyber-api/test/gestion-demandes/aide/MiseEnRelationParCriteres.spec.ts
+++ mon-aide-cyber-api/test/gestion-demandes/aide/MiseEnRelationParCriteres.spec.ts
@@ -376,7 +376,6 @@ describe('Mise en relation par critères', () => {
 
     it('Chiffre et déchiffre le token de mise en relation', async () => {
       const serviceDeChiffrement = new ServiceDeChiffrementChacha20(
-        Buffer.from('ivlongueur12'),
         Buffer.from('assoc-longueur16'),
         'ma-clef-secrete-de-longueur-0032'
       );
@@ -390,9 +389,7 @@ describe('Mise en relation par critères', () => {
         serviceDeChiffrement
       ).chiffre(demandeAide.email, demandeAide.identifiant, identifiantAidant);
 
-      expect(valeurChiffree).toBe(
-        'Njk3NjZjNmY2ZTY3NzU2NTc1NzIzMTMyNjE3MzczNmY2MzJkNmM2ZjZlNjc3NTY1NzU3MjMxMzZmNWY0ZDFhNjc0Y2JhYjMwMGM1OGZmMmI4ZWI4YmFjMmEzOTY5NWY3NWMxMDY2MTg0ZGY3MWMyNTViZWIwNTVjOTg4NDIyYzE0OWVmMDI4YzdhMzlkMzliMzRlYTJjNTkyM2ZiZTRlZmM0ZjhhMTFmYmUxOTE2MGRjMDQzNDhlYmFlNjczNGYxNGVjZjhkY2JhNzk4MjE0N2U5ZDA4NjliZDI4NTE3MTYxNjEyOTAwMGVhZjg3NTA5N2Y1MDc3NDNiN2M1YTZkNDBkZDE4ZWQ3NDlhMDNjZDcwZWQxNjc1YmRmYzI2YTE0ZWY0ZjdhYjRhZjFkOTliNzhhYjM2MzFhOTYxYjZmYjNmZWU4MDg4ZmM2MDhlM2I4MDg3MTViMDMxOTA1ZTE0OGMxMjg1MDE3MTc2NGY5ZjY4MTcxYzMxMjMzM2FjOWQwNmVmZmViMTA3MWIyMzU0YmJmMDQ1YQ=='
-      );
+      expect(valeurChiffree.length).toBe(544);
       expect(
         tokenAttributionDemandeAide(serviceDeChiffrement).dechiffre(
           valeurChiffree
